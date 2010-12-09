09bdf9c42950e092dc67fcd3bc66ab94edf8b91e
fddima <fddima@gmail.com>
2010-12-09 21:56:31 +0200

    Added new test project "CefUsageTests".

---
+++ CefUsageTests/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CefUsageTests
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnTest1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel.Location = new System.Drawing.Point(12, 106);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(556, 355);
+            this.panel.TabIndex = 0;
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.Location = new System.Drawing.Point(12, 12);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(75, 23);
+            this.btnTest1.TabIndex = 1;
+            this.btnTest1.Text = "Test1";
+            this.btnTest1.UseVisualStyleBackColor = true;
+            this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 473);
+            this.Controls.Add(this.btnTest1);
+            this.Controls.Add(this.panel);
+            this.Name = "MainForm";
+            this.Text = "Cef Usage Tests";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnTest1;
+    }
+}
+
---
+++ CefUsageTests/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp;
+
+namespace CefUsageTests
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private BrowserControl browser;
+        private void btnTest1_Click(object sender, EventArgs e)
+        {
+            browser = new BrowserControl("http://google.com");
+            browser.Parent = panel;
+            browser.Dock = DockStyle.Fill;
+        }
+    }
+}
