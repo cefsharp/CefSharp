23925d61420f30b73d2299e5fce1a7370efd65b0
fddima <fddima@gmail.com>
2010-12-14 00:50:15 +0200

    Initial NUnit tests.

---
+++ CefSharp.Tests/BindObjectTest.cs
@@ -0,0 +1,38 @@
+﻿namespace CefSharp.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Windows.Forms;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BindObjectTest
+    {
+        private BrowserControl WebBrowser { get { return BrowserApplication.WebBrowser; } }
+
+        [Test]
+        public void Test1()
+        {
+            WebBrowser.Load("http://google.com/");
+            WebBrowser.WaitForLoadCompletion();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            WebBrowser.Load("http://ya.ru/");
+            WebBrowser.WaitForLoadCompletion();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            WebBrowser.Load("http://microsoft.com/");
+            WebBrowser.WaitForLoadCompletion();
+        }
+
+    }
+}
---
+++ CefSharp.Tests/BrowserApplication.cs
@@ -0,0 +1,73 @@
+﻿namespace CefSharp.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NUnit.Framework;
+    using System.Windows.Forms;
+    using System.Threading;
+
+    [SetUpFixture]
+    public class BrowserApplication
+    {
+        public static BrowserApplication Instance { get; private set; }
+        public static Form MainForm { get; private set; }
+        public static BrowserControl WebBrowser { get; private set; }
+
+        private ManualResetEvent waitCreated;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Instance = this;
+            waitCreated = new ManualResetEvent(false);
+
+            Settings settings = new Settings();
+            BrowserSettings browserSettings = new BrowserSettings();
+
+            if(!CEF.Initialize(settings, browserSettings))
+            {
+                Assert.Fail("Couldn't initialise CEF.");
+                return;
+            }
+
+            // CEF.RegisterScheme("test", new TestSchemeHandlerFactory());
+
+            // Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+
+            var uiThread = new Thread(UIThread);
+            uiThread.SetApartmentState(ApartmentState.STA);
+            uiThread.Start();
+            waitCreated.WaitOne();
+        }
+
+        private void UIThread()
+        {
+            MainForm = new Form();
+            MainForm.Shown += new EventHandler(MainForm_Shown);
+
+            Application.Run(MainForm);
+        }
+
+        void MainForm_Shown(object sender, EventArgs e)
+        {
+            WebBrowser = new BrowserControl("http://google.com");
+            WebBrowser.Parent = MainForm;
+            WebBrowser.Dock = DockStyle.Fill;
+            WebBrowser.WaitForLoadCompletion();
+
+            waitCreated.Set();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("TearDown");
+            waitCreated.WaitOne();
+            MainForm.Close();
+            Application.Exit();
+        }
+    }
+}
