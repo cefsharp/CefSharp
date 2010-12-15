b2c08d61421ed57831d3dfef1ffb91ca29900424
fddima <fddima@gmail.com>
2010-12-15 19:07:38 +0200

    Swap to home.

--- CefSharp/BindingHandler.cpp
+++ CefSharp/BindingHandler.cpp
@@ -15,25 +15,26 @@ namespace CefSharp
             return CefV8Value::CreateNull();
         }
 
-	    if (type == Boolean::typeid)
+        if (type == Boolean::typeid)
         {
             return CefV8Value::CreateBool(safe_cast<bool>(obj));
         }
-	    if (type == Int16::typeid || type == Int32::typeid)
+        if (type == Int16::typeid || type == Int32::typeid)
         {
             return CefV8Value::CreateInt(safe_cast<int>(obj));
         }
-	    if (type == Single::typeid || type == Double::typeid)
+        if (type == Single::typeid || type == Double::typeid)
         {
             return CefV8Value::CreateDouble(safe_cast<double>(obj));
         }
         if (type == String::typeid)
-	    {
+        {
             CefString str = convertFromString(safe_cast<String^>(obj));
             return CefV8Value::CreateString(str);
-	    }
-        
-        throw gcnew Exception("cannot convert object from CLR to Cef " + type->ToString());
+        }
+
+        //TODO: What exception type?
+        throw gcnew Exception("Cannot convert object from CLR to Cef " + type->ToString() + ".");
     }
 
     Object^ BindingHandler::ConvertFromCef(CefRefPtr<CefV8Value> obj)
@@ -51,8 +52,9 @@ namespace CefSharp
         if (obj->IsString()){
             return convertToString(obj->GetStringValue());
         }
-        
-        throw gcnew Exception("cannot convert object from Cef to CLR");
+
+        //TODO: What exception type?
+        throw gcnew Exception("Cannot convert object from Cef to CLR.");
     }
 
     bool BindingHandler::Execute(const CefString& name, CefRefPtr<CefV8Value> object, const CefV8ValueList& arguments, CefRefPtr<CefV8Value>& retval, CefString& exception)
@@ -68,7 +70,7 @@ namespace CefSharp
         String^ memberName = convertToString(name);
         Type^ type = self->GetType();
         array<System::Reflection::MemberInfo^>^ members = type->GetMember(memberName, MemberTypes::Method, 
-            BindingFlags::IgnoreCase | BindingFlags::Instance | BindingFlags::Public);
+            /* BindingFlags::IgnoreCase |*/ BindingFlags::Instance | BindingFlags::Public);
 
         if(members->Length == 0)
         {
@@ -77,17 +79,17 @@ namespace CefSharp
         }
 
         array<System::Object^>^ suppliedArguments = gcnew array<Object^>(arguments.size());
-        for(int i = 0; i < suppliedArguments->Length; i++) 
+        try
         {
-            try
+            for(int i = 0; i < suppliedArguments->Length; i++) 
             {
-                suppliedArguments[i] = ConvertFromCef(arguments[i]);
+                    suppliedArguments[i] = ConvertFromCef(arguments[i]);
             }
-            catch(System::Exception^ err)
-            {
-                exception = convertFromString(err->Message);
-                return true;
-            }
+        }
+        catch(System::Exception^ err)
+        {
+            exception = convertFromString(err->Message);
+            return true;
         }
 
         MethodInfo^ bestMethod;
@@ -95,66 +97,66 @@ namespace CefSharp
         int bestMethodMatchedArgs = -1;
 
         for (int i = 0; i < members->Length; i++)
-	    {
-		    MethodInfo^ method = (MethodInfo^) members[i];
-		    array<ParameterInfo^>^ parametersInfo = method->GetParameters();
-		    array<Object^>^ arguments;
-            
-		    if (suppliedArguments->Length == parametersInfo->Length)
-		    {
-			    int match = 0;
-			    int failed = 0;
+        {
+            MethodInfo^ method = (MethodInfo^) members[i];
+            array<ParameterInfo^>^ parametersInfo = method->GetParameters();
+            array<Object^>^ arguments;
 
-			    
-			    arguments = gcnew array<Object^>(suppliedArguments->Length);
-			    for (int p = 0; p < suppliedArguments->Length; p++)
-			    {
-				    System::Type^ paramType = parametersInfo[p]->ParameterType;
+            if (suppliedArguments->Length == parametersInfo->Length)
+            {
+                int match = 0;
+                int failed = 0;
 
-				    if (suppliedArguments[p] != nullptr)
-				    {
-					    System::Type^ suppliedType = suppliedArguments[p]->GetType();
 
-					    if (suppliedType == paramType)
-					    {
-						    arguments[p] = suppliedArguments[p];
-						    match++;
-					    }
-					    else
-					    {
-                            // TODO
-						    //arguments[p] = ConvertToType(suppliedArguments[p], paramType);
-						    //if (arguments[p] == nullptr)
-						    //{
-							    failed++;
-							    break;
-						    //}
-					    }
-				    }
-			    }
-
-			    if (failed > 0)
+                arguments = gcnew array<Object^>(suppliedArguments->Length);
+                for (int p = 0; p < suppliedArguments->Length; p++)
                 {
-				    continue;
+                    System::Type^ paramType = parametersInfo[p]->ParameterType;
+
+                    if (suppliedArguments[p] != nullptr)
+                    {
+                        System::Type^ suppliedType = suppliedArguments[p]->GetType();
+
+                        if (suppliedType == paramType)
+                        {
+                            arguments[p] = suppliedArguments[p];
+                            match++;
+                        }
+                        else
+                        {
+                            // TODO
+                            //arguments[p] = ConvertToType(suppliedArguments[p], paramType);
+                            //if (arguments[p] == nullptr)
+                            //{
+                            failed++;
+                            break;
+                            //}
+                        }
+                    }
                 }
 
-			    if (match > bestMethodMatchedArgs)
-			    {
-				    bestMethod = method;
-				    bestMethodArguments = arguments;
-				    bestMethodMatchedArgs = match;
-			    }
+                if (failed > 0)
+                {
+                    continue;
+                }
 
-			    if (match == arguments->Length)
-				    break;
-		    }
+                if (match > bestMethodMatchedArgs)
+                {
+                    bestMethod = method;
+                    bestMethodArguments = arguments;
+                    bestMethodMatchedArgs = match;
+                }
+
+                if (match == arguments->Length)
+                    break;
+            }
         }
 
         if (bestMethod != nullptr)
         {
             try
             {
-	            Object^ result = bestMethod->Invoke(self, bestMethodArguments);
+                Object^ result = bestMethod->Invoke(self, bestMethodArguments);
                 retval = ConvertToCef(bestMethod->ReturnType, result);
                 return true;
             }
@@ -180,7 +182,7 @@ namespace CefSharp
         CefRefPtr<CefBase> userData = static_cast<CefRefPtr<CefBase>>(bindingData);
         CefRefPtr<CefV8Value> wrappedObject = window->CreateObject(userData);
         CefRefPtr<CefV8Handler> handler = static_cast<CefV8Handler*>(new BindingHandler());
-        
+
         array<MethodInfo^>^ methods = obj->GetType()->GetMethods(BindingFlags::Instance | BindingFlags::Public);
 
         IList<String^>^ methodNames = gcnew List<String^>();
@@ -193,7 +195,7 @@ namespace CefSharp
         }
 
         for each(String^ methodName in methodNames)
-        {                
+        {
             CefString nameStr = convertFromString(methodName);
             wrappedObject->SetValue(nameStr, CefV8Value::CreateFunction(nameStr, handler));
         }
