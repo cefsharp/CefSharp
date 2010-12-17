78ca2c17e3625cfed8563696ceaa9b19e97d7ca3
chillitom <tom.rathbone@gmail.com>
2010-12-17 20:11:33 +0800

    reworked IBeforeResourceLoad with a cleaner API
    users now manipulate a RequestResponse object rather than setting ref arguments

---
+++ CefSharp/RequestResponse.h
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#pragma once
+
+#include "Request.h"
+
+using namespace System;
+using namespace System::IO;
+
+namespace CefSharp
+{
+
+    public interface class IRequestResponse
+    {
+        /// cancel the request, return nothing
+        void Cancel();        
+        /// the current request
+        property IRequest^ Request { IRequest^ get(); };
+        /// respond with redirection to the provided URL
+        void Redirect(String^ url);
+        /// respond with data from Stream
+        void RespondWith(Stream^ stream, String^ mimeType);
+    };
+
+    enum class ResponseAction
+    {
+        Continue, Cancel, Redirect, Respond
+    };
+
+
+    ref class RequestResponse : IRequestResponse
+    {
+        IRequest^ _request;
+        Stream^ _responseStream;
+        String^ _mimeType;
+        String^ _redirectUrl;        
+        ResponseAction _action;
+
+    internal:
+
+        RequestResponse(IRequest^ request) : 
+            _action(ResponseAction::Continue),
+            _request(request) {}
+
+        property Stream^ ResponseStream { Stream^ get() { return _responseStream; } }
+        property String^ MimeType { String^ get() { return _mimeType; } }
+        property String^ RedirectUrl { String^ get() { return _redirectUrl; } }
+        property ResponseAction Action { ResponseAction get() { return _action; } }
+
+    public:
+
+        virtual void Cancel();
+        virtual property IRequest^ Request { IRequest^ get() { return _request; } }
+        virtual void Redirect(String^ url);
+        virtual void RespondWith(Stream^ stream, String^ mimeType);
+    };
+
+}
