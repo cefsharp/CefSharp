1485dc5adbfd1d611436371273479c41c9d4070e
anthony taranto <anthony.taranto@gmail.com>
2011-04-19 12:26:49 -0700

    add CefSharp.WpfExample project

---
+++ CefSharp.WpfExample/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace CefSharp.WpfExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
