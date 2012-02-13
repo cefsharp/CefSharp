182237f863703da85e58be96e9299fec9eaf4da1
anthony taranto <anthony.taranto@gmail.com>
2012-02-13 15:39:56 -0800

    base CefSharp on CEF binary 1.963.439

--- include/internal/cef_types.h
+++ include/internal/cef_types.h
@@ -391,14 +391,9 @@ typedef struct _cef_browser_settings_t
   bool accelerated_2d_canvas_disabled;
 
   ///
-  // Set to true (1) to disable accelerated painting.
+  // Set to true (1) to disable accelerated drawing.
   ///
-  bool accelerated_painting_disabled;
-
-  ///
-  // Set to true (1) to disable accelerated filters.
-  ///
-  bool accelerated_filters_disabled;
+  bool accelerated_drawing_disabled;
 
   ///
   // Set to true (1) to disable accelerated plugins.
