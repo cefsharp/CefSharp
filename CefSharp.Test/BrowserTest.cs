095a785908222d8be5b9c45403a7c1d938b16b1c
anthony taranto <anthony.taranto@gmail.com>
2012-02-16 15:27:06 -0800

    reimplement basic nunit test setup

---
+++ CefSharp.Test/BrowserTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace CefSharp.Test
+{
+    [TestFixture]
+    public class BrowserTest
+    {
+        [Test]
+        public void FooTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void RunScriptText()
+        {
+            Assert.AreEqual("4", Fixture.Browser.RunScript("2+2"));
+        }
+
+        [Test]
+        public void RunScriptConcurrentTest()
+        {
+            for (var x = 0; x < 3; x++)
+            {
+                var value = x.ToString();
+                new Thread(() =>
+                {
+                    var result = Fixture.Browser.RunScript(value);
+                    Console.WriteLine("{0} => {1}", value, result);
+                    Assert.AreEqual(value, result);
+                }).Start();
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ test.sh
@@ -0,0 +1,1 @@
+./nunit/nunit-console-x86.exe -domain=None Debug/CefSharp.Test.dll
