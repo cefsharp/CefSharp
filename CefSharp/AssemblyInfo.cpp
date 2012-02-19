00a7faabe8ba27b646d3ad11d03f228cc3f05108
anthony taranto <anthony.taranto@gmail.com>
2012-02-18 21:36:36 -0800

    clean up AssemblyInfo

--- CefSharp/AssemblyInfo.cpp
+++ CefSharp/AssemblyInfo.cpp
@@ -6,35 +6,12 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-//
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly:AssemblyTitleAttribute("CefSharp")];
-[assembly:AssemblyDescriptionAttribute("")];
-[assembly:AssemblyConfigurationAttribute("")];
-[assembly:AssemblyCompanyAttribute("Tom Rathbone")];
-[assembly:AssemblyProductAttribute("CefSharp")];
-[assembly:AssemblyCopyrightAttribute("Copyright (c) Tom Rathbone 2010")];
-[assembly:AssemblyTrademarkAttribute("")];
-[assembly:AssemblyCultureAttribute("")];
+[assembly:AssemblyTitle("CefSharp")];
+[assembly:AssemblyCompany("Anthony Taranto")];
+[assembly:AssemblyProduct("CefSharp")];
+[assembly:AssemblyCopyright("Copyright (c) Anthony Taranto 2012")];
 
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the value or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-
-[assembly:AssemblyVersionAttribute("0.3.*")];
-
+[assembly:AssemblyVersion("0.9.*")];
 [assembly:ComVisible(false)];
-
-[assembly:CLSCompliantAttribute(true)];
-
+[assembly:CLSCompliant(true)];
 [assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
