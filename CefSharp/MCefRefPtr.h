e8084b2e23e6028c49effbf5a03c38ad1517a7b9
anthony taranto <anthony.taranto@gmail.com>
2012-02-23 16:43:58 -0800

    namespace MCefRefPtr

--- CefSharp/MCefRefPtr.h
+++ CefSharp/MCefRefPtr.h
@@ -1,118 +1,122 @@
 #pragma once
 
-template <typename T>
-ref class MCefRefPtr sealed
+namespace CefSharp
 {
-
-public:
-
-  MCefRefPtr() : _ptr(NULL) {}
-
-  MCefRefPtr(T* p) : _ptr(p)
-  {
-    if (_ptr)
+    template <typename T>
+    ref class MCefRefPtr sealed
     {
-        _ptr->AddRef();
-    }
-  }
+    private:
+        T* _ptr;
 
-  MCefRefPtr(const MCefRefPtr<T>% r) : _ptr(r._ptr)
-  {
-    if (_ptr)
-    {
-        _ptr->AddRef();
-    }
-  }
+    public:
+        MCefRefPtr() : _ptr(NULL) {}
 
-  MCefRefPtr(const CefRefPtr<T>& r) : _ptr(r.get())
-  {
-    if (_ptr)
-    {
-        _ptr->AddRef();
-    }
-  }
+        MCefRefPtr(T* p) : _ptr(p)
+        {
+            if (_ptr)
+            {
+                _ptr->AddRef();
+            }
+        }
 
-  ~MCefRefPtr()
-  {
-      if (_ptr)
-      {
-          _ptr->Release();
-      }
-  }
+        MCefRefPtr(const MCefRefPtr<T>% r) : _ptr(r._ptr)
+        {
+            if (_ptr)
+            {
+                _ptr->AddRef();
+            }
+        }
 
-  !MCefRefPtr()
-  {
-      if (_ptr)
-      {
-          _ptr->Release();
-      }
-  }
+        MCefRefPtr(const CefRefPtr<T>& r) : _ptr(r.get())
+        {
+            if (_ptr)
+            {
+                _ptr->AddRef();
+            }
+        }
 
-  T* get()
-  {
-      return _ptr;
-  }
+        ~MCefRefPtr()
+        {
+            if (_ptr)
+            {
+                _ptr->Release();
+            }
+        }
 
-  /*
-  operator T*()  // commented out for now as this operator interferes with the return statement of MCefRefPtr<T>% operator=(T* p)
-  { 
-      return _ptr; 
-  }
-  */
-  
-  T* operator->() 
-  {
-      return _ptr;
-  }
+        !MCefRefPtr()
+        {
+            if (_ptr)
+            {
+                _ptr->Release();
+            }
+        }
 
-  MCefRefPtr<T>% operator=(T* p)
-  {
-      // AddRef first so that self assignment should work
-      if (p)
-      {
-          p->AddRef();
-      }
-      if (_ptr )
-      {
-          _ptr->Release();
-      }
-      _ptr = p;
-      return *this;
-  }
+        T* get()
+        {
+            return _ptr;
+        }
 
-  MCefRefPtr<T>% operator=(const MCefRefPtr<T>% r)
-  {
-      return %this = r._ptr;
-  }
+        /*
+         * commented out for now as this operator interferes with the
+         * return statement of MCefRefPtr<T>% operator=(T* p)
+        operator T*()
+        { 
+          return _ptr; 
+        }
+        */
+      
+        T* operator->() 
+        {
+            return _ptr;
+        }
 
-  void swap(T** pp)
-  {
-      T* p = _ptr;
-      _ptr = *pp;
-      *pp = p;
-  }
+        MCefRefPtr<T>% operator=(T* p)
+        {
+            // AddRef first so that self assignment should work
+            if (p)
+            {
+                p->AddRef();
+            }
+            if (_ptr )
+            {
+                _ptr->Release();
+            }
 
-  void swap(MCefRefPtr<T>% r)
-  {
-      swap(%r._ptr);
-  }
+            _ptr = p;
+            return *this;
+        }
 
-  virtual bool Equals(Object^ obj) override
-  {
-      if(obj->GetType() == GetType())
-      {
-          MCefRefPtr^ other = safe_cast<MCefRefPtr^>(obj);
-          return (*other)._ptr == _ptr;
-      }
-      return false;
-  }
+        MCefRefPtr<T>% operator=(const MCefRefPtr<T>% r)
+        {
+            return %this = r._ptr;
+        }
 
-  virtual int GetHashCode() override
-  {
-      return (int)_ptr;
-  }
+        void swap(T** pp)
+        {
+            T* p = _ptr;
+            _ptr = *pp;
+            *pp = p;
+        }
 
-private:
-    T* _ptr;
-};
+        void swap(MCefRefPtr<T>% r)
+        {
+            swap(%r._ptr);
+        }
 
+        virtual bool Equals(Object^ obj) override
+        {
+            if(obj->GetType() == GetType())
+            {
+                MCefRefPtr^ other = safe_cast<MCefRefPtr^>(obj);
+                return (*other)._ptr == _ptr;
+            }
+
+            return false;
+        }
+
+        virtual int GetHashCode() override
+        {
+            return (int)_ptr;
+        }
+    };
+}
