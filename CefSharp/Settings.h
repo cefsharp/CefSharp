93052b090cb0c608a0b1e71a5f095b0ebdb1733c
anthony taranto <anthony.taranto@gmail.com>
2012-03-19 18:32:48 -0700

    Settings.h: use collapsed format to match BrowserSettings.h

--- CefSharp/Settings.h
+++ CefSharp/Settings.h
@@ -48,54 +48,26 @@ namespace CefSharp
 
         property String^ CachePath
         {
-            String^ get()
-            {
-                return toClr(_cefSettings->cache_path);
-            }
-
-            void set(String^ path)
-            {
-                assignFromString(_cefSettings->cache_path, path);
-            }
+            String^ get() { return toClr(_cefSettings->cache_path); }
+            void set(String^ path) { assignFromString(_cefSettings->cache_path, path); }
         }
 
         property String^ UserAgent
         {
-            String^ get()
-            {
-                return toClr(_cefSettings->user_agent);
-            }
-
-            void set(String^ userAgent)
-            {
-                assignFromString(_cefSettings->user_agent, userAgent);
-            }
+            String^ get() { return toClr(_cefSettings->user_agent); }
+            void set(String^ userAgent) { assignFromString(_cefSettings->user_agent, userAgent); }
         }
 
         property String^ ProductVersion
         {
-            String^ get()
-            {
-                return toClr(_cefSettings->product_version);
-            }
-
-            void set(String^ productVersion)
-            {
-                assignFromString(_cefSettings->product_version, productVersion);
-            }
+            String^ get() { return toClr(_cefSettings->product_version); }
+            void set(String^ productVersion) { assignFromString(_cefSettings->product_version, productVersion); }
         }
 
         property String^ Locale
         {
-            String^ get()
-            {
-                return toClr(_cefSettings->locale);
-            }
-
-            void set(String^ locale)
-            {
-                assignFromString(_cefSettings->locale, locale);
-            }
+            String^ get() { return toClr(_cefSettings->locale); }
+            void set(String^ locale) { assignFromString(_cefSettings->locale, locale); }
         }
 
         void AddPluginPath(String^ path)
@@ -110,80 +82,38 @@ namespace CefSharp
 
         property String^ LogFile
         {
-            String^ get()
-            {
-                return toClr(_cefSettings->log_file);
-            }
-
-            void set(String^ logFile)
-            {
-                assignFromString(_cefSettings->log_file, logFile);
-            }
+            String^ get() { return toClr(_cefSettings->log_file); }
+            void set(String^ logFile) { assignFromString(_cefSettings->log_file, logFile); }
         }
 
         property CefSharp::LogSeverity LogSeverity
         {
-            CefSharp::LogSeverity get()
-            {
-                return static_cast<CefSharp::LogSeverity>(_cefSettings->log_severity);
-            }
-
-            void set(CefSharp::LogSeverity logSeverity)
-            {
-                _cefSettings->log_severity = static_cast<cef_log_severity_t>(logSeverity);
-            }
+            CefSharp::LogSeverity get() { return static_cast<CefSharp::LogSeverity>(_cefSettings->log_severity); }
+            void set(CefSharp::LogSeverity logSeverity) { _cefSettings->log_severity = static_cast<cef_log_severity_t>(logSeverity); }
         }
 
         property bool AutoDetectProxySettings
         {
-            bool get()
-            {
-                return _cefSettings->auto_detect_proxy_settings_enabled;
-            }
-
-            void set(bool autoDetectProxySettings)
-            {
-                _cefSettings->auto_detect_proxy_settings_enabled = autoDetectProxySettings;
-            }
+            bool get() { return _cefSettings->auto_detect_proxy_settings_enabled; }
+            void set(bool autoDetectProxySettings) { _cefSettings->auto_detect_proxy_settings_enabled = autoDetectProxySettings; }
         }
 
         property String^ PackFilePath
         {
-            String^ get()
-            {
-                return toClr(_cefSettings->pack_file_path);
-            }
-
-            void set(String^ packFilePath)
-            {
-                assignFromString(_cefSettings->pack_file_path, packFilePath);
-            }
+            String^ get() { return toClr(_cefSettings->pack_file_path); }
+            void set(String^ packFilePath) { assignFromString(_cefSettings->pack_file_path, packFilePath); }
         }
 
         property String^ LocalesDirPath
         {
-            String^ get()
-            {
-                return toClr(_cefSettings->locales_dir_path);
-            }
-
-            void set(String^ localesDirPath)
-            {
-                assignFromString(_cefSettings->locales_dir_path, localesDirPath);
-            }
+            String^ get() { return toClr(_cefSettings->locales_dir_path); }
+            void set(String^ localesDirPath) { assignFromString(_cefSettings->locales_dir_path, localesDirPath); }
         }
 
         property bool PackLoadingDisabled
         {
-            bool get()
-            {
-                return _cefSettings->pack_loading_disabled;
-            }
-
-            void set(bool packLoadingDisabled)
-            {
-                _cefSettings->pack_loading_disabled = packLoadingDisabled;
-            }
+            bool get() { return _cefSettings->pack_loading_disabled; }
+            void set(bool packLoadingDisabled) { _cefSettings->pack_loading_disabled = packLoadingDisabled; }
         }
     };
 }
