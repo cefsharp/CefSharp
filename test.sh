e3837a07d9dda1e3409d12972e7396966924bf18
Marcin Wisnicki <marcin.wisnicki@microgen.com>
2012-07-26 17:18:48 +0200

    Run tests with single nunit runner (in separate processes).

--- test.sh
+++ test.sh
@@ -3,7 +3,4 @@
 CONF=$1
 PATH=./$CONF:$PWD/Release:$PATH
 
-for dll in `ls $CONF/*.Test.dll`
-do
-    ./nunit/nunit-console-x86.exe -domain=None $dll
-done
+./nunit/nunit-console-x86.exe -domain=None -process=Multiple $CONF/*.Test.dll
