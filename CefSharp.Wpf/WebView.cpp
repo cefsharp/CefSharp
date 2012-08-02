a27a31a90047949fe5a7f6d41c7ccce210e20a1a
Marcin Wisnicki <marcin.wisnicki@microgen.com>
2012-08-02 11:43:12 +0200

    Keep CefSharp types inside CefSharp assembly. Fixes ataranto#32.

--- CefSharp.Wpf/WebView.cpp
+++ CefSharp.Wpf/WebView.cpp
@@ -609,7 +609,7 @@ namespace Wpf
         CefString url = toNative(_browserCore->Address);
 
         CefBrowser::CreateBrowser(window, _clientAdapter.get(),
-            url, *_settings->_browserSettings);
+            url, *(CefBrowserSettings*)_settings->_internalBrowserSettings);
 
         Content = _image = gcnew Image();
 
