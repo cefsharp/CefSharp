9e828f39c2c7352cf92c42a43bc793799f4554dd
anthony taranto <anthony.taranto@gmail.com>
2012-08-06 17:37:10 -0700

    Improve wpf rendering - fixes font smudging when resizing
    Conflicts:
    
    	CefSharp.Wpf/WebView.cpp

--- CefSharp.Wpf/WebView.cpp
+++ CefSharp.Wpf/WebView.cpp
@@ -617,6 +617,7 @@ namespace Wpf
             url, *_settings->_browserSettings);
 
         Content = _image = gcnew Image();
+        RenderOptions::SetBitmapScalingMode(_image, BitmapScalingMode::NearestNeighbor);
 
         _popup = gcnew Popup();
         _popup->Child = _popupImage = gcnew Image();
