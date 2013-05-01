0c3115288f965dd19ed1b8243da78180020de49b
Per Lundberg <per.lundberg@ecraft.com>
2013-05-01 20:36:54 +0300

    WIP code, trying to figure out why we get a weird TypeLoadException...

--- CefSharp.Wpf.Example/MainWindow.xaml.cs
+++ CefSharp.Wpf.Example/MainWindow.xaml.cs
@@ -1,12 +1,17 @@
-﻿using CefSharp.Example;
-using System;
+﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Threading;
+using CefSharp.Example;
 
 namespace CefSharp.Wpf.Example
 {
-    public partial class MainWindow : Window, IExampleView
+    public partial class MainWindow : Window //, IExampleView
     {
         // file
         public event EventHandler ShowDevToolsActivated;
@@ -39,111 +44,116 @@ namespace CefSharp.Wpf.Example
         public event EventHandler BackActivated;
         public event EventHandler ForwardActivated;
 
-        private readonly IDictionary<object, EventHandler> handlers;
+        private IDictionary<object, EventHandler> handlers;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            var presenter = new ExamplePresenter(web_view, this,
-                invoke => Dispatcher.BeginInvoke(invoke));
+            //        var presenter = new ExamplePresenter(web_view, this,
+            //            invoke => Dispatcher.BeginInvoke(invoke));
 
-            handlers = new Dictionary<object, EventHandler>
-                    {
-                        // file
-                        { showDevToolsMenuItem, ShowDevToolsActivated},
-                        { closeDevToolsMenuItem, CloseDevToolsActivated},
-                        { exitMenuItem, ExitActivated },
+            //        handlers = new Dictionary<object, EventHandler>
+            //        {
+            //            // file
+            //            { showDevToolsMenuItem, ShowDevToolsActivated},
+            //            { closeDevToolsMenuItem, CloseDevToolsActivated},
+            //            { exitMenuItem, ExitActivated },
 
-                        // edit
-                        { undoMenuItem, UndoActivated },
-                        { redoMenuItem, RedoActivated },
-                        { cutMenuItem, CutActivated },
-                        { copyMenuItem, CopyActivated },
-                        { pasteMenuItem, PasteActivated },
-                        { deleteMenuItem, DeleteActivated },
-                        { selectAllMenuItem, SelectAllActivated },
+            //            // edit
+            //            { undoMenuItem, UndoActivated },
+            //            { redoMenuItem, RedoActivated },
+            //            { cutMenuItem, CutActivated },
+            //            { copyMenuItem, CopyActivated },
+            //            { pasteMenuItem, PasteActivated },
+            //            { deleteMenuItem, DeleteActivated },
+            //            { selectAllMenuItem, SelectAllActivated },
 
-                        // test
-                        { testResourceLoadMenuItem, TestResourceLoadActivated },
-                        { testSchemeLoadMenuItem, TestSchemeLoadActivated },
-                        { testExecuteScriptMenuItem, TestExecuteScriptActivated },
-                        { testEvaluateScriptMenuItem, TestEvaluateScriptActivated },
-                        { testBindMenuItem, TestBindActivated },
-                        { testConsoleMessageMenuItem, TestConsoleMessageActivated },
-                        { testTooltipMenuItem, TestTooltipActivated },
-                        { testPopupMenuItem, TestPopupActivated },
-                        { testLoadStringMenuItem, TestLoadStringActivated },
-                        { testCookieVisitorMenuItem, TestCookieVisitorActivated },
+            //            // test
+            //            { testResourceLoadMenuItem, TestResourceLoadActivated },
+            //            { testSchemeLoadMenuItem, TestSchemeLoadActivated },
+            //            { testExecuteScriptMenuItem, TestExecuteScriptActivated },
+            //            { testEvaluateScriptMenuItem, TestEvaluateScriptActivated },
+            //            { testBindMenuItem, TestBindActivated },
+            //            { testConsoleMessageMenuItem, TestConsoleMessageActivated },
+            //            { testTooltipMenuItem, TestTooltipActivated },
+            //            { testPopupMenuItem, TestPopupActivated },
+            //            { testLoadStringMenuItem, TestLoadStringActivated },
+            //            { testCookieVisitorMenuItem, TestCookieVisitorActivated },
 
-                        // navigation
-                        { backButton, BackActivated },
-                        { forwardButton, ForwardActivated },
-                    };
-        }
+            //            // navigation
+            //            { backButton, BackActivated },
+            //            { forwardButton, ForwardActivated },
+            //        };
+            //    }
 
-        public void SetTitle(string title)
-        {
-            Title = title;
-        }
+            //    public void SetTitle(string title)
+            //    {
+            //        Title = title;
+            //    }
 
-        public void SetAddress(string address)
-        {
-            urlTextBox.Text = address;
-        }
+            //    public void SetAddress(string address)
+            //    {
+            //        urlTextBox.Text = address;
+            //    }
 
-        public void SetCanGoBack(bool can_go_back)
-        {
-            backButton.IsEnabled = can_go_back;
-        }
+            //    public void SetCanGoBack(bool can_go_back)
+            //    {
+            //        backButton.IsEnabled = can_go_back;
+            //    }
 
-        public void SetCanGoForward(bool can_go_forward)
-        {
-            forwardButton.IsEnabled = can_go_forward;
-        }
+            //    public void SetCanGoForward(bool can_go_forward)
+            //    {
+            //        forwardButton.IsEnabled = can_go_forward;
+            //    }
 
-        public void SetIsLoading(bool is_loading)
-        {
+            //    public void SetIsLoading(bool is_loading)
+            //    {
 
-        }
+            //    }
 
-        public void ExecuteScript(string script)
-        {
-            web_view.ExecuteScript(script);
-        }
+            //    public void ExecuteScript(string script)
+            //    {
+            //        web_view.ExecuteScript(script);
+            //    }
 
-        public object EvaluateScript(string script)
-        {
-            return web_view.EvaluateScript(script);
-        }
+            //    public object EvaluateScript(string script)
+            //    {
+            //        return web_view.EvaluateScript(script);
+            //    }
 
-        public void DisplayOutput(string output)
-        {
-            outputLabel.Content = output;
-        }
+            //    public void DisplayOutput(string output)
+            //    {
+            //        outputLabel.Content = output;
+            //    }
 
-        private void control_Activated(object sender, RoutedEventArgs e)
-        {
-            EventHandler handler;
-            if (handlers.TryGetValue(sender, out handler) &&
-                handler != null)
-            {
-                handler(this, EventArgs.Empty);
-            }
-        }
+            //    private void control_Activated(object sender, RoutedEventArgs e)
+            //    {
+            //        EventHandler handler;
+            //        if (handlers.TryGetValue(sender, out handler) &&
+            //            handler != null)
+            //        {
+            //            handler(this, EventArgs.Empty);
+            //        }
+            //    }
 
-        private void urlTextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key != Key.Enter)
-            {
-                return;
-            }
+            //    private void urlTextBox_KeyDown(object sender, KeyEventArgs e)
+            //    {
+            //        if (e.Key != Key.Enter)
+            //        {
+            //            return;
+            //        }
 
-            var handler = UrlActivated;
-            if (handler != null)
-            {
-                handler(this, urlTextBox.Text);
-            }
+            //        var handler = UrlActivated;
+            //        if (handler != null)
+            //        {
+            //            handler(this, urlTextBox.Text);
+            //        }
+            //    }
+            //}
+
+            // This line throws a TypeLoadException for a yet unidentified reason...
+            var webView = new WebView();
         }
     }
 }
