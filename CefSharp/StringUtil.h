9a66f29fb32fd3ff3e7e8534c49e121c3d2d4146
Per Lundberg <per@halleluja.nu>
2013-05-09 23:33:07 +0300

    Added our first XML method comments, to improve the IntelliSense experience and general maintainability of the code.

--- CefSharp/StringUtil.h
+++ CefSharp/StringUtil.h
@@ -5,8 +5,27 @@ using namespace System;
 
 namespace CefSharp
 {
+	/// <summary>
+	///	Converts an unmanaged string to a (managed) .NET string.
+	/// </summary>
+	/// <param name="str">The string that should be converted.</param>
+	/// <returns>A .NET string.</returns>
     String^ toClr(const cef_string_t& cefStr);
-    String^ toClr(const CefString& cefStr);
+
+	/// <summary>
+	///	Converts an unmanaged string to a (managed) .NET string.
+	/// </summary>
+	/// <param name="str">The string that should be converted.</param>
+	/// <returns>A .NET string.</returns>
+	String^ toClr(const CefString& cefStr);
+
+	/// <summary>
+	///	Converts a .NET string to native (unamanged) format. Note that this method does not allocate a new copy of the string, but
+	/// rather returns a pointer to the memory in the existing managed String object.
+	/// </summary>
+	/// <param name="str">The string that should be converted.</param>
+	/// <returns>An unmanaged representation of the provided string.</returns>
     CefString toNative(String^ str);
-    void assignFromString(cef_string_t& cefStrT, String^ str);
+    
+	void assignFromString(cef_string_t& cefStrT, String^ str);
 }
--- CefSharp/TypeUtil.h
+++ CefSharp/TypeUtil.h
@@ -5,6 +5,17 @@
 
 namespace CefSharp
 {
+	/// <summary>
+	///	Converts a .NET object to an (unmanaged) Chromium V8 object.
+	/// </summary>
+	/// <param name="str">The .NET object that should be converted.</param>
+	/// <returns>A corresponding V8 value.</returns>
     CefRefPtr<CefV8Value> convertToCef(Object^ obj, Type^ type);
-    Object^ convertFromCef(CefRefPtr<CefV8Value> obj);
+
+	/// <summary>
+	///	Converts a Chromium V8 value to a (managed) .NET object.
+	/// </summary>
+	/// <param name="str">The V8 value that should be converted.</param>
+	/// <returns>A corresponding .NET object.</returns>
+	Object^ convertFromCef(CefRefPtr<CefV8Value> obj);
 }
