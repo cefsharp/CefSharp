13c876327c39ec078a6c5a2bea0b5c697bc83758
Per Lundberg <per.lundberg@ecraft.com>
2013-07-17 21:11:48 +0300

    Moved the StringUtil methods into the Internals folder & namespace (since that's where they really belong), and moved them into a class proper + fixed their naming to be true CamelCase, not camelCase. This is C++, not Java. :P

---
+++ CefSharp/Internals/StringUtil.cpp
@@ -0,0 +1,35 @@
+#include "Stdafx.h"
+
+namespace CefSharp
+{
+    namespace Internals
+    {
+        String^ StringUtil::ToClr(const cef_string_t& cefStr)
+        {
+            return gcnew String(cefStr.str);
+        }
+
+        String^ StringUtil::ToClr(const CefString& cefStr)
+        {
+            return gcnew String(cefStr.c_str());
+        }
+
+        CefString StringUtil::ToNative(String^ str)
+        {
+            pin_ptr<const wchar_t> pStr = PtrToStringChars(str);
+            CefString cefStr(pStr);
+            return cefStr;
+        }
+
+        void StringUtil::AssignFromString(cef_string_t& cefStr, String^ str)
+        {
+            cef_string_clear(&cefStr);
+
+            if (str != nullptr)
+            {
+                pin_ptr<const wchar_t> pStr = PtrToStringChars(str);
+                cef_string_copy(pStr, str->Length, &cefStr);
+            }
+        }
+    }
+}
---
+++ CefSharp/Internals/StringUtil.h
@@ -0,0 +1,43 @@
+#include "Stdafx.h"
+#pragma once
+
+using namespace System;
+
+namespace CefSharp
+{
+    namespace Internals
+    {
+        private class StringUtil
+        {
+        public:
+            /// <summary>
+            /// Converts an unmanaged string to a (managed) .NET string.
+            /// </summary>
+            /// <param name="cefStr">The string that should be converted.</param>
+            /// <returns>A .NET string.</returns>
+            static String^ ToClr(const cef_string_t& cefStr);
+
+            /// <summary>
+            /// Converts an unmanaged string to a (managed) .NET string.
+            /// </summary>
+            /// <param name="cefStr">The string that should be converted.</param>
+            /// <returns>A .NET string.</returns>
+            static String^ ToClr(const CefString& cefStr);
+
+            /// <summary>
+            /// Converts a .NET string to native (unmanaged) format. Note that this method does not allocate a new copy of the string, but
+            /// rather returns a pointer to the memory in the existing managed String object.
+            /// </summary>
+            /// <param name="str">The string that should be converted.</param>
+            /// <returns>An unmanaged representation of the provided string.</returns>
+            static CefString ToNative(String^ str);
+
+            /// <summary>
+            /// Assigns the provided cef_string_t object from the given .NET string.
+            /// </summary>
+            /// <param name="cefStr">The cef_string_t that should be updated.</param>
+            /// <param name="str">The .NET string whose value should be used to update cefStr.</param>
+            static void AssignFromString(cef_string_t& cefStr, String^ str);
+        };
+    }
+}
