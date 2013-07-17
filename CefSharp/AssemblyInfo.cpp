c8fb2c1da132e5ca2fff1da60f88242997386758
Per Lundberg <per.lundberg@ecraft.com>
2013-07-17 22:48:45 +0300

    Wrong version. :)

--- CefSharp/AssemblyInfo.cpp
+++ CefSharp/AssemblyInfo.cpp
@@ -11,7 +11,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyProduct("CefSharp")];
 [assembly:AssemblyCopyright("Copyright (c) The CefSharp Project 2010-2013")];
 
-[assembly:AssemblyVersion("3.27.2.*")];
+[assembly:AssemblyVersion("3.27.0.*")];
 [assembly:ComVisible(false)];
 [assembly:CLSCompliant(true)];
 [assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
