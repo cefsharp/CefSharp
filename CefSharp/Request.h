8ebf1091e2cf046e0842295ee0b499ac07e91e2d
Per Lundberg <per.lundberg@ecraft.com>
2013-07-18 21:14:11 +0300

    Added copyright header.

--- CefSharp/Request.h
+++ CefSharp/Request.h
@@ -1,6 +1,12 @@
+// Copyright © 2010-2013 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#pragma once
+
 #include "Stdafx.h"
 #include "IRequest.h"
-#pragma once
+#include "MCefRefPtr.h"
 
 using namespace System;
 using namespace System::Collections::Generic;
