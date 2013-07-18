a6244903a771dfa4290a75663a1e6fa0327efb40
Per Lundberg <per.lundberg@ecraft.com>
2013-07-18 22:58:56 +0300

    Moved to the CefSharp.Internals namespace, where it now should be.

--- CefSharp/Internals/CefRequestWrapper.cpp
+++ CefSharp/Internals/CefRequestWrapper.cpp
@@ -1,91 +1,97 @@
-#include "Stdafx.h"
+// Copyright © 2010-2013 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-#include "Request.h"
+#include "Stdafx.h"
+#include "CefRequestWrapper.h"
 
 namespace CefSharp
 {
-    String^ CefRequestWrapper::Url::get()
+    namespace Internals
     {
-        return toClr(_wrappedRequest->GetURL());
-    }
-
-    void CefRequestWrapper::Url::set(String^ url)
-    {
-        if (url == nullptr)
+        String^ CefRequestWrapper::Url::get()
         {
-            throw gcnew System::ArgumentException("cannot be null", "url");
+            return toClr(_wrappedRequest->GetURL());
         }
 
-        CefString str = toNative(url);
-        _wrappedRequest->SetURL(str);
-    }
-
-    String^ CefRequestWrapper::Method::get()
-    {
-        return toClr(_wrappedRequest->GetMethod());
-    }
-
-    String^ CefRequestWrapper::Body::get()
-    {
-        CefPostData::ElementVector ev;
-
-        CefRefPtr<CefPostData> data = _wrappedRequest->GetPostData();
-
-        if (data.get() != nullptr) 
+        void CefRequestWrapper::Url::set(String^ url)
         {
-            data.get()->GetElements(ev);
-
-            for (CefPostData::ElementVector::iterator it = ev.begin(); it != ev.end(); ++it)
+            if (url == nullptr)
             {
-                CefPostDataElement *el = it->get();
+                throw gcnew System::ArgumentException("cannot be null", "url");
+            }
 
-                if (el->GetType() == PDE_TYPE_BYTES) 
+            CefString str = toNative(url);
+            _wrappedRequest->SetURL(str);
+        }
+
+        String^ CefRequestWrapper::Method::get()
+        {
+            return toClr(_wrappedRequest->GetMethod());
+        }
+
+        String^ CefRequestWrapper::Body::get()
+        {
+            CefPostData::ElementVector ev;
+
+            CefRefPtr<CefPostData> data = _wrappedRequest->GetPostData();
+
+            if (data.get() != nullptr) 
+            {
+                data.get()->GetElements(ev);
+
+                for (CefPostData::ElementVector::iterator it = ev.begin(); it != ev.end(); ++it)
                 {
-                    size_t count = el->GetBytesCount();
-                    char* bytes = new char[count];
+                    CefPostDataElement *el = it->get();
 
-                    el->GetBytes(count, bytes);
+                    if (el->GetType() == PDE_TYPE_BYTES) 
+                    {
+                        size_t count = el->GetBytesCount();
+                        char* bytes = new char[count];
 
-                    return gcnew String(bytes, 0, count);
-                }
-                else if (el->GetType() == PDE_TYPE_FILE)
-                {
-                    return toClr(el->GetFile());
+                        el->GetBytes(count, bytes);
+
+                        return gcnew String(bytes, 0, count);
+                    }
+                    else if (el->GetType() == PDE_TYPE_FILE)
+                    {
+                        return toClr(el->GetFile());
+                    }
                 }
             }
+
+            return nullptr;
         }
 
-        return nullptr;
-    }
-
-    IDictionary<String^, String^>^ CefRequestWrapper::GetHeaders()
-    {
-        CefRequest::HeaderMap hm;
-        _wrappedRequest->GetHeaderMap(hm);
-
-        IDictionary<String^, String^>^ headers = gcnew Dictionary<String^, String^>();
-
-        for (CefRequest::HeaderMap::iterator it = hm.begin(); it != hm.end(); ++it)
+        IDictionary<String^, String^>^ CefRequestWrapper::GetHeaders()
         {
-            String^ name = toClr(it->first);
-            String^ value = toClr(it->second);
-            headers->Add(name, value);
+            CefRequest::HeaderMap hm;
+            _wrappedRequest->GetHeaderMap(hm);
+
+            IDictionary<String^, String^>^ headers = gcnew Dictionary<String^, String^>();
+
+            for (CefRequest::HeaderMap::iterator it = hm.begin(); it != hm.end(); ++it)
+            {
+                String^ name = toClr(it->first);
+                String^ value = toClr(it->second);
+                headers->Add(name, value);
+            }
+
+            return headers;
         }
 
-        return headers;
-    }
-
-    void CefRequestWrapper::SetHeaders(IDictionary<String^, String^>^ headers)
-    {
-        CefRequest::HeaderMap hm;
-
-        for each(KeyValuePair<String^, String^>^ pair in headers)
+        void CefRequestWrapper::SetHeaders(IDictionary<String^, String^>^ headers)
         {
-            CefString name = toNative(pair->Key);
-            CefString value = toNative(pair->Value);
-            hm.insert(std::make_pair(name, value));
-        }
+            CefRequest::HeaderMap hm;
 
-        _wrappedRequest->SetHeaderMap(hm);
+            for each(KeyValuePair<String^, String^>^ pair in headers)
+            {
+                CefString name = toNative(pair->Key);
+                CefString value = toNative(pair->Value);
+                hm.insert(std::make_pair(name, value));
+            }
+
+            _wrappedRequest->SetHeaderMap(hm);
+        }
     }
 }
--- CefSharp/Internals/CefRequestWrapper.h
+++ CefSharp/Internals/CefRequestWrapper.h
@@ -13,18 +13,21 @@ using namespace System::Collections::Generic;
 
 namespace CefSharp
 {
-    ref class CefRequestWrapper : public IRequest
+    namespace Internals
     {
-        MCefRefPtr<CefRequest> _wrappedRequest;
-    internal:
-        CefRequestWrapper(CefRefPtr<CefRequest> cefRequest) : _wrappedRequest(cefRequest) {}
+        ref class CefRequestWrapper : public IRequest
+        {
+            MCefRefPtr<CefRequest> _wrappedRequest;
+        internal:
+            CefRequestWrapper(CefRefPtr<CefRequest> cefRequest) : _wrappedRequest(cefRequest) {}
 
-    public:
-        virtual property String^ Url { String^ get(); void set(String^ url); }
-        virtual property String^ Method { String^ get(); }
-        virtual property String^ Body { String^ get(); }
-        virtual IDictionary<String^, String^>^ GetHeaders();
-        virtual void SetHeaders(IDictionary<String^, String^>^ headers);
+        public:
+            virtual property String^ Url { String^ get(); void set(String^ url); }
+            virtual property String^ Method { String^ get(); }
+            virtual property String^ Body { String^ get(); }
+            virtual IDictionary<String^, String^>^ GetHeaders();
+            virtual void SetHeaders(IDictionary<String^, String^>^ headers);
 
-    };
+        };
+    }
 }
