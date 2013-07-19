9af257c046259957ec4c06e0c3a20691a279731c
Per Lundberg <per.lundberg@ecraft.com>
2013-07-19 14:37:26 +0300

    Updated with new file header.

--- CefSharp/CookieVisitor.cpp
+++ CefSharp/CookieVisitor.cpp
@@ -1,5 +1,8 @@
-#include "Stdafx.h"
+// Copyright © 2010-2013 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+#include "Stdafx.h"
 #include "CookieVisitor.h"
 
 namespace CefSharp
