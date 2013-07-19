2d8c0ec5d0a7f74c6f5e31799dda731d4de15b24
Per Lundberg <per.lundberg@ecraft.com>
2013-07-19 14:33:02 +0300

    Added dummy CefApp-based class. It's quite likely that we will have to implement more stuff here eventually… :)

---
+++ CefSharp/Internals/CefSharpApp.h
@@ -0,0 +1,10 @@
+// Copyright © 2010-2013 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#include "include/cef_app.h"
+
+public class CefSharpApp : public CefApp
+{
+    IMPLEMENT_REFCOUNTING(CefSharpApp)
+};
