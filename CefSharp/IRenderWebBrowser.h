14695ea3a7b9750e15ad5261ab199a9f8ecec90b
Per Lundberg <per.lundberg@ecraft.com>
2013-07-22 13:04:42 +0300

    Added copyright header and got rid of explicit public for interface.

--- CefSharp/IRenderWebBrowser.h
+++ CefSharp/IRenderWebBrowser.h
@@ -1,13 +1,16 @@
-#include "Stdafx.h"
+// Copyright © 2010-2013 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 #pragma once
 
+#include "Stdafx.h"
 #include "IWebBrowser.h"
 
 namespace CefSharp
 {
     public interface class IRenderWebBrowser : IWebBrowser
     {
-    public:
         void SetCursor(IntPtr cursor);
         void SetBuffer(int width, int height, const void* buffer);
 
