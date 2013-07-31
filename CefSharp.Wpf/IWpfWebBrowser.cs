f0c8f71ff326d728ef8ca247ff420ad27aeb44cc
Per Lundberg <per.lundberg@ecraft.com>
2013-07-31 08:40:51 +0300

    Added XML comments.

--- CefSharp.Wpf/IWpfWebBrowser.cs
+++ CefSharp.Wpf/IWpfWebBrowser.cs
@@ -4,7 +4,16 @@ namespace CefSharp.Wpf
 {
     public interface IWpfWebBrowser : IWebBrowser
     {
+        /// <summary>
+        /// Command which navigates to the previous page in the browser history. Will automatically be enabled/disabled depending
+        /// on the availability of the command.
+        /// </summary>
         ICommand BackCommand { get; }
+
+        /// <summary>
+        /// Command which navigates to the next page in the browser history. Will automatically be enabled/disabled depending on
+        /// the availability of the command.
+        /// </summary>
         ICommand ForwardCommand { get; }
     }
 }
