6c3a7d0304e8a07959213c54dc5872a14090f439
Per Lundberg <per.lundberg@ecraft.com>
2013-07-31 08:42:35 +0300

    Let's make this property be available also, so we can document it properly.

--- CefSharp.Wpf/IWpfWebBrowser.cs
+++ CefSharp.Wpf/IWpfWebBrowser.cs
@@ -5,6 +5,13 @@ namespace CefSharp.Wpf
     public interface IWpfWebBrowser : IWebBrowser
     {
         /// <summary>
+        /// The address (URL) which the browser control is currently displaying. Can be set to a simplified URL
+        /// (e.g. www.google.com) or a full URL (e.g. http://www.google.com). Will automatically be updated as the user
+        /// navigates to another page (e.g. by clickig on a link).
+        /// </summary>
+        string Address { get; set; }
+
+        /// <summary>
         /// Command which navigates to the previous page in the browser history. Will automatically be enabled/disabled depending
         /// on the availability of the command.
         /// </summary>
