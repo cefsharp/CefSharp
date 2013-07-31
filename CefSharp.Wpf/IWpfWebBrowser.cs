43b835cc08ee3d659b22ab1059077036a9768904
Per Lundberg <per.lundberg@ecraft.com>
2013-07-31 08:58:48 +0300

    Added the TItle here also, for documentary purposes. We should consider creating online documentation from our classes when their level of information is of a more decent level.

--- CefSharp.Wpf/IWpfWebBrowser.cs
+++ CefSharp.Wpf/IWpfWebBrowser.cs
@@ -1,15 +1,23 @@
-﻿using System.Windows.Input;
+﻿using System.ComponentModel;
+using System.Windows.Input;
 
 namespace CefSharp.Wpf
 {
-    public interface IWpfWebBrowser : IWebBrowser
+    public interface IWpfWebBrowser : IWebBrowser, INotifyPropertyChanged
     {
         /// <summary>
         /// The address (URL) which the browser control is currently displaying. Can be set to a simplified URL
         /// (e.g. www.google.com) or a full URL (e.g. http://www.google.com). Will automatically be updated as the user
         /// navigates to another page (e.g. by clickig on a link).
         /// </summary>
+        /// <remarks>This property is a Dependency Property and fully supports data binding.</remarks>
         string Address { get; set; }
+
+        /// <summary>
+        /// The title of the web page being currently displayed.
+        /// </summary>
+        /// <remarks>This property is a Dependency Property and fully supports data binding.</remarks>
+        string Title { get; }
 
         /// <summary>
         /// Command which navigates to the previous page in the browser history. Will automatically be enabled/disabled depending
