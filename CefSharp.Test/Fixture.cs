b1e37c459dc85955377c1d9b2bb1a5ea5f85ecb7
Per Lundberg <per.lundberg@ecraft.com>
2013-08-03 23:45:32 +0300

    Commented out for now, so the project compiles.

--- CefSharp.Test/Fixture.cs
+++ CefSharp.Test/Fixture.cs
@@ -1,54 +1,51 @@
 ﻿using System.Threading;
-using System.Windows.Forms;
 using NUnit.Framework;
-using CefSharp.WinForms;
 
 namespace CefSharp.Test
 {
     [SetUpFixture]
     public class Fixture
     {
-        public static WebView Browser { get; private set; }
+        //public static WebView Browser { get; private set; }
 
         private ManualResetEvent createdEvent = new ManualResetEvent(false);
 
         [SetUp]
         public void SetUp()
         {
-            var settings = new Settings();
-            if (!CEF.Initialize(settings))
-            {
-                Assert.Fail();
-            }
+            //var settings = new Settings();
 
-            var thread = new Thread(() =>
-            {
-                var form = new Form();
-                form.Shown += (sender, e) =>
-                {
-                    Browser = new WebView
-                    {
-                        Parent = form,
-                        Dock = DockStyle.Fill,
-                    };
+            //var cef = new Cef();
+            //cef.Initialize(settings);
 
-                    createdEvent.Set();
-                };
+            //    var thread = new Thread(() =>
+            //    {
+            //        var form = new Form();
+            //        form.Shown += (sender, e) =>
+            //        {
+            //            Browser = new WebView
+            //            {
+            //                Parent = form,
+            //                Dock = DockStyle.Fill,
+            //            };
 
-                Application.Run(form);
-            });
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
+            //            createdEvent.Set();
+            //        };
 
-            createdEvent.WaitOne();
+            //        Application.Run(form);
+            //    });
+            //    thread.SetApartmentState(ApartmentState.STA);
+            //    thread.Start();
+
+            //    createdEvent.WaitOne();
         }
 
         [TearDown]
         public void TearDown()
         {
-            createdEvent.WaitOne();
-            CEF.Shutdown();
-            Application.Exit();
+            //createdEvent.WaitOne();
+            //CEF.Shutdown();
+            //Application.Exit();
         }
     }
 }
