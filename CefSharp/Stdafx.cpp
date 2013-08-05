6ae41303df86a240c1eebdd3daeeec5e6a4d50df
Per Lundberg <per.lundberg@ecraft.com>
2013-08-05 11:50:08 +0300

    Added standard copyright header.

--- CefSharp/Stdafx.cpp
+++ CefSharp/Stdafx.cpp
@@ -1,1 +1,5 @@
+// Copyright © 2010-2013 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 #include "Stdafx.h"
--- CefSharp/TypeUtil.h
+++ CefSharp/TypeUtil.h
@@ -1,6 +1,10 @@
-#include "Stdafx.h"
+// Copyright © 2010-2013 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 #pragma once
 
+#include "Stdafx.h"
 #include "include/cef_v8.h"
 
 namespace CefSharp
