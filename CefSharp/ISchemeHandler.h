76f671f2216197e00424a490b2518a7444bb24ba
Per Lundberg <per.lundberg@ecraft.com>
2013-09-04 13:50:28 +0300

    Renamed an argument and added more XML documentation about all of them.

--- CefSharp/ISchemeHandler.h
+++ CefSharp/ISchemeHandler.h
@@ -12,7 +12,12 @@ namespace CefSharp
         /// Processes a custom scheme-based request asynchronously. The implementing method should call the callback whenever the
         /// request is completed.
         /// </summary>
+        /// <param name="request">The request object.</param>
+        /// <param name="response">The SchemeHandlerResponse object in which the handler is supposed to place the response
+        /// information.</param>
+        /// <param name="requestCompletedCallback">A callback which the handler is supposed to call once the processing is
+        /// complete. The callback may be called on any thread.</param>
         /// <returns>true if the request is handled, false otherwise.</returns>
-        bool ProcessRequestAsync(IRequest^ request, SchemeHandlerResponse^ response, OnRequestCompletedHandler^ callback);
+        bool ProcessRequestAsync(IRequest^ request, SchemeHandlerResponse^ response, OnRequestCompletedHandler^ requestCompletedCallback);
     };
 }
