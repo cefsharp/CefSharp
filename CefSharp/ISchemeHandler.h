c1e2dc32743d44e94de995eca2ca4cc669b8d9b4
Per Lundberg <per.lundberg@ecraft.com>
2013-10-02 09:27:57 +0300

    Added standard file header.

--- CefSharp/ISchemeHandler.h
+++ CefSharp/ISchemeHandler.h
@@ -1,3 +1,7 @@
+// Copyright © 2010-2013 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 #include "Stdafx.h"
 #include "Request.h"
 
