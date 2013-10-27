b07deda22e9f45988c8f4be04c6eaec9c20f057d
Per Lundberg <per.lundberg@ecraft.com>
2013-10-27 10:53:17 +0200

    Started some of the fun work on the "home" page, based heavily on Twitter Bootstrap's "general usage" page (which is liberally licenses, to our benefit). Still a lot of work to be done, and the unfinished parts are obviously hard to document, but it's a good start. I think we could also publish this up using GitHub Pages eventually.

--- CefSharp.Example/CefSharpSchemeHandler.cs
+++ CefSharp.Example/CefSharpSchemeHandler.cs
@@ -14,7 +14,16 @@ namespace CefSharp.Example
         {
             resources = new Dictionary<string, string>
             {
-                { "/", Resources.Home },
+                { "/home", Resources.home_html },
+
+                { "/assets/css/shCore.css", Resources.assets_css_shCore_css },
+                { "/assets/css/shCoreDefault.css", Resources.assets_css_shCoreDefault_css },
+                { "/assets/css/docs.css", Resources.assets_css_docs_css },
+                { "/assets/js/application.js", Resources.assets_js_application_js },
+                { "/assets/js/jquery.js", Resources.assets_js_jquery_js },
+                { "/assets/js/shBrushCSharp.js", Resources.assets_js_shBrushCSharp_js },
+                { "/assets/js/shCore.js", Resources.assets_js_shCore_js },
+
                 { "/bootstrap/bootstrap-theme.min.css", Resources.bootstrap_theme_min_css },
                 { "/bootstrap/bootstrap.min.css", Resources.bootstrap_min_css },
                 { "/bootstrap/bootstrap.min.js", Resources.bootstrap_min_js },
@@ -28,6 +37,7 @@ namespace CefSharp.Example
 
         public bool ProcessRequestAsync(IRequest request, SchemeHandlerResponse response, OnRequestCompletedHandler requestCompletedCallback)
         {
+            // The 'host' portion is entirely ignored by this scheme handler.
             var uri = new Uri(request.Url);
             var fileName = uri.AbsolutePath;
 
