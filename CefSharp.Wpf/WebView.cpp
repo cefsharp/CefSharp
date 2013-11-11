39d091b593139c61e9eaab56f54452e4d14c7a22
Z.Shen <shenzheng@54sh.com>
2013-11-11 14:05:46 +0800

    handle chinese ime input text

--- CefSharp.Wpf/WebView.cpp
+++ CefSharp.Wpf/WebView.cpp
@@ -201,7 +201,26 @@ namespace CefSharp
                 e->Handled = true;
             }
         }
+        
+        void WebView::OnPreviewTextInput(TextCompositionEventArgs^ e)
+		{
+			CefRefPtr<CefBrowser> browser;
+			if (!TryGetCefBrowser(browser))
+			{
+				return;
+			}
 
+			CefBrowser::KeyType type;
+			for (int i = 0;i<e->Text->Length;i++)
+			{
+				CefKeyInfo keyInfo;
+				keyInfo.key =(int)e->Text[i];
+				type = KT_CHAR; 
+				browser->SendKeyEvent(type, keyInfo, 0);
+			}
+			e->Handled = true;
+		}
+		
         void WebView::OnMouseButton(MouseButtonEventArgs^ e)
         {
             CefRefPtr<CefBrowser> browser;
