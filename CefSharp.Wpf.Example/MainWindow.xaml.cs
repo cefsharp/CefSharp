f06ab5bd821760f3faf872729902c159a3e318a6
Per Lundberg <per.lundberg@ecraft.com>
2013-11-13 17:51:53 +0200

    Ah. There’s nothing more awesome than being able to delete a bunch of old code. :)

--- CefSharp.Wpf.Example/MainWindow.xaml.cs
+++ CefSharp.Wpf.Example/MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using CefSharp.Wpf.Example.Views.Main;
 
 namespace CefSharp.Wpf.Example
 {
-    public partial class MainWindow : Window //, IExampleView
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
@@ -18,49 +18,3 @@ namespace CefSharp.Wpf.Example
         }
     }
 }
-
-//        public void SetTitle(string title)
-        //        {
-        //            Title = title;
-        //        }
-
-        //        public void SetAddress(string address)
-        //        {
-        //            urlTextBox.Text = address;
-        //        }
-
-        //        public void SetAddress(Uri uri)
-        //        {
-        //            urlTextBox.Text = uri.ToString();
-        //        }
-
-        //        public void SetCanGoBack(bool can_go_back)
-        //        {
-        //            backButton.IsEnabled = can_go_back;
-        //        }
-
-        //        public void SetCanGoForward(bool can_go_forward)
-        //        {
-        //            forwardButton.IsEnabled = can_go_forward;
-        //        }
-
-        //        public void SetIsLoading(bool is_loading)
-        //        {
-
-        //        }
-
-        //        public void ExecuteScript(string script)
-        //        {
-        //            webView.ExecuteScript(script);
-        //        }
-
-        //        public object EvaluateScript(string script)
-        //        {
-        //            return webView.EvaluateScript(script);
-        //        }
-
-        //        public void DisplayOutput(string output)
-        //        {
-        //            outputLabel.Content = output;
-        //        }
-        //    }
