da81a98443fa0ec278c7c080759efeaf042af7d0
Per Lundberg <per.lundberg@ecraft.com>
2013-11-13 22:56:07 +0200

    Moved the PropertyChangedExtensionMethods class, since it will be used from the WinForms example also.

--- CefSharp.Example/ExtensionMethods/PropertyChangedExtensionMethods.cs
+++ CefSharp.Example/ExtensionMethods/PropertyChangedExtensionMethods.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq.Expressions;
 
-namespace CefSharp.Wpf.Example.Mvvm
+namespace CefSharp.Example
 {
     public static class PropertyChangedExtensionMethods
     {
