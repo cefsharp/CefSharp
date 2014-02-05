532437fb500fa8992cb5460494ac9ef301237b1f
Per Lundberg <per.lundberg@ecraft.com>
2014-02-05 10:13:59 +0200

    [Cherry-pick me] Don't include *.diff files in the .zip release.

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -9r CefSharp.zip * -x Debug/\* -x \*/obj/* -x \*/bin/* -x *.DS_Store -x *.*sdf -x *.suo -x *.zip -x *.nupkg -x ipch/\* -x _ReSharper\*/*
+zip -9r CefSharp.zip * -x Debug/\* -x \*/obj/* -x \*/bin/* -x *.DS_Store -x *.*sdf -x *.suo -x *.zip -x *.nupkg -x *.diff -x ipch/\* -x _ReSharper\*/*
