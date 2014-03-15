f5976430168b75970995f5bd5a679c9790d79afd
Per Lundberg <per.lundberg@ecraft.com>
2014-03-15 22:07:54 +0200

    Refactored Jan's comment about why this seemingly more complex is needed (and yes, it is really. :)

--- CefSharp.BrowserSubprocess/JavascriptServiceHost.cs
+++ CefSharp.BrowserSubprocess/JavascriptServiceHost.cs
@@ -10,29 +10,27 @@ namespace CefSharp.BrowserSubprocess
     {
         public static ServiceHost Create(int parentProcessId, int browserId)
         {
+            // The setup of the WCF host here is a bit non-trivial because we must support multiple subprocesses (when there is
+            // more than one WebView in the application, for example). Inspired by this SO post:
+            // http://stackoverflow.com/questions/10362246/two-unique-named-pipes-conflicting-and-invalidcredentialexception
             var host = new ServiceHost(typeof(JavascriptProxy), new Uri[0]);
             AddDebugBehavior(host);
 
-            var serviceName = JavascriptProxySupport.GetServiceName(parentProcessId, browserId);
-            
-            //use absultadress for hosting 
-            //http://stackoverflow.com/questions/10362246/two-unique-named-pipes-conflicting-and-invalidcredentialexception
-
-            serviceName = JavascriptProxySupport.BaseAddress + "/" + serviceName;
+            var serviceName = JavascriptProxySupport.BaseAddress + "/" + JavascriptProxySupport.GetServiceName(parentProcessId, browserId);
 
             KillExistingServiceIfNeeded(serviceName);
-            
+
             Kernel32.OutputDebugString("Setting up IJavascriptProxy using service name: " + serviceName);
             host.AddServiceEndpoint(
                 typeof(IJavascriptProxy),
                 new NetNamedPipeBinding(),
-                new Uri( serviceName )
+                new Uri(serviceName)
             );
-            
+
             host.Open();
             return host;
         }
-        
+
         private static void KillExistingServiceIfNeeded(string serviceName)
         {
             // It might be that there is an existing process already bound to this port. We must get rid of that one, so that the
@@ -41,7 +39,7 @@ namespace CefSharp.BrowserSubprocess
             {
                 var channelFactory = new ChannelFactory<IJavascriptProxy>(
                     new NetNamedPipeBinding(),
-                    new EndpointAddress( serviceName )
+                    new EndpointAddress(serviceName)
                     );
                 channelFactory.Open(TimeSpan.FromSeconds(1));
                 var javascriptProxy = channelFactory.CreateChannel();
