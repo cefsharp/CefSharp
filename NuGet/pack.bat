11567eed63a05d8ceb701b801894dd49c2760ac1
jornh <jornhansen@gmail.com>
2014-03-28 17:57:29 +0100

    Good enough that `CS.MinEx` want's to build ... This is what goes into the `CefSharp.Common` and `.Wpf` Nugets version 3.31.0.pre-1

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,4 +1,4 @@
-set version=3.31.0-pre0
+set version=3.31.0-pre1
 NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version%
 NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version%
 NuGet pack CefSharp.WinForms.nuspec -NoPackageAnalysis -Version %version%
