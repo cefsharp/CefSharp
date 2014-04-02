0a439c1a7e5bad32c2ce5f703bbe95cefb3bee80
Per Lundberg <per.lundberg@ecraft.com>
2014-04-02 18:05:02 +0100

    Removed the <value> stuff. I don't really think we need it, it just clutters the class and doesn't really add that much value.

--- CefSharp/Internals/JavaScriptMemberDescription.cs
+++ CefSharp/Internals/JavaScriptMemberDescription.cs
@@ -9,9 +9,6 @@ namespace CefSharp.Internals
         /// <summary>
         /// Identifies the <see cref="JavaScriptMemberDescription" /> for BrowserProcess to RenderProcess communication
         /// </summary>
-        /// <value>
-        /// The identifier.
-        /// </value>
         [DataMember]
         public long Id { get; set; }
 
@@ -19,46 +16,31 @@ namespace CefSharp.Internals
         /// Gets or sets the name of the managed property.
         /// May be null as <see cref="JavaScriptObject" />s may have values attached that are not present in the managed object
         /// </summary>
-        /// <value>
-        /// The name of the managed property.
-        /// </value>
         [DataMember]
         public string ManagedName { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the JavaScriptProperty.
         /// </summary>
-        /// <value>
-        /// The name of the JavaScriptProperty
-        /// </value>
         [DataMember]
         public string JSName { get; set; }
 
-        // the following members are used to cache reflection code to improve performance
+        // The following members are used to cache reflection code to improve performance
 
         /// <summary>
         /// Gets or sets the set value Action which is used to set the property / field value in the managed Object.
         /// </summary>
-        /// <value>
-        /// The set value.
-        /// </value>
         public Action<object, object> SetValue { get; set; }
 
         /// <summary>
         /// Gets or sets the get value Function which is used to get the property / field value from the managed Object.
         /// </summary>
-        /// <value>
-        /// The get value.
-        /// </value>
         public Func<object, object> GetValue { get; set; }
 
         /// <summary>
         /// Gets or sets the function that is called if the Member is a Function.
         /// Will be null if the Member is no function.
         /// </summary>
-        /// <value>
-        /// The function.
-        /// </value>
         public Delegate Function { get; set; }
     }
 }
