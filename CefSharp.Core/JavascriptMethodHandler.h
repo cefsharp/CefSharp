78015cb04944d0cf860ed1c8dd915d92d5911517
Per Lundberg <per.lundberg@ecraft.com>
2014-04-08 17:47:22 +0300

    Fixed wrong class name.

--- CefSharp.Core/JavascriptMethodHandler.h
+++ CefSharp.Core/JavascriptMethodHandler.h
@@ -6,7 +6,7 @@
 
 namespace CefSharp
 {
-    private class BindingHandler : public CefV8Handler
+    private class JavascriptMethodHandler : public CefV8Handler
     {
         virtual bool Execute(const CefString& name, CefRefPtr<CefV8Value> object, const CefV8ValueList& arguments, CefRefPtr<CefV8Value>& retval, CefString& exception)
         {
