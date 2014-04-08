c4e56764e571bbd28abbf5fb7ba464fd995a0952
Per Lundberg <per.lundberg@ecraft.com>
2014-04-08 19:00:42 +0200

    Slightly unrelated change, but here goes: Converted the CefEventFlags class to C#.

--- CefSharp.Core/MouseButtonType.h
+++ CefSharp.Core/MouseButtonType.h
@@ -12,33 +12,4 @@ namespace CefSharp
         Middle = CefBrowserHost::MouseButtonType::MBT_MIDDLE,
         Right = CefBrowserHost::MouseButtonType::MBT_RIGHT
     };
-
-    /// <summary>
-    /// Supported event bit flags.
-    /// </summary>
-    [FlagsAttribute]
-    public enum class CefEventFlags : UInt32
-    {
-        None = 0,
-
-        CapsLockOn = 1 << 0,
-
-        ShiftDown = 1 << 1,
-        ControlDown = 1 << 2,
-        AltDown = 1 << 3,
-
-        LeftMouseButton = 1 << 4,
-        MiddleMouseButton = 1 << 5,
-        RightMouseButton = 1 << 6,
-
-        /// <summary>
-        /// Mac OS-X command key.
-        /// </summary>
-        CommandDown = 1 << 7,
-
-        NumLockOn = 1 << 8,
-        IsKeyPad = 1 << 9,
-        IsLeft = 1 << 10,
-        IsRight = 1 << 11,
-    };
 }
