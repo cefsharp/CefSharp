f2e0b12dba674eb9b06a3a90d8a2606645f0957c
Per Lundberg <per.lundberg@ecraft.com>
2014-04-08 18:36:16 +0100

    Let's try doing something new and fancy... We move out everything Subprocess-related from CefSharp.Core, so that we try and better adhere to the "separation of concerns" design philosophy. Arguably, the work that the subprocess does it *quite different* to what the parent process does. Also, it forces you to actually remember this distinction, which might be a good idea anyway (since the availability of the CEF API:s differ greatly between the processes), so I think this is kind of a Good Thing<tm> actually.

---
+++ CefSharp.BrowserSubprocess.Core/AssemblyInfo.cpp
@@ -0,0 +1,23 @@
+#include "Stdafx.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+using namespace CefSharp;
+
+//[assembly:AssemblyTitle("CefSharpBrowserSubprocess.Core")];
+//[assembly:AssemblyCompany(AssemblyInfo::AssemblyCompany)];
+//[assembly:AssemblyProduct(AssemblyInfo::AssemblyProduct)];
+//[assembly:AssemblyCopyright(AssemblyInfo::AssemblyCopyright)];
+
+[assembly:AssemblyVersion(AssemblyInfo::AssemblyVersion)];
+[assembly:ComVisible(AssemblyInfo::ComVisible)];
+[assembly:CLSCompliant(AssemblyInfo::ClsCompliant)];
+[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
+
+[assembly:AssemblyDescription("")];
+[assembly:AssemblyConfiguration("")];
+[assembly:AssemblyTrademark("")];
+[assembly:AssemblyCulture("")];
