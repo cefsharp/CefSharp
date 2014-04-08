1974dda75dd02fe6c21f5cd2ff0bad8e782f98f9
Per Lundberg <per.lundberg@ecraft.com>
2014-04-08 17:07:44 +0200

    Refactored so that the whole creation of the ServiceHost is taken care of by one single method.

--- CefSharp.BrowserSubprocess/SubprocessServiceHost.cs
+++ CefSharp.BrowserSubprocess/SubprocessServiceHost.cs
@@ -16,11 +16,9 @@ namespace CefSharp.BrowserSubprocess
 
         public static SubprocessServiceHost Create(int parentProcessId, int browserId)
         {
-            var host = new SubprocessServiceHost();
-            AddDebugBehavior(host);
+            var host = CreateServiceHost();
 
             var serviceName = SubprocessProxySupport.GetServiceName(parentProcessId, browserId);
-
             host.KillExistingServiceIfNeeded(serviceName);
 
             host.AddServiceEndpoint(
@@ -33,9 +31,9 @@ namespace CefSharp.BrowserSubprocess
             return host;
         }
 
-
-        private static void AddDebugBehavior(ServiceHostBase host)
+        private static SubprocessServiceHost CreateServiceHost()
         {
+            var host = new SubprocessServiceHost();
             var serviceDebugBehavior = host.Description.Behaviors.Find<ServiceDebugBehavior>();
 
             if (serviceDebugBehavior == null)
@@ -50,15 +48,16 @@ namespace CefSharp.BrowserSubprocess
             {
                 serviceDebugBehavior.IncludeExceptionDetailInFaults = true;
             }
-        }
 
+            return host;
+        }
 
         private void KillExistingServiceIfNeeded(string serviceName)
         {
             // It might be that there is an existing process already bound to this port. We must get rid of that one, so that the
             // endpoint address gets available for us to use.
             try
-        {
+            {
                 var javascriptProxy = SubprocessProxySupport.CreateSubprocessProxyClient(serviceName, this, TimeSpan.FromSeconds(1));
                 javascriptProxy.Terminate();
             }
