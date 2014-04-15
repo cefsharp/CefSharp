5a9790c0764e17af150a50377ce709ec711ea1aa
Per Lundberg <per.lundberg@ecraft.com>
2014-04-15 22:46:25 +0300

    Fixed copyright to indicate that we are now on the 2014 side of things.

--- CefSharp.Wpf.Example/Properties/AssemblyInfo.cs
+++ CefSharp.Wpf.Example/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CefSharp.Wpf.Example")]
 [assembly: AssemblyCompany("The CefSharp Project")]
 [assembly: AssemblyProduct("CefSharp")]
-[assembly: AssemblyCopyright("Copyright © The CefSharp Authors 2010-2013")]
+[assembly: AssemblyCopyright("Copyright © The CefSharp Authors 2010-2014")]
 
 [assembly: AssemblyVersion("3.29.0.0")]
 [assembly: ComVisible(false)]
