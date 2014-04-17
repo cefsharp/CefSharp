389e2de9695ccdd5cb41d2384affbbab092c8422
amaitland <alex@maitlandcomputing.com.au>
2014-04-17 11:53:06 +1000

    Add license disclaimer Remove an unused reference in MainWindow.xaml.cs Fix comment for OnPropertyChanged event

--- CefSharp.Wpf.Example/Views/Main/MainView.xaml.cs
+++ CefSharp.Wpf.Example/Views/Main/MainView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace CefSharp.Wpf.Example.Views.Main
