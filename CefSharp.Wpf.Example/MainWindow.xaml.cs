c50dca10ebbe7bb6ff9962360cc0dc58c2ea54cc
amaitland <alex@maitlandcomputing.com.au>
2014-05-02 11:06:27 +1000

    Add Refresh, Force Refresh and FocusAddress KeyBindings

--- CefSharp.Wpf.Example/MainWindow.xaml.cs
+++ CefSharp.Wpf.Example/MainWindow.xaml.cs
@@ -2,11 +2,13 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+using System;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using CefSharp.Example;
 using CefSharp.Wpf.Example.Controls;
+using CefSharp.Wpf.Example.Mvvm;
 using CefSharp.Wpf.Example.ViewModels;
 using CefSharp.Wpf.Example.Views;
 
@@ -27,8 +29,28 @@ namespace CefSharp.Wpf.Example
 
 			CommandBindings.Add(new CommandBinding(ApplicationCommands.New, OpenNewTab));
 			CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, CloseTab));
+			CommandBindings.Add(new CommandBinding(NavigationCommands.Refresh, ReloadTab));
+			CommandBindings.Add(new CommandBinding(CefBrowserRoutedCommands.FocusAddress, FocusAddress));
 
 			Loaded += MainWindowLoaded;
+		}
+
+		//TODO: This is a bit of a hack - ideally would like to find a WPF/RoutedEvent method of passing the focus command
+		// Previously had the Command binding captured in the BrowserTabView which only works if it has focus - this will work in all cases
+		private void FocusAddress(object sender, ExecutedRoutedEventArgs e)
+		{
+			var view = GetCurrentView();
+			view.FocusAddress();
+		}
+
+		//TODO: Look at using routed commands, this should probably be handled in the View Directly
+		private void ReloadTab(object sender, ExecutedRoutedEventArgs e)
+		{
+			var view = GetCurrentView();
+
+			var forceReload = e.Parameter != null && string.Compare(e.Parameter.ToString(), "Force", StringComparison.OrdinalIgnoreCase) == 0;
+
+			view.Reload(forceReload);
 		}
 
 		private void CloseTab(object sender, ExecutedRoutedEventArgs e)
