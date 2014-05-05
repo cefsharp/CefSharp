6ceb2f72136e16d72d782356f4dbc3be53579e5b
JanEggers <jan.Eggers@ferag.com>
2014-05-05 18:55:50 +0200

    fixed javascript spelling

--- CefSharp/IWebBrowser.cs
+++ CefSharp/IWebBrowser.cs
@@ -13,7 +13,7 @@ namespace CefSharp
     public interface IWebBrowser
     {
         /// <summary>
-        /// Event handler for receiving JavaScript console messages being sent from web pages.
+        /// Event handler for receiving Javascript console messages being sent from web pages.
         /// </summary>
         event ConsoleMessageEventHandler ConsoleMessage;
 
@@ -37,24 +37,24 @@ namespace CefSharp
         void LoadHtml(string html, string url);
 
         /// <summary>
-        /// Registers a JavaScript object in this specific browser instance.
+        /// Registers a Javascript object in this specific browser instance.
         /// </summary>
         /// <param name="name">The name of the object. (e.g. "foo", if you want the object to be accessible as window.foo).</param>
-        /// <param name="objectToBind">The object to be made accessible to JavaScript.</param>
+        /// <param name="objectToBind">The object to be made accessible to Javascript.</param>
         void RegisterJsObject(string name, object objectToBind);
 
         /// <summary>
-        /// Execute some JavaScript code in the context of this WebBrowser. As the method name implies, the script will be
+        /// Execute some Javascript code in the context of this WebBrowser. As the method name implies, the script will be
         /// executed asynchronously, and the method therefore returns before the script has actually been executed.
         /// </summary>
-        /// <param name="script">The JavaScript code that should be executed.</param>
+        /// <param name="script">The Javascript code that should be executed.</param>
         void ExecuteScriptAsync(string script);
 
         /// <summary>
-        /// Execute some JavaScript code in the context of this WebBrowser, and return the result of the evaluation.
+        /// Execute some Javascript code in the context of this WebBrowser, and return the result of the evaluation.
         /// </summary>
-        /// <param name="script">The JavaScript code that should be executed.</param>
-        /// <param name="timeout">The timeout after which the JavaScript code execution should be aborted.</param>
+        /// <param name="script">The Javascript code that should be executed.</param>
+        /// <param name="timeout">The timeout after which the Javascript code execution should be aborted.</param>
         object EvaluateScript(string script, TimeSpan? timeout = null);
 
         IRequestHandler RequestHandler { get; set; }
