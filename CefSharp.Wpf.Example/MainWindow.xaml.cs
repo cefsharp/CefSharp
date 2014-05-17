4a231544fd6975272b826275b78dae61eb1834b6
JanEggers <jan.Eggers@ferag.com>
2014-05-17 05:59:17 +0200

    fixed whitespace (contained tabs)

--- CefSharp.Wpf.Example/MainWindow.xaml.cs
+++ CefSharp.Wpf.Example/MainWindow.xaml.cs
@@ -10,65 +10,65 @@ using CefSharp.Wpf.Example.ViewModels;
 
 namespace CefSharp.Wpf.Example
 {
-	public partial class MainWindow : Window
-	{
-		private const string DefaultUrlForAddedTabs = "https://www.google.com";
+    public partial class MainWindow : Window
+    {
+        private const string DefaultUrlForAddedTabs = "https://www.google.com";
 
-		public ObservableCollection<BrowserTabViewModel> BrowserTabs { get; set; }
+        public ObservableCollection<BrowserTabViewModel> BrowserTabs { get; set; }
 
-		public MainWindow()
-		{
-			InitializeComponent();
-			DataContext = this;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
 
-			BrowserTabs = new ObservableCollection<BrowserTabViewModel>();
+            BrowserTabs = new ObservableCollection<BrowserTabViewModel>();
 
-			CommandBindings.Add(new CommandBinding(ApplicationCommands.New, OpenNewTab));
-			CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, CloseTab));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.New, OpenNewTab));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, CloseTab));
 
-			Loaded += MainWindowLoaded;
-		}
+            Loaded += MainWindowLoaded;
+        }
 
-		private void CloseTab(object sender, ExecutedRoutedEventArgs e)
-		{
-			if (BrowserTabs.Count > 0)
-			{
-				//Obtain the original source element for this event
-				var originalSource = (FrameworkElement)e.OriginalSource;
+        private void CloseTab(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (BrowserTabs.Count > 0)
+            {
+                //Obtain the original source element for this event
+                var originalSource = (FrameworkElement)e.OriginalSource;
 
                 BrowserTabViewModel browserViewModel = null;
 
-				if (originalSource is MainWindow)
-				{
+                if (originalSource is MainWindow)
+                {
                     browserViewModel = BrowserTabs[TabControl.SelectedIndex];
                     BrowserTabs.RemoveAt(TabControl.SelectedIndex);
-				}
-				else
-				{
-					//Remove the matching DataContext from the BrowserTabs collection
+                }
+                else
+                {
+                    //Remove the matching DataContext from the BrowserTabs collection
                     browserViewModel = (BrowserTabViewModel)originalSource.DataContext;
                     BrowserTabs.Remove(browserViewModel);
-				}
+                }
 
-			    browserViewModel.WebBrowser.CleanupCommand.Execute( null );
-			}
-		}
+                browserViewModel.WebBrowser.Dispose();
+            }
+        }
 
-		private void OpenNewTab(object sender, ExecutedRoutedEventArgs e)
-		{
-			CreateNewTab();
+        private void OpenNewTab(object sender, ExecutedRoutedEventArgs e)
+        {
+            CreateNewTab();
 
-			TabControl.SelectedIndex = TabControl.Items.Count - 1;
-		}
+            TabControl.SelectedIndex = TabControl.Items.Count - 1;
+        }
 
-		private void MainWindowLoaded(object sender, RoutedEventArgs e)
-		{
-			CreateNewTab(ExamplePresenter.DefaultUrl, true);
-		}
+        private void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            CreateNewTab(ExamplePresenter.DefaultUrl, true);
+        }
 
-		private void CreateNewTab(string url = DefaultUrlForAddedTabs, bool showSideBar = false)
-		{
-			BrowserTabs.Add(new BrowserTabViewModel(url) { ShowSidebar = showSideBar });
-		}
-	}
+        private void CreateNewTab(string url = DefaultUrlForAddedTabs, bool showSideBar = false)
+        {
+            BrowserTabs.Add(new BrowserTabViewModel(url) { ShowSidebar = showSideBar });
+        }
+    }
 }
