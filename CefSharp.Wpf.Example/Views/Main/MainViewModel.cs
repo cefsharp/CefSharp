22c1ccef0c96bff9e4159c5a7fe927b30df4214c
fraenke75 <fweingaertner@gmail.com>
2014-05-22 17:30:07 +0200

    Added the usage of the OnBeforePluginLoad to the ExamplePresenter

--- CefSharp.Wpf.Example/Views/Main/MainViewModel.cs
+++ CefSharp.Wpf.Example/Views/Main/MainViewModel.cs
@@ -126,6 +126,7 @@ namespace CefSharp.Wpf.Example.Views.Main
                         // TODO: method, but it seems like "something" gets messed up (= doesn't work correctly) if we give it
                         // TODO: focus "too early" in the loading process...
                         WebBrowser.LoadCompleted += delegate { Application.Current.Dispatcher.BeginInvoke((Action)(() => webBrowser.Focus())); };
+                        ExamplePresenter presenter = new ExamplePresenter(WebBrowser, (action) => Application.Current.Dispatcher.BeginInvoke(action));
                     }
 
                     break;
