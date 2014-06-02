728bff341bd10e19de1993ef49cfca478259c1ee
Per Lundberg <per.lundberg@ecraft.com>
2014-06-02 11:27:27 -0700

    Added missing license header also.

--- CefSharp/IWinFormsWebBrowser.cs
+++ CefSharp/IWinFormsWebBrowser.cs
@@ -1,4 +1,8 @@
-﻿namespace CefSharp
+﻿// Copyright © 2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+namespace CefSharp
 {
     // Should rightfully live in the CefSharp.WinForms project, but the problem is that it's being used from CefSharp.Core
     // so the dependency would go the wrong way... Has to be here for the time being.
