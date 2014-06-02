a5a75a11764f8901cb83122e5fc82cba44bad093
Per Lundberg <per.lundberg@ecraft.com>
2014-06-02 12:23:16 +0100

    Once again, cleaned up usings. Maybe I should just run it on the whole solution and commit all of it at once...

--- CefSharp/Internals/JavascriptMethod.cs
+++ CefSharp/Internals/JavascriptMethod.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
+﻿using System.Runtime.Serialization;
 
 namespace CefSharp.Internals
 {
