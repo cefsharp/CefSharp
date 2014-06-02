6b2a8d701185e1d6e318e4ed20082c669a43d54d
Per Lundberg <per.lundberg@ecraft.com>
2014-06-02 11:26:56 -0700

    Fixed spelling error + updated string which was funkily replaced a while ago when doing mass updates to the file headers. Also, refer to CefSharp.Core now, since that's where the reference comes from.

--- CefSharp/IWinFormsWebBrowser.cs
+++ CefSharp/IWinFormsWebBrowser.cs
@@ -1,6 +1,6 @@
 ﻿namespace CefSharp
 {
-    // Should rightfulyl live in the CefSharp.WinForms project, but the problem is that it's being used from The CefSharp Authors
+    // Should rightfully live in the CefSharp.WinForms project, but the problem is that it's being used from CefSharp.Core
     // so the dependency would go the wrong way... Has to be here for the time being.
     public interface IWinFormsWebBrowser : IWebBrowser
     {
