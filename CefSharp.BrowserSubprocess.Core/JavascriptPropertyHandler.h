36ed4dbdc0cf0cb0d6b294df9d81a6a4fb7c1646
JanEggers <jan.Eggers@ferag.com>
2014-06-07 09:52:45 +0200

    implemented Property Getter / Setter

--- CefSharp.BrowserSubprocess.Core/JavascriptPropertyHandler.h
+++ CefSharp.BrowserSubprocess.Core/JavascriptPropertyHandler.h
@@ -4,24 +4,40 @@
 
 #include "Stdafx.h"
 
+#include "include/cef_v8.h"
+#include "TypeUtils.h"
+
 #pragma once
 
 namespace CefSharp
 {
     private class JavascriptPropertyHandler : public CefV8Accessor
     {
+        gcroot<Func<Object^>^> _getter;
+        gcroot<Action<Object^>^> _setter;
+
+    public:
+        JavascriptPropertyHandler(Func<Object^>^ getter, Action<Object^>^ setter)
+        {
+            _getter = getter;
+            _setter = setter;
+        }
+
         virtual bool Get(const CefString& name, const CefRefPtr<CefV8Value> object, CefRefPtr<CefV8Value>& retval,
             CefString& exception) override
         {
-            // TODO: implement.
-            return false;
+            auto result = _getter->Invoke();
+            retval = TypeUtils::ConvertToCef(result, nullptr);
+            return true;
         }
 
         virtual bool Set(const CefString& name, const CefRefPtr<CefV8Value> object, const CefRefPtr<CefV8Value> value,
             CefString& exception) override
         {
-            // TODO: implement
-            return false;
+            System::Diagnostics::Debugger::Break();
+            auto managedValue = TypeUtils::ConvertFromCef(value);
+            _setter->Invoke(managedValue);
+            return true;
         }
 
         IMPLEMENT_REFCOUNTING(JavascriptPropertyHandler)
