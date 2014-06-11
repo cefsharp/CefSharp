1ca86e4c080cb9950a16dde31886f9ccda31978a
Per Lundberg <per.lundberg@ecraft.com>
2014-06-12 01:11:09 +0300

    Duh. I fouled up big-time in my previous commit. Always compile before committing... I guess I should start Visual Studio and not just sit in a text editor. ;)

--- CefSharp/IWebBrowser.cs
+++ CefSharp/IWebBrowser.cs
@@ -6,10 +6,8 @@ namespace CefSharp
     /// <summary>
     /// A delegate type used to listen to LoadError messages.
     /// </summary>
-    /// <param name="failedUrl">The URL that failed to load.</param>
-    /// <param name="errorCode">The error code.</param>
-    /// <param name="errorText">The error text.</param>
-    public delegate void LoadErrorEventHandler(string failedUrl, CefErrorCode errorCode, string errorText);
+    /// <param name="e">The event arguments.</param>
+    public delegate void LoadErrorEventHandler(object sender, LoadErrorEventArgs e);
 
     public interface IWebBrowser : IDisposable
     {
