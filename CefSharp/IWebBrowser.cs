e5fea12dbcad04ba7441dee556507e38ab5f1c59
Per Lundberg <per.lundberg@ecraft.com>
2014-06-12 01:14:18 +0300

    Even though it's a tad extreme, let's go with the "one-file-per-type" approach. It just makes things so much easier, and ReSharper makes these kind of changes be only a single keyboard shortcut away anyway.

--- CefSharp/IWebBrowser.cs
+++ CefSharp/IWebBrowser.cs
@@ -3,12 +3,6 @@ using System.Threading.Tasks;
 
 namespace CefSharp
 {
-    /// <summary>
-    /// A delegate type used to listen to LoadError messages.
-    /// </summary>
-    /// <param name="e">The event arguments.</param>
-    public delegate void LoadErrorEventHandler(object sender, LoadErrorEventArgs e);
-
     public interface IWebBrowser : IDisposable
     {
         /// <summary>
