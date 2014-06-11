b65587cdfc1cbccd8d1b449f8203cd474d52d428
Per Lundberg <per.lundberg@ecraft.com>
2014-06-12 02:16:53 +0300

    CefKeyType is now called just KeyType, for simplicity. (or maybe stupidity. ;)

--- CefSharp/IKeyboardHandler.cs
+++ CefSharp/IKeyboardHandler.cs
@@ -3,6 +3,6 @@
     public interface IKeyboardHandler
     {
         bool OnKeyEvent(IWebBrowser browser, KeyType type, int code, int modifiers, bool isSystemKey);
-        bool OnPreKeyEvent(IWebBrowser browser, CefKeyType type, int windowsKeyCode, int nativeKeyCode, int modifiers, bool isSystemKey, bool isKeyboardShortcut);
+        bool OnPreKeyEvent(IWebBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, int modifiers, bool isSystemKey, bool isKeyboardShortcut);
     }
 }
