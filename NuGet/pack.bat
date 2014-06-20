a58ef26a15cf93429b19d5acec87263face8e3d7
jornh <jornhansen@gmail.com>
2014-06-20 10:36:07 +0200

    WIP on factoring x64 and x86 out as parameter to `*.nuspec` files

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,4 +1,9 @@
-set version=31.0.0-pre1
-NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version%
-NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version%
+set version=31.0.0-pre1a
+NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
+NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x86
+NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x64
+NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
+NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x86
+NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x64
 rem NuGet pack CefSharp.WinForms.nuspec -NoPackageAnalysis -Version %version%
+pause
