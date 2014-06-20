b234a69efe98b6013186bdcf76f2a1acb54b54ed
jornh <jornhansen@gmail.com>
2014-06-21 00:11:33 +0200

    Stop forcing x86 again

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,5 +1,5 @@
 set version=31.0.0-pre1a
-set platform=x86
+REM set platform=x86
 rem NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
 NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.%Platform%;Platform=%Platform%
 rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
