59266e50fd51867c7e618ce0b61a2efdd683ea6d
jornh <jornhansen@gmail.com>
2014-06-20 17:18:47 +0200

    A clean build is a clean build ...
    ... Which means there are no leftover x64 binaries to include in a x86 package

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,8 +1,7 @@
 set version=31.0.0-pre1a
-NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
-NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x86
-NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x64
-NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
-NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x86
-NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x64
+rem NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
+NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.%Platform%;Platform=%Platform%
+rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
+rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x86
+rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x64
 rem NuGet pack CefSharp.WinForms.nuspec -NoPackageAnalysis -Version %version%
