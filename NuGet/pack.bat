2817c15f13f90c980fffab160b4798462977fc48
jornh <jornhansen@gmail.com>
2014-06-21 10:34:06 +0200

    Pack WinForms

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -5,4 +5,4 @@ NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Propert
 rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
 rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x86
 NuGet pack CefSharp.WpfOrWinForms.nuspec -NoPackageAnalysis -Version %version% -Properties ControlType=Wpf;DotPlatform=.%Platform%;Platform=%Platform%
-rem NuGet pack CefSharp.WinForms.nuspec -NoPackageAnalysis -Version %version%
+NuGet pack CefSharp.WpfOrWinForms.nuspec -NoPackageAnalysis -Version %version% -Properties ControlType=WinForms;DotPlatform=.%Platform%;Platform=%Platform%
