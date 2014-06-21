7b62ced9121d13d7ab6d11982d142772c268b91f
jornh <jornhansen@gmail.com>
2014-06-21 10:19:53 +0200

    Pack WPF

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -4,5 +4,5 @@ rem NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Pro
 NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.%Platform%;Platform=%Platform%
 rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
 rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x86
-rem NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=.x64
+NuGet pack CefSharp.WpfOrWinForms.nuspec -NoPackageAnalysis -Version %version% -Properties ControlType=Wpf;DotPlatform=.%Platform%;Platform=%Platform%
 rem NuGet pack CefSharp.WinForms.nuspec -NoPackageAnalysis -Version %version%
