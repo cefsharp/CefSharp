07e3014feb5718fad81c26666db0a12f7c9dd04d
jornh <jornhansen@gmail.com>
2014-06-22 13:00:51 +0200

    x64 Core.dll is in a x64 folder
    Another kind of errors clean builds will ruthlessly reveal :smile:

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -3,7 +3,7 @@ REM set platform=x86
 rem NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version% -Properties DotPlatform=
 
 if "%platform%"=="x64" (
-	copy ..\x86\Release\CefSharp.Core.dll
+	copy ..\x64\Release\CefSharp.Core.dll
 	set DotPlatform=.x64
 ) else (
 	copy ..\Win32\Release\CefSharp.Core.dll
