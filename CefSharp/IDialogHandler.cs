68edca8d35c94202dfba9644bb1f97cccbcad760
amaitland <alex@maitlandcomputing.com.au>
2014-06-24 13:38:59 +1000

    Add license disclaimer to all files in CefSharp project

--- CefSharp/IDialogHandler.cs
+++ CefSharp/IDialogHandler.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.Collections.Generic;
+
 namespace CefSharp
 {
     /// <summary>
--- CefSharp/Internals/Kernel32.cs
+++ CefSharp/Internals/Kernel32.cs
@@ -1,4 +1,8 @@
-﻿using System.Runtime.InteropServices;
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.Runtime.InteropServices;
 
 namespace CefSharp.Internals
 {
