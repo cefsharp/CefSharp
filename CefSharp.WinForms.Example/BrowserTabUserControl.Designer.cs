395a09c270b9fe0b799fb5ab49bc57dc6945f1ce
amaitland <alex@maitlandcomputing.com.au>
2014-06-25 16:39:09 +1000

    Create BrowserTabUserControl in preparation for migration to TabControl

---
+++ CefSharp.WinForms.Example/BrowserTabUserControl.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CefSharp.WinForms.Example
+{
+    partial class BrowserTabUserControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripContainer = new System.Windows.Forms.ToolStripContainer();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.backButton = new System.Windows.Forms.ToolStripButton();
+            this.forwardButton = new System.Windows.Forms.ToolStripButton();
+            this.urlTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.goButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripContainer.ContentPanel.SuspendLayout();
+            this.toolStripContainer.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer
+            // 
+            // 
+            // toolStripContainer.ContentPanel
+            // 
+            this.toolStripContainer.ContentPanel.Controls.Add(this.outputLabel);
+            this.toolStripContainer.ContentPanel.Size = new System.Drawing.Size(730, 465);
+            this.toolStripContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer.LeftToolStripPanelVisible = false;
+            this.toolStripContainer.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer.Name = "toolStripContainer";
+            this.toolStripContainer.RightToolStripPanelVisible = false;
+            this.toolStripContainer.Size = new System.Drawing.Size(730, 490);
+            this.toolStripContainer.TabIndex = 0;
+            this.toolStripContainer.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer.TopToolStripPanel
+            // 
+            this.toolStripContainer.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.outputLabel.Location = new System.Drawing.Point(0, 452);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 13);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backButton,
+            this.forwardButton,
+            this.urlTextBox,
+            this.goButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0);
+            this.toolStrip1.Size = new System.Drawing.Size(730, 25);
+            this.toolStrip1.Stretch = true;
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Layout += new System.Windows.Forms.LayoutEventHandler(this.HandleToolStripLayout);
+            // 
+            // backButton
+            // 
+            this.backButton.Enabled = false;
+            this.backButton.Image = global::CefSharp.WinForms.Example.Properties.Resources.nav_left_green;
+            this.backButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(52, 22);
+            this.backButton.Text = "Back";
+            this.backButton.Click += new System.EventHandler(this.BackButtonClick);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Enabled = false;
+            this.forwardButton.Image = global::CefSharp.WinForms.Example.Properties.Resources.nav_right_green;
+            this.forwardButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(70, 22);
+            this.forwardButton.Text = "Forward";
+            this.forwardButton.Click += new System.EventHandler(this.ForwardButtonClick);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.AutoSize = false;
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(500, 25);
+            this.urlTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.UrlTextBoxKeyUp);
+            // 
+            // goButton
+            // 
+            this.goButton.Image = global::CefSharp.WinForms.Example.Properties.Resources.nav_plain_green;
+            this.goButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(42, 22);
+            this.goButton.Text = "Go";
+            this.goButton.Click += new System.EventHandler(this.GoButtonClick);
+            // 
+            // BrowserTabUserControl
+            // 
+            this.Controls.Add(this.toolStripContainer);
+            this.Name = "BrowserTabUserControl";
+            this.Size = new System.Drawing.Size(730, 490);
+            this.toolStripContainer.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer.ContentPanel.PerformLayout();
+            this.toolStripContainer.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer.ResumeLayout(false);
+            this.toolStripContainer.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton backButton;
+        private System.Windows.Forms.ToolStripButton forwardButton;
+        private System.Windows.Forms.ToolStripTextBox urlTextBox;
+        private System.Windows.Forms.ToolStripButton goButton;
+        private System.Windows.Forms.Label outputLabel;
+
+    }
+}
---
+++ CefSharp.WinForms.Example/BrowserTabUserControl.cs
@@ -0,0 +1,133 @@
+﻿using CefSharp.Example;
+using CefSharp.WinForms.Example.Controls;
+using System;
+using System.Windows.Forms;
+
+namespace CefSharp.WinForms.Example
+{
+    public partial class BrowserTabUserControl : UserControl
+    {
+        private readonly ChromiumWebBrowser browser;
+
+        public BrowserTabUserControl()
+        {
+            InitializeComponent();
+
+            browser = new ChromiumWebBrowser(CefExample.DefaultUrl)
+            {
+                Dock = DockStyle.Fill,
+            };
+            toolStripContainer.ContentPanel.Controls.Add(browser);
+
+            browser.MenuHandler = new MenuHandler();
+            browser.NavStateChanged += OnBrowserNavStateChanged;
+            browser.ConsoleMessage += OnBrowserConsoleMessage;
+            browser.TitleChanged += OnBrowserTitleChanged;
+            browser.AddressChanged += OnBrowserAddressChanged;
+
+            var version = String.Format("Chromium: {0}, CEF: {1}, CefSharp: {2}", Cef.ChromiumVersion, Cef.CefVersion, Cef.CefSharpVersion);
+            DisplayOutput(version);
+        }
+
+        private void OnBrowserConsoleMessage(object sender, ConsoleMessageEventArgs args)
+        {
+            DisplayOutput(string.Format("Line: {0}, Source: {1}, Message: {2}", args.Line, args.Source, args.Message));
+        }
+
+        private void OnBrowserNavStateChanged(object sender, NavStateChangedEventArgs args)
+        {
+            SetCanGoBack(args.CanGoBack);
+            SetCanGoForward(args.CanGoForward);
+
+            this.InvokeOnUiThreadIfRequired(() => SetIsLoading(!args.CanReload));
+        }
+
+        private void OnBrowserTitleChanged(object sender, TitleChangedEventArgs args)
+        {
+            this.InvokeOnUiThreadIfRequired(() => Parent.Text = args.Title);
+        }
+
+        private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs args)
+        {
+            this.InvokeOnUiThreadIfRequired(() => urlTextBox.Text = args.Address);
+        }
+
+        private void SetCanGoBack(bool canGoBack)
+        {
+            this.InvokeOnUiThreadIfRequired(() => backButton.Enabled = canGoBack);
+        }
+
+        private void SetCanGoForward(bool canGoForward)
+        {
+            this.InvokeOnUiThreadIfRequired(() => forwardButton.Enabled = canGoForward);
+        }
+
+        private void SetIsLoading(bool isLoading)
+        {
+            goButton.Text = isLoading ?
+                "Stop" :
+                "Go";
+            goButton.Image = isLoading ?
+                Properties.Resources.nav_plain_red :
+                Properties.Resources.nav_plain_green;
+
+            HandleToolStripLayout();
+        }
+
+        public void DisplayOutput(string output)
+        {
+            this.InvokeOnUiThreadIfRequired(() => outputLabel.Text = output);
+        }
+
+        private void HandleToolStripLayout(object sender, LayoutEventArgs e)
+        {
+            HandleToolStripLayout();
+        }
+
+        private void HandleToolStripLayout()
+        {
+            var width = toolStrip1.Width;
+            foreach (ToolStripItem item in toolStrip1.Items)
+            {
+                if (item != urlTextBox)
+                {
+                    width -= item.Width - item.Margin.Horizontal;
+                }
+            }
+            urlTextBox.Width = Math.Max(0, width - urlTextBox.Margin.Horizontal - 18);
+        }
+
+        private void GoButtonClick(object sender, EventArgs e)
+        {
+            LoadUrl(urlTextBox.Text);
+        }
+
+        private void BackButtonClick(object sender, EventArgs e)
+        {
+            browser.Back();
+        }
+
+        private void ForwardButtonClick(object sender, EventArgs e)
+        {
+            browser.Forward();
+        }
+
+        private void UrlTextBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            LoadUrl(urlTextBox.Text);
+        }
+
+        private void LoadUrl(string url)
+        {
+            if (Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+            {
+                browser.Load(url);
+            }
+        }
+    }
+}
