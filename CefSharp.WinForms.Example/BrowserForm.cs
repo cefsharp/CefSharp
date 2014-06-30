f358f5c7efa53632f2e0789e9baa955c2c578266
amaitland <alex@maitlandcomputing.com.au>
2014-06-30 14:02:22 +1000

    Fix issue caused by rebasing on Master (using statement got wrapped in with the license disclaimer)

--- CefSharp.WinForms.Example/BrowserForm.cs
+++ CefSharp.WinForms.Example/BrowserForm.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
 //
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.using CefSharp.Example;
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 using System;
 using System.Windows.Forms;
+using CefSharp.Example;
 
 namespace CefSharp.WinForms.Example
 {
