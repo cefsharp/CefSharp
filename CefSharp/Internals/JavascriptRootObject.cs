55119d501abb8a27f5c7fbb310ab72abf509ed7b
amaitland <alex@maitlandcomputing.com.au>
2014-08-28 18:29:36 +1000

    Init list in JavascriptRootObject

--- CefSharp/Internals/JavascriptRootObject.cs
+++ CefSharp/Internals/JavascriptRootObject.cs
@@ -3,10 +3,15 @@ using System.Runtime.Serialization;
 
 namespace CefSharp.Internals
 {
-	[DataContract]
-	public class JavascriptRootObject
-	{
-		[DataMember]
-		public List<JavascriptObject> MemberObjects { get; set; }
-	}
+    [DataContract]
+    public class JavascriptRootObject
+    {
+        [DataMember]
+        public List<JavascriptObject> MemberObjects { get; set; }
+
+        public JavascriptRootObject()
+        {
+            MemberObjects = new List<JavascriptObject>();
+        }
+    }
 }
