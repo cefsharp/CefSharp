0b4d80a4603320f2931c43878819888c4e99757d
amaitland <alex@maitlandcomputing.com.au>
2014-08-29 12:39:53 +1000

    Add License disclaimer

--- CefSharp/Internals/IBrowserProcess.cs
+++ CefSharp/Internals/IBrowserProcess.cs
@@ -1,4 +1,8 @@
-﻿using System.ServiceModel;
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.ServiceModel;
 
 namespace CefSharp.Internals
 {
--- CefSharp/Internals/RenderprocessClientFactory.cs
+++ CefSharp/Internals/RenderprocessClientFactory.cs
@@ -2,9 +2,6 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-﻿using System;
-using System.ServiceModel;
-
 namespace CefSharp.Internals
 {
     public class RenderprocessClientFactory
