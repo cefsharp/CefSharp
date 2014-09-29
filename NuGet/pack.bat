20ad0f47ed9bc858d10493f169220e5be2f7606e
amaitland <alex@maitlandcomputing.com.au>
2014-09-29 19:48:21 +1000

    Update to -pre2 version number Add CefSharp.WinForms.nuspec for generation of WinForms package

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,4 +1,4 @@
-set version=33.0.0-pre0
+set version=33.0.0-pre2
 NuGet pack CefSharp.Common.nuspec -NoPackageAnalysis -Version %version%
 NuGet pack CefSharp.Wpf.nuspec -NoPackageAnalysis -Version %version%
-rem NuGet pack CefSharp.WinForms.nuspec -NoPackageAnalysis -Version %version%
+NuGet pack CefSharp.WinForms.nuspec -NoPackageAnalysis -Version %version%
