cefsharp/CefSharp

commit 3bb42fe8cbf5248bfa28416e9134930a1e071adb

Fixed #542: Forward Ctrl key state
When building the CefEventFlags to be forwarded to CEF, consider the
current state of the left and right control key.

CefSharp.Wpf/ChromiumWebBrowser.cs
@@ -697,6 +697,14 @@ namespace CefSharp.Wpf
             {
                 modifiers |= CefEventFlags.RightMouseButton;
             }
+
+            if (Keyboard.IsKeyDown(Key.LeftCtrl)) {
+              modifiers |= CefEventFlags.ControlDown | CefEventFlags.IsLeft;
+            }
+            if (Keyboard.IsKeyDown(Key.RightCtrl)) {
+              modifiers |= CefEventFlags.ControlDown | CefEventFlags.IsRight;
+            }
+
             return modifiers;
         }
 

commit f703aa548e97b2f1e47b20fa93a8ce198535be43

Corrected code formatting
Corrected the code formatting issues introduced with
3bb42fe8cbf5248bfa28416e9134930a1e071adb

CefSharp.Wpf/ChromiumWebBrowser.cs
@@ -698,10 +698,13 @@ namespace CefSharp.Wpf
                 modifiers |= CefEventFlags.RightMouseButton;
             }
 
-            if (Keyboard.IsKeyDown(Key.LeftCtrl)) {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
               modifiers |= CefEventFlags.ControlDown | CefEventFlags.IsLeft;
             }
-            if (Keyboard.IsKeyDown(Key.RightCtrl)) {
+
+            if (Keyboard.IsKeyDown(Key.RightCtrl))
+            {
               modifiers |= CefEventFlags.ControlDown | CefEventFlags.IsRight;
             }
 