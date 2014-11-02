c61bc9a762a6ac5ad5e7af159237846ae16cfbe5
Bodekaer <michael@bodekaer.dk>
2014-11-02 21:16:52 +0800

    Updated build.ps1 version to 2171.1899

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ param(
     [Parameter(Position = 1)]
     [string] $Version = "37.0.0",
     [Parameter(Position = 2)]
-    [string] $RedistVersion = "3.2062.1876"
+    [string] $RedistVersion = "3.2171.1899"
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
