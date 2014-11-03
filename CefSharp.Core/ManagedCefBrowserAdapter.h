37c9b1652c5c6b9c2f23e3113791c3edc6a6df69
kropewnicki <kropewnicki@gmail.com>
2014-11-03 07:14:01 -0500

    Update ManagedCefBrowserAdapter.h
    Check if _webBrowserInternal is null before checking if it is Initialized.

--- CefSharp.Core/ManagedCefBrowserAdapter.h
+++ CefSharp.Core/ManagedCefBrowserAdapter.h
@@ -1,4 +1,4 @@
-// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+// Copyright Â© 2010-2014 The CefSharp Authors. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
@@ -83,14 +83,17 @@ namespace CefSharp
 
         void OnInitialized()
         {
-            _webBrowserInternal->OnInitialized();
-
-            auto address = _address;
-
-            if ( address != nullptr )
-            {
-                LoadUrl(address);
-            }
+            if(_webBrowserInternal != nullptr)
+			{
+                _webBrowserInternal->OnInitialized();
+    
+                auto address = _address;
+    
+                if ( address != nullptr )
+                {
+                    LoadUrl(address);
+                }
+			}
         };
 
         void LoadHtml(String^ html, String^ url)
