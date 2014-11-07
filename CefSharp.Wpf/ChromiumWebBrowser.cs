0539636f50d0f691bfc5f811a953be2f7403e0d5
Bjarte Skogøy <bjarte.skogoy@agrando.no>
2014-11-07 21:10:21 +0100

    - Support auto zoom for dpi scaling

--- CefSharp.Wpf/ChromiumWebBrowser.cs
+++ CefSharp.Wpf/ChromiumWebBrowser.cs
@@ -1150,6 +1150,9 @@ namespace CefSharp.Wpf
 
         void IWebBrowserInternal.OnFrameLoadEnd(string url, bool isMainFrame, int httpStatusCode)
         {
+            //if (httpStatusCode == 200)
+            //    DoAutoZoom();
+
             var handler = FrameLoadEnd;
 
             if (handler != null)
