757bc661229db813c65fe4a783c8a4d7d9a4b3b8
amaitland <alex@maitlandcomputing.com.au>
2014-11-18 14:33:26 +1000

    Untabify

--- CefSharp/ResourceHandler.cs
+++ CefSharp/ResourceHandler.cs
@@ -8,29 +8,29 @@ using System.Text;
 
 namespace CefSharp
 {
-	public class ResourceHandler
-	{
-		public string MimeType { get; set; }
-		public Stream Stream { get; set; }
-		public int StatusCode { get; set; }
-		public string StatusText { get; set; }
-		public NameValueCollection Headers { get; set; }
+    public class ResourceHandler
+    {
+        public string MimeType { get; set; }
+        public Stream Stream { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusText { get; set; }
+        public NameValueCollection Headers { get; set; }
 
-		public ResourceHandler()
-		{
-			StatusCode = 200;
-			StatusText = "OK";
-			MimeType = "text/html";
-		}
+        public ResourceHandler()
+        {
+            StatusCode = 200;
+            StatusText = "OK";
+            MimeType = "text/html";
+        }
 
-		public static ResourceHandler FromFileName(string fileName)
-		{
-			return new ResourceHandler { Stream = File.OpenRead(fileName) };
-		}
+        public static ResourceHandler FromFileName(string fileName)
+        {
+            return new ResourceHandler { Stream = File.OpenRead(fileName) };
+        }
 
-		public static ResourceHandler FromString(string text)
-		{
-			return new ResourceHandler { Stream = new MemoryStream(Encoding.UTF8.GetBytes(text)) };
-		}
-	}
+        public static ResourceHandler FromString(string text)
+        {
+            return new ResourceHandler { Stream = new MemoryStream(Encoding.UTF8.GetBytes(text)) };
+        }
+    }
 }
