df4bff834372040af3f675bc6e8bd75173399d4c
amaitland <alex@maitlandcomputing.com.au>
2014-11-30 08:40:01 +1000

    Rename DragData properties

--- CefSharp/DragData.cs
+++ CefSharp/DragData.cs
@@ -3,6 +3,8 @@
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 
+using System.Collections.Generic;
+
 namespace CefSharp
 {
     public class DragData
@@ -10,42 +12,42 @@ namespace CefSharp
         /// <summary>
         /// Return the name of the file being dragged out of the browser window.
         /// </summary>
-        public string GetFileName { get; set; }
+        public string FileName { get; set; }
 
         /// <summary>
         /// Retrieve the list of file names that are being dragged into the browser window
         /// </summary>
-        public bool GetFileNames { get; set; }
+        public IList<string> FileNames { get; set; }
 
         /// <summary>
         /// Return the base URL that the fragment came from. This value is used for resolving relative URLs and may be empty. 
         /// </summary>
-        public string GetFragmentBaseURL { get; set; }
+        public string FragmentBaseUrl { get; set; }
 
         /// <summary>
         /// Return the text/html fragment that is being dragged. 
         /// </summary>
-        public string GetFragmentHtml { get; set; }
+        public string FragmentHtml { get; set; }
 
         /// <summary>
         /// Return the plain text fragment that is being dragged.
         /// </summary>
-        public string GetFragmentText { get; set; }
+        public string FragmentText { get; set; }
 
         /// <summary>
         /// Return the metadata, if any, associated with the link being dragged. 
         /// </summary>
-        public string GetLinkMetadata { set; get; }
+        public string LinkMetaData { set; get; }
 
         /// <summary>
         /// Return the title associated with the link being dragged.
         /// </summary>
-        public string GetLinkTitle { set; get; }
+        public string LinkTitle { set; get; }
 
         /// <summary>
         /// Return the link URL that is being dragged. 
         /// </summary>
-        public string GetLinkURL { set; get; }
+        public string LinkUrl { set; get; }
 
         /// <summary>
         /// Returns true if the drag data is a file.
