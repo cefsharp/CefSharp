74f130d456a90ae184dadda822e8b9ab16d482d5
amaitland <alex@maitlandcomputing.com.au>
2014-12-09 08:21:30 +1000

    Add license disclaimer

--- CefSharp.Example/DragHandler.cs
+++ CefSharp.Example/DragHandler.cs
@@ -1,4 +1,8 @@
-﻿namespace CefSharp.Example
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+namespace CefSharp.Example
 {
     public class DragHandler : IDragHandler
     {
