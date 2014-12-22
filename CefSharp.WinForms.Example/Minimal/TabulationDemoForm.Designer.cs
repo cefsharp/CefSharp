8f5ec9043e6254b6e35d585e7b64dedfc596f828
amaitland <alex@maitlandcomputing.com.au>
2014-12-22 16:08:31 +1000

    Add license disclaimer Restyle to match other example code

--- CefSharp.WinForms.Example/Minimal/TabulationDemoForm.Designer.cs
+++ CefSharp.WinForms.Example/Minimal/TabulationDemoForm.Designer.cs
@@ -43,8 +43,8 @@
             this.txtURL.Size = new System.Drawing.Size(774, 20);
             this.txtURL.TabIndex = 0;
             this.txtURL.Text = "http://www.google.com";
-            this.txtURL.Enter += new System.EventHandler(this.txtURL_Enter);
-            this.txtURL.Leave += new System.EventHandler(this.txtURL_Leave);
+            this.txtURL.Enter += new System.EventHandler(this.TxtUrlEnter);
+            this.txtURL.Leave += new System.EventHandler(this.TxtUrlLeave);
             // 
             // btnGO
             // 
@@ -55,7 +55,7 @@
             this.btnGO.TabIndex = 1;
             this.btnGO.Text = "GO";
             this.btnGO.UseVisualStyleBackColor = true;
-            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            this.btnGO.Click += new System.EventHandler(this.BtnGoClick);
             // 
             // grpBrowser
             // 
