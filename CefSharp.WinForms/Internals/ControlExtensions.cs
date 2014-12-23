41efcca94d71d1c2f8d6ea74d0bf9116f1090689
amaitland <alex@maitlandcomputing.com.au>
2014-12-23 13:00:16 +1000

    Move InvokeOnUiThreadIfRequired into WinForms lib

--- CefSharp.WinForms/Internals/ControlExtensions.cs
+++ CefSharp.WinForms/Internals/ControlExtensions.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows.Forms;
 
 namespace CefSharp.WinForms.Internals
 {
     public static class ControlExtensions
     {
+        /// <summary>
+        /// Executes the Action asynchronously on the UI thread, does not block execution on the calling thread.
+        /// </summary>
+        /// <param name="control">the control for which the update is required</param>
+        /// <param name="action">action to be performed on the control</param>
+        public static void InvokeOnUiThreadIfRequired(this Control control, Action action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.BeginInvoke(action);
+            }
+            else
+            {
+                action.Invoke();
+            }
+        }
+
         public static void Activate(this Control control)
         {
             //Notify WinForms world that inner browser window got focus. This will trigger Leave event to previous focused control
