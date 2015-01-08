44d00d79a0fc5218fc75cf54a674b0f7c278a5b1
amaitland <alex@maitlandcomputing.com.au>
2015-01-08 13:34:32 +1000

    Cleanup some of the Cookie code in Cef.h - remove redundant casts, simplify if statements

--- CefSharp.Core/Cef.h
+++ CefSharp.Core/Cef.h
@@ -164,17 +164,16 @@ namespace CefSharp
         /// <return>Returns false if the CookieManager is not available; otherwise, true.</return>
         static bool VisitAllCookies(ICookieVisitor^ visitor)
         {
-            CefRefPtr<CookieVisitor> cookieVisitor = new CookieVisitor(visitor);
             CefRefPtr<CefCookieManager> manager = CefCookieManager::GetGlobalManager();
 
-            if (manager != nullptr)
-            {
-                return manager->VisitAllCookies(static_cast<CefRefPtr<CefCookieVisitor>>(cookieVisitor));
-            }
-            else
+            if (manager == nullptr)
             {
                 return false;
             }
+
+            CefRefPtr<CookieVisitor> cookieVisitor = new CookieVisitor(visitor);
+            
+            return manager->VisitAllCookies(cookieVisitor);
         }
 
         /// <summary>Visits a subset of the cookies. The results are filtered by the given url scheme, host, domain and path. 
@@ -186,18 +185,16 @@ namespace CefSharp
         /// <return>Returns false if the CookieManager is not available; otherwise, true.</return>
         static bool VisitUrlCookies(String^ url, bool includeHttpOnly, ICookieVisitor^ visitor)
         {
-            CefRefPtr<CookieVisitor> cookieVisitor = new CookieVisitor(visitor);
             CefRefPtr<CefCookieManager> manager = CefCookieManager::GetGlobalManager();
 
-            if (manager != nullptr)
-            {
-                return manager->VisitUrlCookies(StringUtils::ToNative(url), includeHttpOnly,
-                    static_cast<CefRefPtr<CefCookieVisitor>>(cookieVisitor));
-            }
-            else
+            if (manager == nullptr)
             {
                 return false;
             }
+
+            CefRefPtr<CookieVisitor> cookieVisitor = new CookieVisitor(visitor);
+
+            return manager->VisitUrlCookies(StringUtils::ToNative(url), includeHttpOnly, cookieVisitor);
         }
 
         /// <summary>Sets a CefSharp Cookie. This function expects each attribute to be well-formed. It will check for disallowed
@@ -296,14 +293,12 @@ namespace CefSharp
         {
             CefRefPtr<CefCookieManager> manager = CefCookieManager::GetGlobalManager();
 
-            if (manager != nullptr)
-            {
-                return manager->SetStoragePath(StringUtils::ToNative(path), persistSessionCookies);
-            }
-            else
+            if (manager == nullptr)
             {
                 return false;
             }
+
+            return manager->SetStoragePath(StringUtils::ToNative(path), persistSessionCookies);
         }
 
         /// <summary> Flush the backing store (if any) to disk and execute the specified |handler| on the IO thread when done. Returns </summary>
@@ -318,9 +313,9 @@ namespace CefSharp
                 return false;
             }
 
-            auto wrapper = new CompletionHandler(handler);
+            CefRefPtr<CefCompletionCallback> wrapper = new CompletionHandler(handler);
 
-            return manager->FlushStore(static_cast<CefRefPtr<CefCompletionCallback>>(wrapper));
+            return manager->FlushStore(wrapper);
         }
 
         /// <summary>Shuts down CefSharp and the underlying CEF infrastructure. This method is safe to call multiple times; it will only
