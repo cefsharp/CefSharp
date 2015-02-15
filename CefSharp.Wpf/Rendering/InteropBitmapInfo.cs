ffe8302921269a1dc4d053eb9564bbb96482c4d9
amaitland <alex@maitlandcomputing.com.au>
2015-02-15 22:37:37 +1000

    Add license disclaimer

--- CefSharp.Wpf/Rendering/InteropBitmapInfo.cs
+++ CefSharp.Wpf/Rendering/InteropBitmapInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Interop;
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
