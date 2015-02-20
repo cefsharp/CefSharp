37d33323ff4f844e1040a1c621bfd6a6330f1557
amaitland <alex@maitlandcomputing.com.au>
2015-02-20 23:01:41 +1000

    Add license Call start after tick delegate has been assigned

--- CefSharp.Wpf/WpfExtensions.cs
+++ CefSharp.Wpf/WpfExtensions.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
 using System.Windows.Controls;
 using System.Windows.Threading;
 
@@ -9,13 +13,15 @@ namespace CefSharp.Wpf
         public static void DelayUiThreadRunAsync(this Control control, Action action, int milliseconds = 2000, DispatcherPriority priority = DispatcherPriority.DataBind)
         {
             var timer = new DispatcherTimer(priority, control.Dispatcher) { Interval = TimeSpan.FromMilliseconds(milliseconds) };
-            timer.Start();
+            
             timer.Tick += (sender, args) =>
             {
                 timer.Stop();
 
                 action();
             };
+
+            timer.Start();
         }
 
         public static void UiThreadRunAsync(this Control control, Action action, DispatcherPriority priority = DispatcherPriority.DataBind)
