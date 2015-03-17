d779f3374fa54cff8d0e743e16f2a94a14c2b1c0
Per Lundberg <per.lundberg@ecraft.com>
2015-03-17 08:53:11 +0200

    Cherry-pick of 9a1c5264567ed9c8792be35260d80e74e3666026.

--- CefSharp.Wpf/ChromiumWebBrowser.cs
+++ CefSharp.Wpf/ChromiumWebBrowser.cs
@@ -2,12 +2,12 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-using System.Text;
 using CefSharp.Internals;
 using Microsoft.Win32.SafeHandles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
