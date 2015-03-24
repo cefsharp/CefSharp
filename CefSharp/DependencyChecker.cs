8a7b9e2adbc1522948ede8e1c1f7d77e35c38b9b
amaitland <alex@maitlandcomputing.com.au>
2015-03-24 11:05:38 +1000

    Untabify

--- CefSharp/DependencyChecker.cs
+++ CefSharp/DependencyChecker.cs
@@ -8,106 +8,106 @@ using System.Reflection;
 
 namespace CefSharp
 {
-	/// <summary>
-	/// TODO: Expand to support optional dependencies
-	/// </summary>
-	public static class DependencyChecker
-	{
-		/// <summary>
-		/// List of Cef Dependencies - currently contains all possabilties
-		/// </summary>
-		public static string[] CefDependencies =
-		{
-			"libcef.dll",
-			"libEGL.dll",
-			"libGLESv2.dll",
-			"pdf.dll",
-			"icudtl.dat",
-			"ffmpegsumo.dll",
-			"d3dcompiler_43.dll",
-			"d3dcompiler_47.dll",
-			"devtools_resources.pak",
-			"cef.pak",
-			"cef_100_percent.pak",
-			"cef_200_percent.pak"
-		};
+    /// <summary>
+    /// TODO: Expand to support optional dependencies
+    /// </summary>
+    public static class DependencyChecker
+    {
+        /// <summary>
+        /// List of Cef Dependencies - currently contains all possabilties
+        /// </summary>
+        public static string[] CefDependencies =
+        {
+            "libcef.dll",
+            "libEGL.dll",
+            "libGLESv2.dll",
+            "pdf.dll",
+            "icudtl.dat",
+            "ffmpegsumo.dll",
+            "d3dcompiler_43.dll",
+            "d3dcompiler_47.dll",
+            "devtools_resources.pak",
+            "cef.pak",
+            "cef_100_percent.pak",
+            "cef_200_percent.pak"
+        };
 
-		/// <summary>
-		/// List of CefSharp Dependencies
-		/// </summary>
-		public static string[] CefSharpDependencies =
-		{
-			"CefSharp.Core.dll",
-			"CefSharp.dll",
-			"CefSharp.BrowserSubprocess.Core.dll",
-			"CefSharp.BrowserSubprocess.exe"
-		};
+        /// <summary>
+        /// List of CefSharp Dependencies
+        /// </summary>
+        public static string[] CefSharpDependencies =
+        {
+            "CefSharp.Core.dll",
+            "CefSharp.dll",
+            "CefSharp.BrowserSubprocess.Core.dll",
+            "CefSharp.BrowserSubprocess.exe"
+        };
 
-		/// <summary>
-		/// Check Dependencies relative to the executing assembly
-		/// </summary>
-		/// <returns>List of missing dependencies, if all present an empty List will be returned</returns>
-		public static List<string> CheckDependencies()
-		{
-			var executingAssembly = Assembly.GetExecutingAssembly();
+        /// <summary>
+        /// Check Dependencies relative to the executing assembly
+        /// </summary>
+        /// <returns>List of missing dependencies, if all present an empty List will be returned</returns>
+        public static List<string> CheckDependencies()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
 
-			var path = Path.GetDirectoryName(executingAssembly.Location);
+            var path = Path.GetDirectoryName(executingAssembly.Location);
 
-			return CheckDependencies(path);
-		}
+            return CheckDependencies(path);
+        }
 
-		/// <summary>
-		/// CheckDependencies iterates through the list of Cef and CefSharp dependencines
-		/// relative to the path provided and returns a list of missing ones
-		/// </summary>
-		/// <param name="path">path to check for dependencies</param>
-		/// <param name="localePackFile">The local pack file</param>
-		/// <returns>List of missing dependencies, if all present an empty List will be returned</returns>
-		public static List<string> CheckDependencies(string path, string localePackFile = @"locales\en-US.pak")
-		{
-			var missingDependencies = new List<string>();
+        /// <summary>
+        /// CheckDependencies iterates through the list of Cef and CefSharp dependencines
+        /// relative to the path provided and returns a list of missing ones
+        /// </summary>
+        /// <param name="path">path to check for dependencies</param>
+        /// <param name="localePackFile">The local pack file</param>
+        /// <returns>List of missing dependencies, if all present an empty List will be returned</returns>
+        public static List<string> CheckDependencies(string path, string localePackFile = @"locales\en-US.pak")
+        {
+            var missingDependencies = new List<string>();
 
-			//Loop through Cef dependencies and add to list if not found
-			foreach (var cefDependency in CefDependencies)
-			{
-				var dependencyPath = Path.Combine(path, cefDependency);
+            //Loop through Cef dependencies and add to list if not found
+            foreach (var cefDependency in CefDependencies)
+            {
+                var dependencyPath = Path.Combine(path, cefDependency);
 
-				if (!File.Exists(dependencyPath))
-				{
-					missingDependencies.Add(cefDependency);
-				}
-			}
+                if (!File.Exists(dependencyPath))
+                {
+                    missingDependencies.Add(cefDependency);
+                }
+            }
 
-			// Loop through CefSharp dependencies and add to list if not found
-			foreach (var cefSharpDependency in CefSharpDependencies)
-			{
-				var dependencyPath = Path.Combine(path, cefSharpDependency);
+            // Loop through CefSharp dependencies and add to list if not found
+            foreach (var cefSharpDependency in CefSharpDependencies)
+            {
+                var dependencyPath = Path.Combine(path, cefSharpDependency);
 
-				if (!File.Exists(dependencyPath))
-				{
-					missingDependencies.Add(cefSharpDependency);
-				}
-			}
+                if (!File.Exists(dependencyPath))
+                {
+                    missingDependencies.Add(cefSharpDependency);
+                }
+            }
 
-			var localePath = Path.Combine(path, localePackFile);
+            var localePath = Path.Combine(path, localePackFile);
 
-			if (!File.Exists(localePath))
-			{
-				missingDependencies.Add(localePackFile);
-			}
+            if (!File.Exists(localePath))
+            {
+                missingDependencies.Add(localePackFile);
+            }
 
-			return missingDependencies;
-		}
+            return missingDependencies;
+        }
 
-		/// <summary>
-		/// Shortcut method that calls <see cref="CheckDependencies()"/>
-		/// </summary>
-		/// <returns>Returns true of missing dependency count is 0</returns>
-		public static bool AreAllDependenciesPresent()
-		{
-			var missingDependencies = CheckDependencies();
+        /// <summary>
+        /// Shortcut method that calls <see cref="CheckDependencies()"/>
+        /// </summary>
+        /// <returns>Returns true of missing dependency count is 0</returns>
+        public static bool AreAllDependenciesPresent()
+        {
+            var missingDependencies = CheckDependencies();
 
-			return missingDependencies.Count == 0;
-		}
-	}
+            return missingDependencies.Count == 0;
+        }
+    }
 }
