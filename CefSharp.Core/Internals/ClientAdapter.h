b5ad359aeacff215e3143989fea8dafa303f509e
amaitland <alex@maitlandcomputing.com.au>
2015-04-08 11:07:10 +1000

    Now that `OnLoadStart` and `OnLoadEnd` simply fire event handlers remove the locking

--- CefSharp.Core/Internals/ClientAdapter.h
+++ CefSharp.Core/Internals/ClientAdapter.h
@@ -9,7 +9,6 @@
 #include "include/cef_app.h"
 #include "include/cef_client.h"
 #include "include/cef_render_process_handler.h"
-#include "AutoLock.h"
 
 using namespace System;
 
@@ -31,7 +30,6 @@ namespace CefSharp
             public CefGeolocationHandler
         {
         private:
-            CriticalSection _syncRoot;
             gcroot<IWebBrowserInternal^> _browserControl;
             gcroot<Action<int>^> _onAfterBrowserCreated;
             HWND _browserHwnd;
