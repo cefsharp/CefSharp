bca6c09114d727c629cfbade60b2a61620ea09ce
amaitland <alex@maitlandcomputing.com.au>
2015-04-09 11:36:09 +1000

    Add license disclaimer Untabify

--- CefSharp/ITaskScheduler.cs
+++ CefSharp/ITaskScheduler.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.Threading.Tasks;
 
 namespace CefSharp
 {
-	public interface ITaskScheduler
-	{
-		void ExecuteTask(Task task);
-	}
+    public interface ITaskScheduler
+    {
+        void ExecuteTask(Task task);
+    }
 }
