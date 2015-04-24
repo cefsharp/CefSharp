e3012e7b521238d761f785a1643d0a50b138ed7f
James <james@twdsolutions.net>
2015-04-24 09:47:45 +0800

    Update Program.cs

--- CefSharp.OffScreen.Example/Program.cs
+++ CefSharp.OffScreen.Example/Program.cs
@@ -1,8 +1,9 @@
-﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
@@ -15,14 +16,42 @@ namespace CefSharp.OffScreen.Example
     public class Program
     {
         private static ChromiumWebBrowser browser;
-        private static bool captureFirstRenderedImage = false;
+        private static readonly string TempDirectory = Path.GetTempPath() + "CefSharp\\";
+        private static readonly List<Browser> Browsers = new List<Browser>();
+        private static string browserName;
+        private static Boolean _imageSaved;
 
         public static void Main(string[] args)
         {
-            const string testUrl = "https://www.google.com/";
+            if (!Directory.Exists(TempDirectory))
+            {
+                Directory.CreateDirectory(TempDirectory);
+            }
+            const string testUrl = "http://multibrowser.com/";
+            //const string testUrl = "http://cnn.com/";
+            var browser1 = new Browser
+            {
+                BrowserName = "800x600",
+                BrowserWidth = 800,
+                BrowserHeight = 600
+            };
+            Browsers.Add(browser1);
+            var browser2 = new Browser
+            {
+                BrowserName = "1024x600",
+                BrowserWidth = 1024,
+                BrowserHeight = 600
+            };
+            Browsers.Add(browser2);
+            var browser3 = new Browser
+            {
+                BrowserName = "1024x800",
+                BrowserWidth = 1024,
+                BrowserHeight = 800
+            };
+            Browsers.Add(browser3);
 
             Console.WriteLine("This example application will load {0}, take a screenshot, and save it to your desktop.", testUrl);
-            Console.WriteLine("You may see a lot of Chromium debugging output, please wait...");
             Console.WriteLine();
 
             // You need to replace this with your own call to Cef.Initialize();
@@ -31,22 +60,47 @@ namespace CefSharp.OffScreen.Example
             // Create the offscreen Chromium browser.
             using (browser = new ChromiumWebBrowser(testUrl))
             {
-
-                // An event that is fired when the first page is finished loading.
-                // This returns to us from another thread.
-                if (captureFirstRenderedImage)
+                browser.FrameLoadEnd += BrowserFrameLoadEnd;
+                //browser.LoadingStateChanged += Browser_LoadingStateChanged;
+                foreach (var browserToTest in Browsers)
                 {
-                    browser.ResourceHandlerFactory.RegisterHandler(testUrl, ResourceHandler.FromString("<html><body><h1>CefSharp OffScreen</h1></body></html>"));
-                    browser.ScreenshotAsync().ContinueWith(DisplayBitmap);
+                    _imageSaved = false;
+                    browserName = browserToTest.BrowserName;
+                    browser.Size = new Size(browserToTest.BrowserWidth, browserToTest.BrowserHeight);
+                    browser.Reload(true);
+                    while (!_imageSaved)
+                    {
+                    }
                 }
-                else
-                {
-                    browser.FrameLoadEnd += BrowserFrameLoadEnd;
-                }
-
-                // We have to wait for something, otherwise the process will exit too soon.
-                Console.ReadKey();
             }
+            Process.Start("explorer.exe", TempDirectory);
+            Console.ReadKey();
+
+            //using (browser = new ChromiumWebBrowser(testUrl))
+            //{
+
+            //    // An event that is fired when the first page is finished loading.
+            //    // This returns to us from another thread.
+            //    browser.FrameLoadEnd += BrowserFrameLoadEnd;
+            //    browser.LoadingStateChanged += Browser_LoadingStateChanged;
+
+            //    foreach (var browserToTest in Browsers)
+            //    {
+            //        _pageLoaded = false;
+            //        browserName = browserToTest.BrowserName;
+            //        browser.Size = new Size(browserToTest.BrowserWidth, browserToTest.BrowserHeight);
+            //        browser.Reload(true);
+            //        while (_pageLoaded)
+            //        {
+
+            //        }
+            //        Console.WriteLine(browserName + " finished loading");
+            //    }
+            //    Process.Start("explorer.exe", TempDirectory);
+
+            //    // We have to wait for something, otherwise the process will exit too soon.
+            //    Console.ReadKey();
+            //}
 
             // Clean up Chromium objects.  You need to call this in your application otherwise
             // you will get a crash when closing.
@@ -59,21 +113,21 @@ namespace CefSharp.OffScreen.Example
             // (rather than an iframe within the main frame).
             if (e.IsMainFrame)
             {
-                // Remove the load event handler, because we only want one snapshot of the initial page.
-                browser.FrameLoadEnd -= BrowserFrameLoadEnd;
-
+                //Task.Delay(2000).Wait();
                 // Wait for the screenshot to be taken.
                 browser.ScreenshotAsync().ContinueWith(DisplayBitmap);
             }
         }
+        //private static void Browser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        //{
+        //    Console.WriteLine("Loading State:" + e.IsLoading);
+        //}
 
         private static void DisplayBitmap(Task<Bitmap> task)
         {
             // Make a file to save it to (e.g. C:\Users\jan\Desktop\CefSharp screenshot.png)
-            var screenshotPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "CefSharp screenshot.png");
+            var screenshotPath = Path.Combine(TempDirectory, browserName + ".png");
 
-            Console.WriteLine();
-            Console.WriteLine("Screenshot ready. Saving to {0}", screenshotPath);
 
             var bitmap = task.Result;
 
@@ -84,13 +138,15 @@ namespace CefSharp.OffScreen.Example
             // We no longer need the Bitmap.
             // Dispose it to avoid keeping the memory alive.  Especially important in 32-bit applications.
             bitmap.Dispose();
-
-            Console.WriteLine("Screenshot saved.  Launching your default image viewer...");
-
-            // Tell Windows to launch the saved image.
-            Process.Start(screenshotPath);
-
-            Console.WriteLine("Image viewer launched.  Press any key to exit.");
+            _imageSaved = true;
+            Console.WriteLine(browserName + " Saved");
         }
+    }
+
+    public class Browser
+    {
+        public string BrowserName { get; set; }
+        public int BrowserWidth { get; set; }
+        public int BrowserHeight { get; set; }
     }
 }
