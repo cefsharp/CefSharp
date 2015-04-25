7128e40fd15d29f60f1b682fc00ab0dd7c7322b6
rassilon <bill@tutts.org>
2015-04-25 02:45:21 -0400

    It's time for copyright updates again. Yay! Throw in a couple of tab containing files for good measure. Evil tabs, evil I say.

--- CefSharp.Core/SchemeHandlerFactoryWrapper.cpp
+++ CefSharp.Core/SchemeHandlerFactoryWrapper.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+// Copyright © 2010-2015 The CefSharp Authors. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
@@ -10,14 +10,14 @@ using namespace System::IO;
 
 namespace CefSharp
 {
-	CefRefPtr<CefResourceHandler> SchemeHandlerFactoryWrapper::Create(
-		CefRefPtr<CefBrowser> browser,
-		CefRefPtr<CefFrame> frame,
-		const CefString& scheme_name,
-		CefRefPtr<CefRequest> request)
-	{
-		ISchemeHandler^ handler = _factory->Create();
-		CefRefPtr<SchemeHandlerWrapper> wrapper = new SchemeHandlerWrapper(handler);
-		return static_cast<CefRefPtr<CefResourceHandler>>(wrapper);
-	}
+    CefRefPtr<CefResourceHandler> SchemeHandlerFactoryWrapper::Create(
+        CefRefPtr<CefBrowser> browser,
+        CefRefPtr<CefFrame> frame,
+        const CefString& scheme_name,
+        CefRefPtr<CefRequest> request)
+    {
+        ISchemeHandler^ handler = _factory->Create();
+        CefRefPtr<SchemeHandlerWrapper> wrapper = new SchemeHandlerWrapper(handler);
+        return static_cast<CefRefPtr<CefResourceHandler>>(wrapper);
+    }
 }
