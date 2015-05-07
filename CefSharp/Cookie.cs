31aea0da848b1e8ad3a9e3536549a755ca3334f6
amaitland <alex@maitlandcomputing.com.au>
2015-05-07 18:42:10 +1000

    Minor formatting of Cookie

--- CefSharp/Cookie.cs
+++ CefSharp/Cookie.cs
@@ -9,12 +9,12 @@ namespace CefSharp
     public class Cookie
     {
         public string Name { get; set; }
-        public string Value  { get; set; }
-        public string Domain  { get; set; }
-        public string Path  { get; set; }
-        public bool Secure  { get; set; }
-        public bool HttpOnly  { get; set; }
-        public DateTime? Expires  { get; set; }
+        public string Value { get; set; }
+        public string Domain { get; set; }
+        public string Path { get; set; }
+        public bool Secure { get; set; }
+        public bool HttpOnly { get; set; }
+        public DateTime? Expires { get; set; }
         public DateTime Creation { get; set; }
         public DateTime LastAccess { get; set; }
     }
