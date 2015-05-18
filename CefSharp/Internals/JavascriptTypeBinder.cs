65cced88505b3afd50ef007aff78f700aeac611b
Erguotou <JeromeLiang@hotmail.com>
2015-05-18 08:37:52 -0700

    Refactor per Alex's comments
    Add a method to insert into explicitConversions, and remove the simple
    nested class to avoid creating a new C# file.

--- CefSharp/Internals/JavascriptTypeBinder.cs
+++ CefSharp/Internals/JavascriptTypeBinder.cs
@@ -1,19 +1,18 @@
-﻿using System;
+﻿// Copyright © 2010-2015 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+    using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace CefSharp.Internals
 {
     internal class JavascriptTypeBinder : Binder
     {
-        private class BinderState
-        {
-            public object[] args;
-        }
         interface IConvert
         {
             object Convert(object obj);
@@ -26,11 +25,6 @@ namespace CefSharp.Internals
             public Converter(Func<F, T> func)
             {
                 this.convert = func;
-                // Add this to JavascriptTypeBinder.explicitConversions
-                var type = typeof(F);
-                if (!explicitConversions.ContainsKey(type))
-                    explicitConversions[type] = new Dictionary<Type, IConvert>();
-                explicitConversions[type][typeof(T)] = this;
             }
 
             public object Convert(object obj)
@@ -52,14 +46,16 @@ namespace CefSharp.Internals
 
         static JavascriptTypeBinder()
         {
-            implicitConversions[TypeCode.SByte] = new TypeCode[] {
+            implicitConversions[TypeCode.SByte] = new TypeCode[] 
+            {
                 TypeCode.Int16,
                 TypeCode.Int32,
                 TypeCode.Int64,
                 TypeCode.Single,
                 TypeCode.Double,
             };
-            implicitConversions[TypeCode.Byte] = new TypeCode[] {
+            implicitConversions[TypeCode.Byte] = new TypeCode[] 
+            {
                 TypeCode.Int16,
                 TypeCode.UInt16,
                 TypeCode.Int32,
@@ -70,14 +66,16 @@ namespace CefSharp.Internals
                 TypeCode.Double,
                 TypeCode.Decimal,
             };
-            implicitConversions[TypeCode.Int16] = new TypeCode[] {
+            implicitConversions[TypeCode.Int16] = new TypeCode[] 
+            {
                 TypeCode.Int32,
                 TypeCode.Int64,
                 TypeCode.Single,
                 TypeCode.Double,
                 TypeCode.Decimal,
             };
-            implicitConversions[TypeCode.UInt16] = new TypeCode[] {
+            implicitConversions[TypeCode.UInt16] = new TypeCode[]
+            {
                 TypeCode.Int32,
                 TypeCode.UInt32,
                 TypeCode.Int64,
@@ -86,25 +84,29 @@ namespace CefSharp.Internals
                 TypeCode.Double,
                 TypeCode.Decimal,
             };
-            implicitConversions[TypeCode.Int32] = new TypeCode[] {
+            implicitConversions[TypeCode.Int32] = new TypeCode[]
+            {
                 TypeCode.Int64,
                 TypeCode.Single,
                 TypeCode.Double,
                 TypeCode.Decimal,
             };
-            implicitConversions[TypeCode.UInt32] = new TypeCode[] {
+            implicitConversions[TypeCode.UInt32] = new TypeCode[] 
+            {
                 TypeCode.Int64,
                 TypeCode.UInt64,
                 TypeCode.Single,
                 TypeCode.Double,
                 TypeCode.Decimal,
             };
-            implicitConversions[TypeCode.Int64] = new TypeCode[] {
+            implicitConversions[TypeCode.Int64] = new TypeCode[] 
+            {
                 TypeCode.Single,
                 TypeCode.Double,
                 TypeCode.Decimal,
             };
-            implicitConversions[TypeCode.Char] = new TypeCode[] {
+            implicitConversions[TypeCode.Char] = new TypeCode[] 
+            {
                 TypeCode.UInt16,
                 TypeCode.Int32,
                 TypeCode.UInt32,
@@ -114,50 +116,62 @@ namespace CefSharp.Internals
                 TypeCode.Double,
                 TypeCode.Decimal,
             };
-            implicitConversions[TypeCode.Single] = new TypeCode[] {
+            implicitConversions[TypeCode.Single] = new TypeCode[] 
+            {
                 TypeCode.Double,
             };
-            implicitConversions[TypeCode.UInt64] = new TypeCode[] {
+            implicitConversions[TypeCode.UInt64] = new TypeCode[]
+            {
                 TypeCode.Single,
                 TypeCode.Double,
                 TypeCode.Decimal,
             };
 
             // signed to unsigned conversion to silence overflow exception
-            new Converter<int, byte>((v) => (byte) v);
-            new Converter<int, char>((v) => (char) v); // char is 16bit unsigned
-            new Converter<int, ushort>((v) => (ushort) v);
-            new Converter<int, uint>((v) => (uint) v);
-            new Converter<int, ulong>((v) => (ulong) v);
-            new Converter<long, ulong>((v) => (ulong) v);
+            AddConverter<int, byte>(v => (byte) v);
+            AddConverter<int, char>(v => (char) v); // char is 16bit unsigned
+            AddConverter<int, ushort>(v => (ushort) v);
+            AddConverter<int, uint>(v => (uint) v);
+            AddConverter<int, ulong>(v => (ulong) v);
+            AddConverter<long, ulong>(v => (ulong) v);
 
-            new Converter<double, char>((v) => (char) v); // char is 16bit unsigned
+            AddConverter<double, char>(v => (char) v); // char is 16bit unsigned
 
-            new Converter<int, bool?>((v) => v != 0);
-            new Converter<int, sbyte?>((v) => (sbyte) v);
-            new Converter<int, byte?>((v) => (byte) v);
-            new Converter<int, short?>((v) => (short) v);
-            new Converter<int, ushort?>((v) => (ushort) v);
-            new Converter<int, char?>((v) => (char) v);
-            new Converter<int, uint?>((v) => (uint) v);
-            new Converter<int, long?>((v) => (long) v);
-            new Converter<int, ulong?>((v) => (ulong) v);
-            new Converter<int, Single?>((v) => (Single) v);
-            new Converter<int, decimal?>((v) => (decimal) v);
-            new Converter<int, double?>((v) => (double) v);
+            AddConverter<int, bool?>(v => v != 0);
+            AddConverter<int, sbyte?>(v => (sbyte) v);
+            AddConverter<int, byte?>(v => (byte) v);
+            AddConverter<int, short?>(v => (short) v);
+            AddConverter<int, ushort?>(v => (ushort) v);
+            AddConverter<int, char?>(v => (char) v);
+            AddConverter<int, uint?>(v => (uint) v);
+            AddConverter<int, long?>(v => (long) v);
+            AddConverter<int, ulong?>(v => (ulong) v);
+            AddConverter<int, Single?>(v => (Single) v);
+            AddConverter<int, decimal?>(v => (decimal) v);
+            AddConverter<int, double?>(v => (double) v);
 
-            new Converter<double, bool?>((v) => v != 0);
-            new Converter<double, sbyte?>((v) => (sbyte) v);
-            new Converter<double, byte?>((v) => (byte) v);
-            new Converter<double, short?>((v) => (short) v);
-            new Converter<double, char?>((v) => (char) v);
-            new Converter<double, ushort?>((v) => (ushort) v);
-            new Converter<double, int?>((v) => (int) v);
-            new Converter<double, uint?>((v) => (uint) v);
-            new Converter<double, long?>((v) => (long) v);
-            new Converter<double, ulong?>((v) => (ulong) v);
-            new Converter<double, Single?>((v) => (Single) v);
-            new Converter<double, decimal?>((v) => (decimal) v);
+            AddConverter<double, bool?>(v => v != 0);
+            AddConverter<double, sbyte?>(v => (sbyte) v);
+            AddConverter<double, byte?>(v => (byte) v);
+            AddConverter<double, short?>(v => (short) v);
+            AddConverter<double, char?>(v => (char) v);
+            AddConverter<double, ushort?>(v => (ushort) v);
+            AddConverter<double, int?>(v => (int) v);
+            AddConverter<double, uint?>(v => (uint) v);
+            AddConverter<double, long?>(v => (long) v);
+            AddConverter<double, ulong?>(v => (ulong) v);
+            AddConverter<double, Single?>(v => (Single) v);
+            AddConverter<double, decimal?>(v => (decimal) v);
+        }
+
+        private static void AddConverter<F, T>(Func<F, T> func)
+        {
+            var type = typeof(F);
+            if (!explicitConversions.ContainsKey(type))
+            {
+                explicitConversions[type] = new Dictionary<Type, IConvert>();
+            }
+            explicitConversions[type][typeof(T)] = new Converter<F, T>(func);
         }
 
         public override FieldInfo BindToField(BindingFlags bindingAttr, FieldInfo[] match, object value, CultureInfo culture)
@@ -179,11 +193,9 @@ namespace CefSharp.Internals
         public override MethodBase BindToMethod(BindingFlags bindingAttr, MethodBase[] match, ref object[] args, ParameterModifier[] modifiers, CultureInfo culture, string[] names, out object state)
         {
             // Store the arguments to the method in a state object.
-            BinderState myBinderState = new BinderState();
             object[] arguments = new Object[args.Length];
             args.CopyTo(arguments, 0);
-            myBinderState.args = arguments;
-            state = myBinderState;
+            state = arguments;
             if (match == null)
             {
                 throw new ArgumentNullException();
@@ -213,7 +225,7 @@ namespace CefSharp.Internals
                         {
                             if (String.Compare(parameters[j].Name, names[k].ToString()) == 0)
                             {
-                                args[j] = myBinderState.args[k];
+                                args[j] = arguments[k];
                             }
                         }
                     }
@@ -285,7 +297,7 @@ namespace CefSharp.Internals
         public override void ReorderArgumentArray(ref object[] args, object state)
         {
             // Return the args that had been reordered by BindToMethod.
-            ((BinderState) state).args.CopyTo(args, 0);
+            ((object[]) state).CopyTo(args, 0);
         }
 
         public override MethodBase SelectMethod(BindingFlags bindingAttr, MethodBase[] match, Type[] types, ParameterModifier[] modifiers)
@@ -369,17 +381,13 @@ namespace CefSharp.Internals
                     {
                         return match[i];
                     }
-                    else
-                    {
-                        continue;
-                    }
                 }
             }
             return null;
         }
 
         // Determines whether type1 can be converted to type2. Check only for primitive types. 
-        private bool CanConvertFrom(Type type1, Type type2)
+        private static bool CanConvertFrom(Type type1, Type type2)
         {
             if (type1.IsPrimitive && type2.IsPrimitive)
             {
