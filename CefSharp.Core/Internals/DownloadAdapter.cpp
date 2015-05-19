bbd366227d2b4d87a013635db4c65c38600fb33c
jamespearce2006 <james@jamesandjo.com>
2015-05-19 08:52:28 +0100

    Remove added line

--- CefSharp.Core/Internals/DownloadAdapter.cpp
+++ CefSharp.Core/Internals/DownloadAdapter.cpp
@@ -38,7 +38,6 @@ namespace CefSharp
         void DownloadAdapter::OnDownloadUpdated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefDownloadItem> download_item,
             CefRefPtr<CefDownloadItemCallback> callback) 
         {
-
             if (!download_item->IsInProgress() && browser->IsPopup() && !browser->HasDocument())
             {
                 browser->GetHost()->CloseBrowser(false);
