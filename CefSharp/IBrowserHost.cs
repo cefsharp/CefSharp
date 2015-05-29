d99cefb8eaaa274e2f9154748c30348316e5446d
amaitland <alex@maitlandcomputing.com.au>
2015-05-29 12:21:51 +1000

    Remove CefFrameWrapper.h (wasn't used) Move CefBrowserHostWrapper into Internals

--- CefSharp/IBrowserHost.cs
+++ CefSharp/IBrowserHost.cs
@@ -7,22 +7,22 @@ using System.Threading.Tasks;
 
 namespace CefSharp
 {
-	public interface IBrowserHost
-	{
-		void StartDownload(string url);
-		void Print();
-		void SetZoomLevel(double zoomLevel);
-		Task<double> GetZoomLevelAsync();
-		IntPtr GetWindowHandle();
-		void CloseBrowser(bool forceClose);
-		
-		void ShowDevTools();
-		void CloseDevTools();
+    public interface IBrowserHost
+    {
+        void StartDownload(string url);
+        void Print();
+        void SetZoomLevel(double zoomLevel);
+        Task<double> GetZoomLevelAsync();
+        IntPtr GetWindowHandle();
+        void CloseBrowser(bool forceClose);
+        
+        void ShowDevTools();
+        void CloseDevTools();
 
-		void AddWordToDictionary(string word);
-		void ReplaceMisspelling(string word);
+        void AddWordToDictionary(string word);
+        void ReplaceMisspelling(string word);
 
-		void Find(int identifier, string searchText, bool forward, bool matchCase, bool findNext);
-		void StopFinding(bool clearSelection);
-	}
+        void Find(int identifier, string searchText, bool forward, bool matchCase, bool findNext);
+        void StopFinding(bool clearSelection);
+    }
 }
