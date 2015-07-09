2eaebcc715824b680f0b379f65726ab3d44c21a7
amaitland <alex@maitlandcomputing.com.au>
2015-07-09 12:04:21 +1000

    Remove stray line

--- CefSharp.BrowserSubprocess.Core/CefAppUnmanagedWrapper.h
+++ CefSharp.BrowserSubprocess.Core/CefAppUnmanagedWrapper.h
@@ -23,7 +23,6 @@ namespace CefSharp
         gcroot<Action<CefBrowserWrapper^>^> _onBrowserDestroyed;
         gcroot<Dictionary<int, CefBrowserWrapper^>^> _browserWrappers;
     public:
-        
         CefAppUnmanagedWrapper(Action<CefBrowserWrapper^>^ onBrowserCreated, Action<CefBrowserWrapper^>^ onBrowserDestoryed)
         {
             _onBrowserCreated = onBrowserCreated;
