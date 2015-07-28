93529cc845d3120395260f62b83c58ea1852d932
amaitland <alex@maitlandcomputing.com.au>
2015-07-28 11:16:02 +1000

    Untabify

--- CefSharp.Wpf/Internals/WpfExtensions.cs
+++ CefSharp.Wpf/Internals/WpfExtensions.cs
@@ -10,164 +10,164 @@ using CefSharp.Internals;
 
 namespace CefSharp.Wpf.Internals
 {
-	public static class WpfExtensions
-	{
-		public static CefEventFlags GetModifiers(this MouseEventArgs e)
-		{
-			CefEventFlags modifiers = 0;
+    public static class WpfExtensions
+    {
+        public static CefEventFlags GetModifiers(this MouseEventArgs e)
+        {
+            CefEventFlags modifiers = 0;
 
-			if (e.LeftButton == MouseButtonState.Pressed)
-			{
-				modifiers |= CefEventFlags.LeftMouseButton;
-			}
-			if (e.MiddleButton == MouseButtonState.Pressed)
-			{
-				modifiers |= CefEventFlags.MiddleMouseButton;
-			}
-			if (e.RightButton == MouseButtonState.Pressed)
-			{
-				modifiers |= CefEventFlags.RightMouseButton;
-			}
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                modifiers |= CefEventFlags.LeftMouseButton;
+            }
+            if (e.MiddleButton == MouseButtonState.Pressed)
+            {
+                modifiers |= CefEventFlags.MiddleMouseButton;
+            }
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                modifiers |= CefEventFlags.RightMouseButton;
+            }
 
-			if (Keyboard.IsKeyDown(Key.LeftCtrl))
-			{
-				modifiers |= CefEventFlags.ControlDown | CefEventFlags.IsLeft;
-			}
+            if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                modifiers |= CefEventFlags.ControlDown | CefEventFlags.IsLeft;
+            }
 
-			if (Keyboard.IsKeyDown(Key.RightCtrl))
-			{
-				modifiers |= CefEventFlags.ControlDown | CefEventFlags.IsRight;
-			}
+            if (Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                modifiers |= CefEventFlags.ControlDown | CefEventFlags.IsRight;
+            }
 
-			if (Keyboard.IsKeyDown(Key.LeftShift))
-			{
-				modifiers |= CefEventFlags.ShiftDown | CefEventFlags.IsLeft;
-			}
+            if (Keyboard.IsKeyDown(Key.LeftShift))
+            {
+                modifiers |= CefEventFlags.ShiftDown | CefEventFlags.IsLeft;
+            }
 
-			if (Keyboard.IsKeyDown(Key.RightShift))
-			{
-				modifiers |= CefEventFlags.ShiftDown | CefEventFlags.IsRight;
-			}
+            if (Keyboard.IsKeyDown(Key.RightShift))
+            {
+                modifiers |= CefEventFlags.ShiftDown | CefEventFlags.IsRight;
+            }
 
-			if (Keyboard.IsKeyDown(Key.LeftAlt))
-			{
-				modifiers |= CefEventFlags.AltDown | CefEventFlags.IsLeft;
-			}
+            if (Keyboard.IsKeyDown(Key.LeftAlt))
+            {
+                modifiers |= CefEventFlags.AltDown | CefEventFlags.IsLeft;
+            }
 
-			if (Keyboard.IsKeyDown(Key.RightAlt))
-			{
-				modifiers |= CefEventFlags.AltDown | CefEventFlags.IsRight;
-			}
+            if (Keyboard.IsKeyDown(Key.RightAlt))
+            {
+                modifiers |= CefEventFlags.AltDown | CefEventFlags.IsRight;
+            }
 
-			return modifiers;
-		}
+            return modifiers;
+        }
 
-		public static CefEventFlags GetModifiers(this KeyEventArgs e)
-		{
-			CefEventFlags modifiers = 0;
+        public static CefEventFlags GetModifiers(this KeyEventArgs e)
+        {
+            CefEventFlags modifiers = 0;
 
-			if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Shift))
-			{
-				modifiers |= CefEventFlags.ShiftDown;
-			}
+            if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Shift))
+            {
+                modifiers |= CefEventFlags.ShiftDown;
+            }
 
-			if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Alt))
-			{
-				modifiers |= CefEventFlags.AltDown;
-			}
+            if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Alt))
+            {
+                modifiers |= CefEventFlags.AltDown;
+            }
 
-			if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Control))
-			{
-				modifiers |= CefEventFlags.ControlDown;
-			}
+            if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Control))
+            {
+                modifiers |= CefEventFlags.ControlDown;
+            }
 
-			return modifiers;
-		}
+            return modifiers;
+        }
 
-		public static CefDragDataWrapper GetDragDataWrapper(this DragEventArgs e)
-		{
-			// Convert Drag Data
-			var dragData = CefDragDataWrapper.Create();
+        public static CefDragDataWrapper GetDragDataWrapper(this DragEventArgs e)
+        {
+            // Convert Drag Data
+            var dragData = CefDragDataWrapper.Create();
 
-			// Files            
-			dragData.IsFile = e.Data.GetDataPresent(DataFormats.FileDrop);
-			if (dragData.IsFile)
-			{
-				// As per documentation, we only need to specify FileNames, not FileName, when dragging into the browser (http://magpcss.org/ceforum/apidocs3/projects/(default)/CefDragData.html)
-				foreach (var filePath in (string[])e.Data.GetData(DataFormats.FileDrop))
-				{
-					var displayName = Path.GetFileName(filePath);
+            // Files            
+            dragData.IsFile = e.Data.GetDataPresent(DataFormats.FileDrop);
+            if (dragData.IsFile)
+            {
+                // As per documentation, we only need to specify FileNames, not FileName, when dragging into the browser (http://magpcss.org/ceforum/apidocs3/projects/(default)/CefDragData.html)
+                foreach (var filePath in (string[])e.Data.GetData(DataFormats.FileDrop))
+                {
+                    var displayName = Path.GetFileName(filePath);
 
-					dragData.AddFile(filePath.Replace("\\", "/"), displayName);
-				}
-			}
+                    dragData.AddFile(filePath.Replace("\\", "/"), displayName);
+                }
+            }
 
-			// Link/Url
-			var link = GetLink(e.Data);
-			dragData.IsLink = !string.IsNullOrEmpty(link);
-			if (dragData.IsLink)
-			{
-				dragData.LinkUrl = link;
-			}
+            // Link/Url
+            var link = GetLink(e.Data);
+            dragData.IsLink = !string.IsNullOrEmpty(link);
+            if (dragData.IsLink)
+            {
+                dragData.LinkUrl = link;
+            }
 
-			// Text/HTML
-			dragData.IsFragment = e.Data.GetDataPresent(DataFormats.Text);
-			if (dragData.IsFragment)
-			{
-				dragData.FragmentText = (string)e.Data.GetData(DataFormats.Text);
-				dragData.FragmentHtml = (string)e.Data.GetData(DataFormats.Html);
-			}
+            // Text/HTML
+            dragData.IsFragment = e.Data.GetDataPresent(DataFormats.Text);
+            if (dragData.IsFragment)
+            {
+                dragData.FragmentText = (string)e.Data.GetData(DataFormats.Text);
+                dragData.FragmentHtml = (string)e.Data.GetData(DataFormats.Html);
+            }
 
-			return dragData;
-		}
+            return dragData;
+        }
 
-		private static string GetLink(IDataObject data)
-		{
-			const string asciiUrlDataFormatName = "UniformResourceLocator";
-			const string unicodeUrlDataFormatName = "UniformResourceLocatorW";
+        private static string GetLink(IDataObject data)
+        {
+            const string asciiUrlDataFormatName = "UniformResourceLocator";
+            const string unicodeUrlDataFormatName = "UniformResourceLocatorW";
 
-			// Try Unicode
-			if (data.GetDataPresent(unicodeUrlDataFormatName))
-			{
-				// Try to read a Unicode URL from the data
-				var unicodeUrl = ReadUrlFromDragDropData(data, unicodeUrlDataFormatName, Encoding.Unicode);
-				if (unicodeUrl != null)
-				{
-					return unicodeUrl;
-				}
-			}
+            // Try Unicode
+            if (data.GetDataPresent(unicodeUrlDataFormatName))
+            {
+                // Try to read a Unicode URL from the data
+                var unicodeUrl = ReadUrlFromDragDropData(data, unicodeUrlDataFormatName, Encoding.Unicode);
+                if (unicodeUrl != null)
+                {
+                    return unicodeUrl;
+                }
+            }
 
-			// Try ASCII
-			if (data.GetDataPresent(asciiUrlDataFormatName))
-			{
-				// Try to read an ASCII URL from the data
-				return ReadUrlFromDragDropData(data, asciiUrlDataFormatName, Encoding.ASCII);
-			}
+            // Try ASCII
+            if (data.GetDataPresent(asciiUrlDataFormatName))
+            {
+                // Try to read an ASCII URL from the data
+                return ReadUrlFromDragDropData(data, asciiUrlDataFormatName, Encoding.ASCII);
+            }
 
-			// Not a valid link
-			return null;
-		}
+            // Not a valid link
+            return null;
+        }
 
-		/// <summary>Reads a URL using a particular text encoding from drag-and-drop data.</summary>
-		/// <param name="data">The drag-and-drop data.</param>
-		/// <param name="urlDataFormatName">The data format name of the URL type.</param>
-		/// <param name="urlEncoding">The text encoding of the URL type.</param>
-		/// <returns>A URL, or <see langword="null"/> if <paramref name="data"/> does not contain a URL
-		/// of the correct type.</returns>
-		private static string ReadUrlFromDragDropData(IDataObject data, string urlDataFormatName, Encoding urlEncoding)
-		{
-			// Read the URL from the data
-			string url;
-			using (var urlStream = (Stream)data.GetData(urlDataFormatName))
-			{
-				using (TextReader reader = new StreamReader(urlStream, urlEncoding))
-				{
-					url = reader.ReadToEnd();
-				}
-			}
+        /// <summary>Reads a URL using a particular text encoding from drag-and-drop data.</summary>
+        /// <param name="data">The drag-and-drop data.</param>
+        /// <param name="urlDataFormatName">The data format name of the URL type.</param>
+        /// <param name="urlEncoding">The text encoding of the URL type.</param>
+        /// <returns>A URL, or <see langword="null"/> if <paramref name="data"/> does not contain a URL
+        /// of the correct type.</returns>
+        private static string ReadUrlFromDragDropData(IDataObject data, string urlDataFormatName, Encoding urlEncoding)
+        {
+            // Read the URL from the data
+            string url;
+            using (var urlStream = (Stream)data.GetData(urlDataFormatName))
+            {
+                using (TextReader reader = new StreamReader(urlStream, urlEncoding))
+                {
+                    url = reader.ReadToEnd();
+                }
+            }
 
-			// URLs in drag/drop data are often padded with null characters so remove these
-			return url.TrimEnd('\0');
-		}
-	}
+            // URLs in drag/drop data are often padded with null characters so remove these
+            return url.TrimEnd('\0');
+        }
+    }
 }
--- CefSharp/KeyEventType.cs
+++ CefSharp/KeyEventType.cs
@@ -4,30 +4,30 @@
 
 namespace CefSharp
 {
-	public enum KeyEventType
-	{
-		/// <summary>
-		/// Notification that a key transitioned from "up" to "down".
-		/// </summary>
-		RawKeyDown = 0,
+    public enum KeyEventType
+    {
+        /// <summary>
+        /// Notification that a key transitioned from "up" to "down".
+        /// </summary>
+        RawKeyDown = 0,
 
-		/// <summary>
-		/// Notification that a key was pressed. This does not necessarily correspond
-		/// to a character depending on the key and language. Use KEYEVENT_CHAR for
-		/// character input.
-		/// </summary>
-		KeyDown,
+        /// <summary>
+        /// Notification that a key was pressed. This does not necessarily correspond
+        /// to a character depending on the key and language. Use KEYEVENT_CHAR for
+        /// character input.
+        /// </summary>
+        KeyDown,
 
-		/// <summary>
-		/// Notification that a key was released.
-		/// </summary>
-		KeyUp,
+        /// <summary>
+        /// Notification that a key was released.
+        /// </summary>
+        KeyUp,
 
-		/// <summary>
-		/// Notification that a character was typed. Use this for text input. Key
-		/// down events may generate 0, 1, or more than one character event depending
-		/// on the key, locale, and operating system.
-		/// </summary>
-		Char
-	}
+        /// <summary>
+        /// Notification that a character was typed. Use this for text input. Key
+        /// down events may generate 0, 1, or more than one character event depending
+        /// on the key, locale, and operating system.
+        /// </summary>
+        Char
+    }
 }
