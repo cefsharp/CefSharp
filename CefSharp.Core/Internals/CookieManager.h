f7200064f35839a493ed9958ff6b054040ffebd9
David Serfozo <david@serfozo.net>
2015-07-29 22:56:46 +0200

    added missing license headers

--- CefSharp.Core/Internals/CookieManager.h
+++ CefSharp.Core/Internals/CookieManager.h
@@ -1,3 +1,7 @@
+// Copyright © 2010-2015 The CefSharp Project. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 #pragma once
 
 #include "MCefRefPtr.h"
