4f31a9309c9c7e66638d32e6dc285416f9443480
David Serfozo <david@serfozo.net>
2015-08-01 14:10:20 +0200

    added cefextension class

---
+++ CefSharp/CefExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CefSharp
+{
+    /// <summary>
+    /// Represents a new V8 extension to be registered.
+    /// </summary>
+    public sealed class CefExtension
+    {
+        /// <summary>
+        /// Gets the name of the extension.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the javascript extension code
+        /// </summary>
+        public string JavascriptCode { get; set; }
+
+        /// <summary>
+        /// Creates a new CwefExtension instance with a given name.
+        /// </summary>
+        /// <param name="name">Name of the CefExtension</param>
+        public CefExtension(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            var ext = (CefExtension)obj;
+            return Name.Equals(ext.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        } 
+    }
+}
