25be7265f20d4a4651cd607cabe4cd6797c77169
amaitland <alex@maitlandcomputing.com.au>
2015-08-03 11:37:49 +1000

    Add DebuggerHidden attribute so VS doesn't break when `Break on User-Unhandled Exceptions` is checked

--- CefSharp.Example/AsyncBoundObject.cs
+++ CefSharp.Example/AsyncBoundObject.cs
@@ -3,17 +3,22 @@
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace CefSharp.Example
 {
     public class AsyncBoundObject
     {
+        //We expect an exception here, so tell VS to ignore
+        [DebuggerHidden]
         public void Error()
         {
             throw new Exception("This is an exception coming from C#");
         }
 
+        //We expect an exception here, so tell VS to ignore
+        [DebuggerHidden]
         public int Div(int divident, int divisor)
         {
             return divident / divisor;
