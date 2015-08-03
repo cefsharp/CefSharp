7498c6574186f0c0dc50431e0825c9aa146d03a2
amaitland <alex@maitlandcomputing.com.au>
2015-08-03 10:41:29 +1000

    Add copyright to CefSharp.Example

--- CefSharp.Example/CefSharpSchemeHandlerFactory.cs
+++ CefSharp.Example/CefSharpSchemeHandlerFactory.cs
@@ -1,3 +1,7 @@
+// Copyright © 2010-2015 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 namespace CefSharp.Example
 {
     internal class CefSharpSchemeHandlerFactory : ISchemeHandlerFactory
