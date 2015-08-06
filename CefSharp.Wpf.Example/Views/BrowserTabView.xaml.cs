7588f8c24fa7616d62f74fd9143a09ac4ed200af
amaitland <alex@maitlandcomputing.com.au>
2015-08-06 21:36:57 +1000

    Set type param to Char for IME WPF example of PreviewTextInput

--- CefSharp.Wpf.Example/Views/BrowserTabView.xaml.cs
+++ CefSharp.Wpf.Example/Views/BrowserTabView.xaml.cs
@@ -26,19 +26,20 @@ namespace CefSharp.Wpf.Example.Views
             browser.MenuHandler = new MenuHandler();
             browser.GeolocationHandler = new GeolocationHandler();
             browser.DownloadHandler = new DownloadHandler();
-            //browser.PreviewTextInput += (sender, args) =>
-            //{
-            //    var host = browser.GetBrowser().GetHost();
-            //    var keyEvent = new KeyEvent();
+            browser.PreviewTextInput += (sender, args) =>
+            {
+                var host = browser.GetBrowser().GetHost();
+                var keyEvent = new KeyEvent();
 
-            //    foreach (var character in args.Text)
-            //    {
-            //        keyEvent.WindowsKeyCode = character;
-            //        host.SendKeyEvent(keyEvent);
-            //    }
+                foreach (var character in args.Text)
+                {
+                    keyEvent.WindowsKeyCode = character;
+                    keyEvent.Type = KeyEventType.Char;
+                    host.SendKeyEvent(keyEvent);
+                }
 
-            //    args.Handled = true;
-            //};
+                args.Handled = true;
+            };
 
             browser.LoadError += (sender, args) =>
             {
