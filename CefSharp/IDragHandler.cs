1c7b887ca029b2d1a164c2c2b88e08eb6c54ff3e
amaitland <alex@maitlandcomputing.com.au>
2015-09-18 16:56:37 +1000

    A very quick attempt to improve some of the Interface documentation before the official 43.0.0 release

--- CefSharp/IDragHandler.cs
+++ CefSharp/IDragHandler.cs
@@ -4,8 +4,20 @@
 
 namespace CefSharp
 {
+    /// <summary>
+    /// Implement this interface to handle events related to dragging.
+    /// The methods of this class will be called on the UI thread. 
+    /// </summary>
     public interface IDragHandler
     {
+        /// <summary>
+        /// Called when an external drag event enters the browser window.
+        /// </summary>
+        /// <param name="browserControl">the ChromiumWebBrowser control</param>
+        /// <param name="browser">the browser object</param>
+        /// <param name="dragData">contains the drag event data</param>
+        /// <param name="mask">represents the type of drag operation</param>
+        /// <returns>Return false for default drag handling behavior or true to cancel the drag event. </returns>
         bool OnDragEnter(IWebBrowser browserControl, IBrowser browser, IDragData dragData, DragOperationsMask mask);
     }
 }
