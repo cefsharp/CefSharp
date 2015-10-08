71acc04c401a762bd4828c57b1503a56b7edde6f
amaitland <amaitland@users.noreply.github.com>
2015-10-08 22:37:14 +1000

    Add CefSharpSettings.ShutdownOnExit - all the user to disable the inbuilt call to Cef.Shutdown (Also needs to set shutdownOnProcessExit:false when calling Cef.Initialize, or the ParentProcessExitHandler will be called to Shutdown Cef)

--- CefSharp.WinForms/ChromiumWebBrowser.cs
+++ CefSharp.WinForms/ChromiumWebBrowser.cs
@@ -70,7 +70,10 @@ namespace CefSharp.WinForms
 
         static ChromiumWebBrowser()
         {
-            Application.ApplicationExit += OnApplicationExit;
+            if (CefSharpSettings.ShutdownOnExit)
+            {
+                Application.ApplicationExit += OnApplicationExit;
+            }
         }
 
         private static void OnApplicationExit(object sender, EventArgs e)
