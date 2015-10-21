a3dff183dc7bf963779be80521fdc71c041fb24e
amaitland <amaitland@users.noreply.github.com>
2015-10-21 20:07:13 +1000

    Upgrade to 3.2454.1342

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ param(
     [Parameter(Position = 2)]
     [string] $AssemblyVersion = "45.0.0",
     [Parameter(Position = 3)]
-    [string] $RedistVersion = "3.2454.1333"
+    [string] $RedistVersion = "3.2454.1342"
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
