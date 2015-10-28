c20d2e4938b99b28a72700834d203d4cee40b34d
Mark <724742136@qq.com>
2015-10-28 18:51:03 +0800

    Update Program.cs

--- CefSharp.WinForms.Example/Program.cs
+++ CefSharp.WinForms.Example/Program.cs
@@ -21,8 +21,6 @@ namespace CefSharp.WinForms.Example
                                 "please make sure you compile in `Release` mode.", "Warning");
             }
 #endif
-            //坑爹呢 .NET4.5编译的项目啊  让XP怎么办呢？
-            //自己来编译源码吧 哎  又是C++
 
             const bool multiThreadedMessageLoop = true;
             CefExample.Init(false, multiThreadedMessageLoop: multiThreadedMessageLoop);
