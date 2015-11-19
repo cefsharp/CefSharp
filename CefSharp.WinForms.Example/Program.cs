db29e0bf9b05b7a9f37d540187c6c4053d3e7e10
amaitland <amaitland@users.noreply.github.com>
2015-11-19 19:03:42 +1000

    Update the WinForms Example to demo using Cef.ExecuteProcess - uses your own app executable to run the sub process - quite a few limitations with this approach currently

--- CefSharp.WinForms.Example/Program.cs
+++ CefSharp.WinForms.Example/Program.cs
@@ -12,35 +12,69 @@ namespace CefSharp.WinForms.Example
     public class Program
     {
         [STAThread]
-        public static void Main()
+        public static int Main(string[] args)
         {
-#if DEBUG
-            if (!System.Diagnostics.Debugger.IsAttached)
+            const bool simpleSubProcess = false;
+
+            //NOTE: Using a simple sub processes uses your existing application executable to spawn instances of the sub process.
+            //Features like JSB, EvaluateScriptAsync, custom schemes require the CefSharp.BrowserSubprocess to function
+            if (simpleSubProcess)
             {
-                MessageBox.Show("When running this Example outside of Visual Studio" +
-                                "please make sure you compile in `Release` mode.", "Warning");
-            }
+                var exitCode = Cef.ExecuteProcess();
+
+                if(exitCode >= 0)
+                {
+                    return exitCode;
+                }
+
+#if DEBUG
+                if (!System.Diagnostics.Debugger.IsAttached)
+                {
+                    MessageBox.Show("When running this Example outside of Visual Studio" +
+                                    "please make sure you compile in `Release` mode.", "Warning");
+                }
 #endif
 
-            const bool multiThreadedMessageLoop = true;
-            CefExample.Init(false, multiThreadedMessageLoop: multiThreadedMessageLoop);
+                var settings = new CefSettings();
+                settings.BrowserSubprocessPath = "CefSharp.WinForms.Example.exe";
 
-            if(multiThreadedMessageLoop == false)
+                Cef.Initialize(settings);
+
+                var browser = new SimpleBrowserForm();
+                Application.Run(browser);
+            }
+            else
             {
-                //http://magpcss.org/ceforum/apidocs3/projects/%28default%29/%28_globals%29.html#CefDoMessageLoopWork%28%29
-                //Perform a single iteration of CEF message loop processing.
-                //This function is used to integrate the CEF message loop into an existing application message loop.
-                //Care must be taken to balance performance against excessive CPU usage.
-                //This function should only be called on the main application thread and only if CefInitialize() is called with a CefSettings.multi_threaded_message_loop value of false.
-                //This function will not block. 
+#if DEBUG
+                if (!System.Diagnostics.Debugger.IsAttached)
+                {
+                    MessageBox.Show("When running this Example outside of Visual Studio" +
+                                    "please make sure you compile in `Release` mode.", "Warning");
+                }
+#endif
 
-                Application.Idle += (s, e) => Cef.DoMessageLoopWork();
+                const bool multiThreadedMessageLoop = true;
+                CefExample.Init(false, multiThreadedMessageLoop: multiThreadedMessageLoop);
+
+                if (multiThreadedMessageLoop == false)
+                {
+                    //http://magpcss.org/ceforum/apidocs3/projects/%28default%29/%28_globals%29.html#CefDoMessageLoopWork%28%29
+                    //Perform a single iteration of CEF message loop processing.
+                    //This function is used to integrate the CEF message loop into an existing application message loop.
+                    //Care must be taken to balance performance against excessive CPU usage.
+                    //This function should only be called on the main application thread and only if CefInitialize() is called with a CefSettings.multi_threaded_message_loop value of false.
+                    //This function will not block. 
+
+                    Application.Idle += (s, e) => Cef.DoMessageLoopWork();
+                }
+
+                var browser = new BrowserForm();
+                //var browser = new SimpleBrowserForm();
+                //var browser = new TabulationDemoForm();
+                Application.Run(browser);
             }
 
-            var browser = new BrowserForm();
-            //var browser = new SimpleBrowserForm();
-            //var browser = new TabulationDemoForm();
-            Application.Run(browser);
+            return 0;
         }
     }
 }
