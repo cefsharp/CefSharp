55209bdb8231665efa5a2aa18cfaee6b34fa0eb0
rover886 <rover886@gmail.com>
2015-11-27 16:00:57 +0530

    Formatting

--- CefSharp.Core/Internals/CefFrameWrapper.h
+++ CefSharp.Core/Internals/CefFrameWrapper.h
@@ -1,4 +1,4 @@
-// Copyright © 2010-2015 The CefSharp Project. All rights reserved.
+// Copyright Â© 2010-2015 The CefSharp Project. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
@@ -131,7 +131,9 @@ namespace CefSharp
 
             ///
             // Load the request represented by the |request| object.
-			virtual void LoadRequest(IRequest^ request);
+            ///
+            /*--cef()--*/
+	    virtual void LoadRequest(IRequest^ request);
 
             ///
             // Load the specified |url|.
@@ -237,7 +239,7 @@ namespace CefSharp
                 return _frame->GetV8Context();
             }
 
-			virtual IRequest^ CreateRequest();
+	    virtual IRequest^ CreateRequest();
         };
     }
 }
