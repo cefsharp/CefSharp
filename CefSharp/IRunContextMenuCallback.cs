2d1f23594286bb70506cdf2f3360383c2276bea1
amaitland <amaitland@users.noreply.github.com>
2016-02-10 08:26:47 +1000

    IRunContextMenuCallback implements IDisposable (checked the other callbacks, it appears this is the only one that's missing the IDisposable interface).

--- CefSharp/IRunContextMenuCallback.cs
+++ CefSharp/IRunContextMenuCallback.cs
@@ -2,9 +2,11 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+using System;
+
 namespace CefSharp
 {
-    public interface IRunContextMenuCallback
+    public interface IRunContextMenuCallback : IDisposable
     {
         /// <summary>
         /// Complete context menu display by selecting the specified commandId and eventFlags;
