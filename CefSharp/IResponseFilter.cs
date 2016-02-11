92f2fac35de497293342ebff62d3c513119d49f6
amaitland <alex@maitlandcomputing.com.au>
2016-02-11 14:55:28 +1000

    Rewrite the xml doc for IResponseFilter

--- CefSharp/IResponseFilter.cs
+++ CefSharp/IResponseFilter.cs
@@ -16,29 +16,21 @@ namespace CefSharp
         bool InitFilter();
 
         /// <summary>
-        /// Called to filter a chunk of data. |data_in| is the input buffer containing
-        /// |data_in_size| bytes of pre-filter data (|data_in| will be NULL if
-        /// |data_in_size| is zero). |data_out| is the output buffer that can accept up
-        /// to |data_out_size| bytes of filtered output data. Set |data_in_read| to the
-        /// number of bytes that were read from |data_in|. Set |data_out_written| to
-        /// the number of bytes that were written into |data_out|. If some or all of
-        /// the pre-filter data was read successfully but more data is needed in order
-        /// to continue filtering (filtered output is pending) return
-        /// RESPONSE_FILTER_NEED_MORE_DATA. If some or all of the pre-filter data was
-        /// read successfully and all available filtered output has been written return
-        /// RESPONSE_FILTER_DONE. If an error occurs during filtering return
-        /// RESPONSE_FILTER_ERROR. This method will be called repeatedly until there is
-        /// no more data to filter (resource response is complete), |data_in_read|
-        /// matches |data_in_size| (all available pre-filter bytes have been read), and
-        /// the method returns RESPONSE_FILTER_DONE or RESPONSE_FILTER_ERROR. Do not
-        /// keep a reference to the buffers passed to this method.
-        /// optional_param=data_in,default_retval=RESPONSE_FILTER_ERROR
+        /// Called to filter a chunk of data.
+        /// This method will be called repeatedly until there is no more data to filter (resource response is complete),
+        /// dataInRead matches dataIn.Length (all available pre-filter bytes have been read), and the method
+        /// returns FilterStatus.Done or FilterStatus.Error.
         /// </summary>
-        /// <param name="dataIn"></param>
-        /// <param name="dataInRead"></param>
-        /// <param name="dataOut"></param>
-        /// <param name="dataOutWritten"></param>
-        /// <returns></returns>
+        /// <param name="dataIn">is a Stream wrapping the underlying input buffer containing pre-filter data. Can be null.</param>
+        /// <param name="dataInRead">Set to the number of bytes that were read from dataIn</param>
+        /// <param name="dataOut">is a Stream wrapping the underlying output buffer that can accept filtered output data.
+        /// Check dataOut.Length for maximum buffer size</param>
+        /// <param name="dataOutWritten">Set to the number of bytes that were written into dataOut</param>
+        /// <returns>If some or all of the pre-filter data was read successfully but more data is needed in order
+        /// to continue filtering (filtered output is pending) return FilterStatus.NeedMoreData. If some or all of the pre-filter
+        /// data was read successfully and all available filtered output has been written return FilterStatus.Done. If an error
+        /// occurs during filtering return FilterStatus.Error. </returns>
+        /// <remarks>Do not keep a reference to the buffers(Steams) passed to this method.</remarks>
         FilterStatus Filter(Stream dataIn, out long dataInRead, Stream dataOut, out long dataOutWritten);
     }
 }
