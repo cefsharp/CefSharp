a09cb9b33a131ee2aa4fa5daf52e547e66c03d40
amaitland <alex@maitlandcomputing.com.au>
2016-02-17 17:22:17 +1000

    Untabify

--- CefSharp/ContextMenuMediaType.cs
+++ CefSharp/ContextMenuMediaType.cs
@@ -6,34 +6,34 @@ using System;
 
 namespace CefSharp
 {
-	/// <summary>
-	/// Supported context menu media types.
-	/// </summary>
-	public enum ContextMenuMediaType
-	{
-		/// <summary>
-		/// No special node is in context.
-		/// </summary>
-		None,
-		/// <summary>
-		/// An image node is selected.
-		/// </summary>
-		Image,
-		/// <summary>
-		/// A video node is selected.
-		/// </summary>
-		Video,
-		/// <summary>
-		/// An audio node is selected.
-		/// </summary>
-		Audio,
-		/// <summary>
-		/// A file node is selected.
-		/// </summary>
-		File,
-		/// <summary>
-		/// A plugin node is selected.
-		/// </summary>
-		Plugin,
-	}
+    /// <summary>
+    /// Supported context menu media types.
+    /// </summary>
+    public enum ContextMenuMediaType
+    {
+        /// <summary>
+        /// No special node is in context.
+        /// </summary>
+        None,
+        /// <summary>
+        /// An image node is selected.
+        /// </summary>
+        Image,
+        /// <summary>
+        /// A video node is selected.
+        /// </summary>
+        Video,
+        /// <summary>
+        /// An audio node is selected.
+        /// </summary>
+        Audio,
+        /// <summary>
+        /// A file node is selected.
+        /// </summary>
+        File,
+        /// <summary>
+        /// A plugin node is selected.
+        /// </summary>
+        Plugin,
+    }
 }
