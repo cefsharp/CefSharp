2aa5603dfb70e14c3759ced85ac0028514d7fea2
amaitland <amaitland@users.noreply.github.com>
2016-02-19 09:12:04 +1000

    Add WPF SimpleMainWindow - bare minimum example

---
+++ CefSharp.Wpf.Example/SimpleMainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CefSharp.Wpf.Example
+{
+    /// <summary>
+    /// Interaction logic for SimpleMainWindow.xaml
+    /// </summary>
+    public partial class SimpleMainWindow : Window
+    {
+        public SimpleMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
