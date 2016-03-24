f08e05a98d0fcf024c765e4536f536d7233a7a6f
Chris Bardsley <chrisbmidas@gmail.com>
2016-03-23 21:17:47 -0700

    Fix for C# paramArray and refactor on arguments mismatch

---
+++ CefSharp/Internals/MethodParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CefSharp.Internals
+{
+    [DataContract]
+    public class MethodParameter
+    {
+        public String Name { get; set; }
+
+        public Type ParameterType { get; set; }
+
+        public Boolean IsParamArray { get; set; }
+
+        public Int32 Position { get; set; }
+    }
+}
