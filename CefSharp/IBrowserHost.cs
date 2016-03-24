8f46500125cb9ec2faa702f8c9d8a86e1578f8f9
dga <me@dgallus.de>
2016-03-24 02:19:55 +0100

    add missing drag xml doc

--- CefSharp/IBrowserHost.cs
+++ CefSharp/IBrowserHost.cs
@@ -38,18 +38,25 @@ namespace CefSharp
         void DragTargetDragEnter(IDragData dragData, MouseEvent mouseEvent, DragOperationsMask allowedOperations);
 
         /// <summary>
-        /// 
+        /// Call this method each time the mouse is moved across the web view during a drag operation (after calling <see cref="DragTargetDragEnter"/> and before calling <see cref="DragTargetDragLeave"/>/<see cref="DragTargetDragDrop"/>). 
+        /// This method is only used when window rendering is disabled.
         /// </summary>
-        /// <param name="mouseEvent"></param>
-        /// <param name="allowedOperations"></param>
         void DragTargetDragOver(MouseEvent mouseEvent, DragOperationsMask allowedOperations);
 
+        /// <summary>
+        /// Call this method when the user completes the drag operation by dropping the object onto the web view (after calling <see cref="DragTargetDragEnter"/>). 
+        /// The object being dropped is <see cref="IDragData"/>, given as an argument to the previous <see cref="DragTargetDragEnter"/> call. 
+        /// This method is only used when window rendering is disabled.
+        /// </summary>
         void DragTargetDragDrop(MouseEvent mouseEvent);
 
         void DragSourceEndedAt(int x, int y, DragOperationsMask op);
 
+        /// <summary>
+        /// Call this method when the user drags the mouse out of the web view (after calling <see cref="DragTargetDragEnter"/>). This method is only used when window rendering is disabled.
+        /// </summary>
         void DragTargetDragLeave();
-
+        
         void DragSourceSystemDragEnded();
 
         /// <summary>
