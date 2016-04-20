1fc8d5e6b1bdee68a3aa450468ae5dfcf56432d5
amaitland <amaitland@users.noreply.github.com>
2016-04-21 07:56:27 +1000

    Update to version 47.0.4

--- build.ps1
+++ build.ps1
@@ -3,9 +3,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2013",
     [Parameter(Position = 1)]
-    [string] $Version = "47.0.3",
+    [string] $Version = "47.0.4",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "47.0.3",
+    [string] $AssemblyVersion = "47.0.4",
     [Parameter(Position = 3)]
     [string] $RedistVersion = "3.2526.1362"
 )
