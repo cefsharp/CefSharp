ab6f0bf48bab5c5d2751d3745ff911533f7bb630
amaitland <amaitland@users.noreply.github.com>
2016-05-30 17:46:29 +1000

    Update AssemblyInfo and build.ps1 to version 49.0.1 Not absolutely required as the build would be generated with those versions when tagged, less confusing for outsiders though

--- CefSharp/Properties/AssemblyInfo.cs
+++ CefSharp/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ namespace CefSharp
         public const bool ComVisible = false;
         public const string AssemblyCompany = "The CefSharp Authors";
         public const string AssemblyProduct = "CefSharp";
-        public const string AssemblyVersion = "49.0.0";
-        public const string AssemblyFileVersion = "49.0.0.0";
+        public const string AssemblyVersion = "49.0.1";
+        public const string AssemblyFileVersion = "49.0.1.0";
         public const string AssemblyCopyright = "Copyright © The CefSharp Authors 2010-2016";
         public const string CefSharpCoreProject = "CefSharp.Core, PublicKey=" + PublicKey;
         public const string CefSharpBrowserSubprocessProject = "CefSharp.BrowserSubprocess, PublicKey=" + PublicKey;
