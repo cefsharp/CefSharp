580a5d344affa5631c581ced874a7a34652b226b
amaitland <amaitland@users.noreply.github.com>
2016-06-03 08:54:46 +1000

    Add CefSettings xml doc

--- CefSharp.Core/CefSettings.h
+++ CefSharp.Core/CefSettings.h
@@ -10,6 +10,10 @@ using namespace System::Collections::Generic;
 
 namespace CefSharp
 {
+    /// <summary>
+    /// Initialization settings. Many of these and other settings can also configured
+    /// using command-line switches.
+    /// </summary>
     public ref class CefSettings
     {
     private:
@@ -22,6 +26,9 @@ namespace CefSharp
         bool _focusedNodeChangedEnabled;
 
     public:
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
         CefSettings() : _cefSettings(new ::CefSettings())
         {
             _cefSettings->multi_threaded_message_loop = true;
@@ -50,33 +57,75 @@ namespace CefSharp
             this->!CefSettings();
         }
 
+        /// <summary>
+        /// Add Customs schemes to this collection
+        /// </summary>
         property IEnumerable<CefCustomScheme^>^ CefCustomSchemes
         {
             IEnumerable<CefCustomScheme^>^ get() { return _cefCustomSchemes; }
         }
 
+        /// <summary>
+        /// Add CefExtensions to be registered
+        /// </summary>
         virtual property IEnumerable<CefExtension^>^ Extensions
         {
             IEnumerable<CefExtension^>^ get() { return _cefExtensions; }
         }
 
+        /// <summary>
+        /// Add custom command line argumens to this collection, they will be
+        /// added in OnBeforeCommandLineProcessing.
+        // The CefSettings.command_line_args_disabled value can be used to start with an empty command-line object. Any values specified in CefSettings that equate to command-line arguments will be set before this method is called.
+        /// </summary>
         virtual property IDictionary<String^, String^>^ CefCommandLineArgs
         {
             IDictionary<String^, String^>^ get() { return _cefCommandLineArgs; }
         }
+        
+        /// <summary>
+        /// Set to true to disable configuration of browser process features using
+        /// standard CEF and Chromium command-line arguments. Configuration can still
+        /// be specified using CEF data structures or by adding to CefCommandLineArgs
+        /// </summary>
+        property bool CommandLineArgsDisabled
+        {
+            bool get() { return _cefSettings->command_line_args_disabled == 1; }
+            void set(bool value) { _cefSettings->command_line_args_disabled = value; }
+        }
 
+        /// <summary>
+        //// Set to true to have the browser process message loop run in a separate
+        /// thread. If false than the CefDoMessageLoopWork() function must be
+        /// called from your application message loop. This option is only supported on
+        /// Windows. The default value is true
+        /// </summary>
         property bool MultiThreadedMessageLoop
         {
             bool get() { return _cefSettings->multi_threaded_message_loop == 1; }
             void set(bool value) { _cefSettings->multi_threaded_message_loop = value; }
         }
 
+        /// <summary>
+        /// The path to a separate executable that will be launched for sub-processes.
+        /// By default the browser process executable is used. See the comments on
+        /// Cef.ExecuteProcess() for details. Also configurable using the
+        /// "browser-subprocess-path" command-line switch. Default is CefSharp.BrowserSubprocess.exe
+        /// </summary>
         property String^ BrowserSubprocessPath
         {
             String^ get() { return StringUtils::ToClr(_cefSettings->browser_subprocess_path); }
             void set(String^ value) { StringUtils::AssignNativeFromClr(_cefSettings->browser_subprocess_path, value); }
         }
 
+        /// <summary>
+        /// The location where cache data will be stored on disk. If empty then
+        /// browsers will be created in "incognito mode" where in-memory caches are
+        /// used for storage and no data is persisted to disk. HTML5 databases such as
+        /// localStorage will only persist across sessions if a cache path is
+        /// specified. Can be overridden for individual CefRequestContext instances via
+        /// the RequestContextSettings.CachePath value.
+        /// </summary>
         property String^ CachePath
         {
             String^ get() { return StringUtils::ToClr(_cefSettings->cache_path); }
@@ -107,30 +156,58 @@ namespace CefSharp
             void set(bool value) { _cefSettings->ignore_certificate_errors = value; }
         }
 
+        /// <summary>
+        /// The locale string that will be passed to WebKit. If empty the default
+        /// locale of "en-US" will be used. Also configurable using the "lang"
+        /// command-line switch.
+        /// </summary>
         property String^ Locale
         {
             String^ get() { return StringUtils::ToClr(_cefSettings->locale); }
             void set(String^ value) { StringUtils::AssignNativeFromClr(_cefSettings->locale, value); }
         }
 
+        /// <summary>
+        /// The fully qualified path for the locales directory. If this value is empty
+        /// the locales directory must be located in the module directory.
+        /// Also configurable using the "locales-dir-path" command-line switch.
+        /// </summary>
         property String^ LocalesDirPath
         {
             String^ get() { return StringUtils::ToClr(_cefSettings->locales_dir_path); }
             void set(String^ value) { StringUtils::AssignNativeFromClr(_cefSettings->locales_dir_path, value); }
         }
 
+        /// <summary>
+        /// The fully qualified path for the resources directory. If this value is
+        /// empty the cef.pak and/or devtools_resources.pak files must be located in
+        /// the module directory. Also configurable using the "resources-dir-path" command-line
+        /// switch.
+        /// </summary>
         property String^ ResourcesDirPath
         {
             String^ get() { return StringUtils::ToClr(_cefSettings->resources_dir_path); }
             void set(String^ value) { StringUtils::AssignNativeFromClr(_cefSettings->resources_dir_path, value); }
         }		
 
+        /// <summary>
+        /// The directory and file name to use for the debug log. If empty a default
+        /// log file name and location will be used. On Windows and Linux a "debug.log"
+        /// file will be written in the main executable directory.
+        /// Also configurable using the"log-file" command-line switch.
+        /// </summary>
         property String^ LogFile
         {
             String^ get() { return StringUtils::ToClr(_cefSettings->log_file); }
             void set(String^ value) { StringUtils::AssignNativeFromClr(_cefSettings->log_file, value); }
         }
 
+        /// <summary>
+        /// The log severity. Only messages of this severity level or higher will be
+        /// logged. Also configurable using the "log-severity" command-line switch with
+        /// a value of "verbose", "info", "warning", "error", "error-report" or
+        /// "disable".
+        /// </summary>
         property CefSharp::LogSeverity LogSeverity
         {
             CefSharp::LogSeverity get() { return (CefSharp::LogSeverity)_cefSettings->log_severity; }
@@ -148,18 +225,38 @@ namespace CefSharp
             void set(String^ value) { StringUtils::AssignNativeFromClr(_cefSettings->javascript_flags, value); }
         }
 
+        /// <summary>
+        /// Set to true to disable loading of pack files for resources and locales.
+        /// A resource bundle handler must be provided for the browser and render
+        /// processes via CefApp::GetResourceBundleHandler() if loading of pack files
+        /// is disabled. Also configurable using the "disable-pack-loading" command-
+        /// line switch.
+        /// </summary>
         property bool PackLoadingDisabled
         {
             bool get() { return _cefSettings->pack_loading_disabled == 1; }
             void set(bool value) { _cefSettings->pack_loading_disabled = value; }
         }
 
+        /// <summary>
+        /// Value that will be inserted as the product portion of the default
+        /// User-Agent string. If empty the Chromium product version will be used. If
+        /// |userAgent| is specified this value will be ignored. Also configurable
+        /// using the "product-version" command-line switch.
+        /// </summary>
         property String^ ProductVersion
         {
             String^ get() { return StringUtils::ToClr(_cefSettings->product_version); }
             void set(String^ value) { StringUtils::AssignNativeFromClr(_cefSettings->product_version, value); }
         }
 
+        /// <summary>
+        /// Set to a value between 1024 and 65535 to enable remote debugging on the
+        /// specified port. For example, if 8080 is specified the remote debugging URL
+        /// will be http://localhost:8080. CEF can be remotely debugged from any CEF or
+        /// Chrome browser window. Also configurable using the "remote-debugging-port"
+        /// command-line switch.
+        /// </summary>
         property int RemoteDebuggingPort
         {
             int get() { return _cefSettings->remote_debugging_port; }
@@ -179,18 +276,38 @@ namespace CefSharp
             void set(int value) { _cefSettings->uncaught_exception_stack_size = value; }
         }		
 
+        /// <summary>
+        /// Value that will be returned as the User-Agent HTTP header. If empty the
+        /// default User-Agent string will be used. Also configurable using the
+        /// "user-agent" command-line switch.
+        /// </summary>
         property String^ UserAgent
         {
             String^ get() { return StringUtils::ToClr(_cefSettings->user_agent); }
             void set(String^ value) { StringUtils::AssignNativeFromClr(_cefSettings->user_agent, value); }
         }
 
+        /// <summary>
+        /// Set to true (1) to enable windowless (off-screen) rendering support. Do not
+        /// enable this value if the application does not use windowless rendering as
+        /// it may reduce rendering performance on some systems.
+        /// </summary>
         property bool WindowlessRenderingEnabled
         {
             bool get() { return _cefSettings->windowless_rendering_enabled == 1; }
             void set(bool value) { _cefSettings->windowless_rendering_enabled = value; }
         }
 
+        /// <summary>
+        /// To persist session cookies (cookies without an expiry date or validity
+        /// interval) by default when using the global cookie manager set this value to
+        /// true. Session cookies are generally intended to be transient and most
+        /// Web browsers do not persist them. A CachePath value must also be
+        /// specified to enable this feature. Also configurable using the
+        /// "persist-session-cookies" command-line switch. Can be overridden for
+        /// individual RequestContext instances via the
+        /// RequestContextSettings.PersistSessionCookies value.
+        /// </summary>
         property bool PersistSessionCookies
         {
             bool get() { return _cefSettings->persist_session_cookies == 1; }
