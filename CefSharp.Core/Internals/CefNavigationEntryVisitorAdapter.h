4912e4515568225e3371644aaf42e6808c30b4c5
amaitland <alex@maitlandcomputing.com.au>
2016-06-07 12:34:45 +1000

    Untabify CefNavigationEntryVisitorAdapter

--- CefSharp.Core/Internals/CefNavigationEntryVisitorAdapter.h
+++ CefSharp.Core/Internals/CefNavigationEntryVisitorAdapter.h
@@ -9,49 +9,49 @@
 
 namespace CefSharp
 {
-	namespace Internals
-	{
-		private class CefNavigationEntryVisitorAdapter : public CefNavigationEntryVisitor
-		{
-		private:
-			gcroot<INavigationEntryVisitor^> _handler;
+    namespace Internals
+    {
+        private class CefNavigationEntryVisitorAdapter : public CefNavigationEntryVisitor
+        {
+        private:
+            gcroot<INavigationEntryVisitor^> _handler;
 
-		public:
-			CefNavigationEntryVisitorAdapter(INavigationEntryVisitor^ handler)
-			{
-				_handler = handler;
-			}
+        public:
+            CefNavigationEntryVisitorAdapter(INavigationEntryVisitor^ handler)
+            {
+                _handler = handler;
+            }
 
-			~CefNavigationEntryVisitorAdapter()
-			{
-				delete _handler;
-				_handler = nullptr;
-			}
+            ~CefNavigationEntryVisitorAdapter()
+            {
+                delete _handler;
+                _handler = nullptr;
+            }
 
-			bool Visit(CefRefPtr<CefNavigationEntry> entry,
-				bool current,
-				int index,
-				int total) OVERRIDE
-			{
-				NavigationEntry navEntry;
+            bool Visit(CefRefPtr<CefNavigationEntry> entry,
+                bool current,
+                int index,
+                int total) OVERRIDE
+            {
+                NavigationEntry navEntry;
 
-				if (entry->IsValid())
-				{
-					auto time = entry->GetCompletionTime();
-					DateTime completionTime = CefSharp::Internals::CefTimeUtils::ConvertCefTimeToDateTime(time.GetDoubleT());
-					navEntry = NavigationEntry(current, completionTime, StringUtils::ToClr(entry->GetDisplayURL()), entry->GetHttpStatusCode(), StringUtils::ToClr(entry->GetOriginalURL()), StringUtils::ToClr(entry->GetTitle()), (TransitionType)entry->GetTransitionType(), StringUtils::ToClr(entry->GetURL()), entry->HasPostData(), true);
-				}
-				else
-				{
-					//Invalid nav entry
-					navEntry = NavigationEntry(current, DateTime::MinValue, nullptr, -1, nullptr, nullptr, (TransitionType)-1, nullptr, false, false);
-				}
+                if (entry->IsValid())
+                {
+                    auto time = entry->GetCompletionTime();
+                    DateTime completionTime = CefSharp::Internals::CefTimeUtils::ConvertCefTimeToDateTime(time.GetDoubleT());
+                    navEntry = NavigationEntry(current, completionTime, StringUtils::ToClr(entry->GetDisplayURL()), entry->GetHttpStatusCode(), StringUtils::ToClr(entry->GetOriginalURL()), StringUtils::ToClr(entry->GetTitle()), (TransitionType)entry->GetTransitionType(), StringUtils::ToClr(entry->GetURL()), entry->HasPostData(), true);
+                }
+                else
+                {
+                    //Invalid nav entry
+                    navEntry = NavigationEntry(current, DateTime::MinValue, nullptr, -1, nullptr, nullptr, (TransitionType)-1, nullptr, false, false);
+                }
 
-				return _handler->Visit(navEntry, current, index, total);
-			}
+                return _handler->Visit(navEntry, current, index, total);
+            }
 
-			IMPLEMENT_REFCOUNTING(CefNavigationEntryVisitorAdapter);
-		};
-	}
+            IMPLEMENT_REFCOUNTING(CefNavigationEntryVisitorAdapter);
+        };
+    }
 }
 
