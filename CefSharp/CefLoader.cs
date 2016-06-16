f60c6ce2ddec34592335cc286a7931506517092c
amaitland <amaitland@users.noreply.github.com>
2016-06-17 08:32:07 +1000

    Upgrade to 3.2704.1429 - Required for following changes as cef.redist targets files have changed Update CefSharp.Common.targets so it copies the cef.redist packages Add support for Win32 target (obviously uses x86 files) Update CefSharp.Native.props to copy the files also for local builds Add AnyCPU target Add CefLoader helper class for AssemblyResolver

---
+++ CefSharp/CefLoader.cs
@@ -0,0 +1,66 @@
+﻿// Copyright © 2010-2016 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace CefSharp
+{
+    /// <summary>
+    /// Used for AnyCPU Support to resolve Assemblies and Dependencies
+    /// </summary>
+    public static class CefLoader
+    {
+        /// <summary>
+        /// RegisterCefSharpAssemblyResolver
+        /// </summary>
+        public static void RegisterCefSharpAssemblyResolver()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+        }
+        
+        /// <summary>
+        /// UnRegisterCefSharpAssemblyResolver
+        /// </summary>
+        public static void UnRegisterCefSharpAssemblyResolver()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= Resolver;
+        }
+
+        /// <summary>
+        /// Helper method to map the CefSharp.BrowserSubProcess.exe file
+        /// based on current process bitness (x64 or x86)
+        /// </summary>
+        /// <returns></returns>
+        public static string GetBrowserSubProcessPath()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                   Environment.Is64BitProcess ? "x64" : "x86",
+                                                   "CefSharp.BrowserSubprocess.exe");
+
+            if(File.Exists(path))
+            {
+                throw new FileNotFoundException("Unable to locate", path);
+            }
+
+            return path;
+        }
+
+        private static Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("CefSharp"))
+            {
+                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                       Environment.Is64BitProcess ? "x64" : "x86",
+                                                       assemblyName);
+
+                return File.Exists(archSpecificPath) ? Assembly.LoadFile(archSpecificPath) : null;
+            }
+
+            return null;
+        }
+    }
+}
