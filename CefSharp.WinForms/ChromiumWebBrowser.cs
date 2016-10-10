e548faf9fd384474e39390ce2077365fdc5b154d
amaitland <alex@maitlandcomputing.com.au>
2016-10-10 12:54:10 +1000

    WinForms - Override IsKeyInput to get arrows + tab working when use MultiThreadedMessageLoop = false Solution from http://stackoverflow.com/a/5606692 Post on ceforum for background info http://magpcss.org/ceforum/viewtopic.php?f=6&t=14557

--- CefSharp.WinForms/ChromiumWebBrowser.cs
+++ CefSharp.WinForms/ChromiumWebBrowser.cs
@@ -648,5 +648,32 @@ namespace CefSharp.WinForms
 
             return browser;
         }
+
+        protected override bool IsInputKey(Keys keyData)
+        {
+            //This code block is only called/required when CEF is running in the
+            //same message loop as the WinForms UI (CefSettings.MultiThreadedMessageLoop = false)
+            //Without this code, arrows and tab won't be processed
+            switch (keyData)
+            {
+                case Keys.Right:
+                case Keys.Left:
+                case Keys.Up:
+                case Keys.Down:
+                case Keys.Tab:
+                { 
+                    return true;
+                }
+                case Keys.Shift | Keys.Right:
+                case Keys.Shift | Keys.Left:
+                case Keys.Shift | Keys.Up:
+                case Keys.Shift | Keys.Down:
+                { 
+                    return true;
+                }
+            }
+
+            return base.IsInputKey(keyData);
+        }
     }
 }
