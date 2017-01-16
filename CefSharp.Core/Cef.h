454be3cfa84838586d27926c492548709e406a4c
amaitland <alex@maitlandcomputing.com.au>
2017-01-16 11:30:37 +1000

    Add Crashreport functions https://bitbucket.org/chromiumembedded/cef/src/6e0baff75ceeb3e079b1dda4099d81573334d741/include/cef_crash_util.h?at=2883&fileviewer=file-view-default

--- CefSharp.Core/Cef.h
+++ CefSharp.Core/Cef.h
@@ -11,15 +11,16 @@
 #include <include/cef_version.h>
 #include <include/cef_origin_whitelist.h>
 #include <include/cef_web_plugin.h>
+#include <include/cef_crash_util.h>
 
 #include "Internals/CefSharpApp.h"
-#include "CookieManager.h"
 #include "Internals/PluginVisitor.h"
+#include "Internals/CefTaskScheduler.h"
+#include "Internals/CefGetGeolocationCallbackWrapper.h"
+#include "CookieManager.h"
 #include "CefSettings.h"
 #include "RequestContext.h"
 #include "SchemeHandlerFactoryWrapper.h"
-#include "Internals/CefTaskScheduler.h"
-#include "Internals/CefGetGeolocationCallbackWrapper.h"
 
 using namespace System::Collections::Generic; 
 using namespace System::Linq;
@@ -510,6 +511,101 @@ namespace CefSharp
             }
 
             return nullptr;
+        }
+
+        /// <summary>
+        ///
+        /// Crash reporting is configured using an INI-style config file named
+        /// "crash_reporter.cfg". This file must be placed next to
+        /// the main application executable. File contents are as follows:
+        ///
+        ///  # Comments start with a hash character and must be on their own line.
+        ///
+        ///  [Config]
+        ///  ProductName=<Value of the "prod" crash key; defaults to "cef">
+        ///  ProductVersion=<Value of the "ver" crash key; defaults to the CEF version>
+        ///  AppName=<Windows only; App-specific folder name component for storing crash
+        ///           information; default to "CEF">
+        ///  ExternalHandler=<Windows only; Name of the external handler exe to use
+        ///                   instead of re-launching the main exe; default to empty>
+        ///  ServerURL=<crash server URL; default to empty>
+        ///  RateLimitEnabled=<True if uploads should be rate limited; default to true>
+        ///  MaxUploadsPerDay=<Max uploads per 24 hours, used if rate limit is enabled;
+        ///                    default to 5>
+        ///  MaxDatabaseSizeInMb=<Total crash report disk usage greater than this value
+        ///                       will cause older reports to be deleted; default to 20>
+        ///  MaxDatabaseAgeInDays=<Crash reports older than this value will be deleted;
+        ///                        default to 5>
+        ///
+        ///  [CrashKeys]
+        ///  my_key1=<small|medium|large>
+        ///  my_key2=<small|medium|large>
+        ///
+        /// Config section:
+        ///
+        /// If "ProductName" and/or "ProductVersion" are set then the specified values
+        /// will be included in the crash dump metadata. 
+        ///
+        /// If "AppName" is set on Windows then crash report information (metrics,
+        /// database and dumps) will be stored locally on disk under the
+        /// "C:\Users\[CurrentUser]\AppData\Local\[AppName]\User Data" folder. On other
+        /// platforms the CefSettings.user_data_path value will be used.
+        ///
+        /// If "ExternalHandler" is set on Windows then the specified exe will be
+        /// launched as the crashpad-handler instead of re-launching the main process
+        /// exe. The value can be an absolute path or a path relative to the main exe
+        /// directory. 
+        ///
+        /// If "ServerURL" is set then crashes will be uploaded as a multi-part POST
+        /// request to the specified URL. Otherwise, reports will only be stored locally
+        /// on disk.
+        ///
+        /// If "RateLimitEnabled" is set to true then crash report uploads will be rate
+        /// limited as follows:
+        ///  1. If "MaxUploadsPerDay" is set to a positive value then at most the
+        ///     specified number of crashes will be uploaded in each 24 hour period.
+        ///  2. If crash upload fails due to a network or server error then an
+        ///     incremental backoff delay up to a maximum of 24 hours will be applied for
+        ///     retries.
+        ///  3. If a backoff delay is applied and "MaxUploadsPerDay" is > 1 then the
+        ///     "MaxUploadsPerDay" value will be reduced to 1 until the client is
+        ///     restarted. This helps to avoid an upload flood when the network or
+        ///     server error is resolved.
+        ///
+        /// If "MaxDatabaseSizeInMb" is set to a positive value then crash report storage
+        /// on disk will be limited to that size in megabytes. For example, on Windows
+        /// each dump is about 600KB so a "MaxDatabaseSizeInMb" value of 20 equates to
+        /// about 34 crash reports stored on disk.
+        ///
+        /// If "MaxDatabaseAgeInDays" is set to a positive value then crash reports older
+        /// than the specified age in days will be deleted.
+        ///
+        /// CrashKeys section:
+        ///
+        /// Any number of crash keys can be specified for use by the application. Crash
+        /// key values will be truncated based on the specified size (small = 63 bytes,
+        /// medium = 252 bytes, large = 1008 bytes). The value of crash keys can be set
+        /// from any thread or process using the Cef.SetCrashKeyValue function. These
+        /// key/value pairs will be sent to the crash server along with the crash dump
+        /// file. Medium and large values will be chunked for submission. For example,
+        /// if your key is named "mykey" then the value will be broken into ordered
+        /// chunks and submitted using keys named "mykey-1", "mykey-2", etc.
+        /// </summary>
+        /// <return>Returns the global request context or null.</return>
+        static property bool CrashReportingEnabled
+        {
+            bool get()
+            {
+                return CefCrashReportingEnabled();
+            }
+        }
+
+        /// <summary>
+        /// Sets or clears a specific key-value pair from the crash metadata.
+        /// </summary>
+        static void SetCrashKeyValue(String^ key, String^ value)
+        {
+            CefSetCrashKeyValue(StringUtils::ToNative(key), StringUtils::ToNative(value));
         }
     };
 }
