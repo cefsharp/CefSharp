d3e5249b9fb2cac31c868f260a30edf375b4152c
amaitland <alex@maitlandcomputing.com.au>
2017-03-02 14:14:24 +1000

    Move most of the logic from CefSharpSchemeHandler into CefSharpSchemeHandlerFactory and use the ResourceHandler.FromStream/FromString methods instead People keep implement IResourceHandler directly, so update the examples in hopes they use the build in methods

--- CefSharp.Example/CefSharpSchemeHandler.cs
+++ CefSharp.Example/CefSharpSchemeHandler.cs
@@ -13,170 +13,59 @@ using CefSharp.Example.Properties;
 
 namespace CefSharp.Example
 {
-    internal class CefSharpSchemeHandler : IResourceHandler
+    internal class CefSharpSchemeHandler : ResourceHandler
     {
-        private static readonly IDictionary<string, string> ResourceDictionary;
-
-        private string mimeType;
-        private MemoryStream stream;
-        
-        static CefSharpSchemeHandler()
+        public override bool ProcessRequestAsync(IRequest request, ICallback callback)
         {
-            ResourceDictionary = new Dictionary<string, string>
-            {
-                { "/home.html", Resources.home_html },
-
-                { "/assets/css/shCore.css", Resources.assets_css_shCore_css },
-                { "/assets/css/shCoreDefault.css", Resources.assets_css_shCoreDefault_css },
-                { "/assets/css/docs.css", Resources.assets_css_docs_css },
-                { "/assets/js/application.js", Resources.assets_js_application_js },
-                { "/assets/js/jquery.js", Resources.assets_js_jquery_js },
-                { "/assets/js/shBrushCSharp.js", Resources.assets_js_shBrushCSharp_js },
-                { "/assets/js/shBrushJScript.js", Resources.assets_js_shBrushJScript_js },
-                { "/assets/js/shCore.js", Resources.assets_js_shCore_js },
-
-                { "/bootstrap/bootstrap-theme.min.css", Resources.bootstrap_theme_min_css },
-                { "/bootstrap/bootstrap.min.css", Resources.bootstrap_min_css },
-                { "/bootstrap/bootstrap.min.js", Resources.bootstrap_min_js },
-
-                { "/BindingTest.html", Resources.BindingTest },
-                { "/ExceptionTest.html", Resources.ExceptionTest },
-                { "/PopupTest.html", Resources.PopupTest },
-                { "/SchemeTest.html", Resources.SchemeTest },
-                { "/TooltipTest.html", Resources.TooltipTest },
-                { "/FramedWebGLTest.html", Resources.FramedWebGLTest },
-                { "/MultiBindingTest.html", Resources.MultiBindingTest },
-                { "/ScriptedMethodsTest.html", Resources.ScriptedMethodsTest },
-                { "/ResponseFilterTest.html", Resources.ResponseFilterTest },
-                { "/DraggableRegionTest.html", Resources.DraggableRegionTest },
-                { "/CssAnimationTest.html", Resources.CssAnimation },
-                { "/CdmSupportTest.html", Resources.CdmSupportTest }
-            };
-        }
-
-        bool IResourceHandler.ProcessRequest(IRequest request, ICallback callback)
-        {
-            // The 'host' portion is entirely ignored by this scheme handler.
             var uri = new Uri(request.Url);
             var fileName = uri.AbsolutePath;
 
-            if (string.Equals(fileName, "/PostDataTest.html", StringComparison.OrdinalIgnoreCase))
+            Task.Run(() =>
             {
-                var postDataElement = request.PostData.Elements.FirstOrDefault();
-                var resourceHandler = ResourceHandler.FromString("Post Data: " + (postDataElement == null ? "null" : postDataElement.GetBody()));
-                stream = (MemoryStream)resourceHandler.Stream;
-                mimeType = "text/html";
-                callback.Continue();
-                return true;
-            }
-
-            if (string.Equals(fileName, "/PostDataAjaxTest.html", StringComparison.OrdinalIgnoreCase))
-            {
-                var postData = request.PostData;
-                if (postData == null)
+                using (callback)
                 {
-                    var resourceHandler = ResourceHandler.FromString("Post Data: null");
-                    stream = (MemoryStream)resourceHandler.Stream;
-                    mimeType = "text/html";
-                    callback.Continue();
-                }
-                else
-                {
-                    var postDataElement = postData.Elements.FirstOrDefault();
-                    var resourceHandler = ResourceHandler.FromString("Post Data: " + (postDataElement == null ? "null" : postDataElement.GetBody()));
-                    stream = (MemoryStream)resourceHandler.Stream;
-                    mimeType = "text/html";
-                    callback.Continue();
-                }
+                    Stream stream = null;
 
-                return true;
-            }
-
-            if (string.Equals(fileName, "/EmptyResponseFilterTest.html", StringComparison.OrdinalIgnoreCase))
-            {
-                stream = null;
-                mimeType = "text/html";
-                callback.Continue();
-
-                return true;
-            }
-
-            string resource;
-            if (ResourceDictionary.TryGetValue(fileName, out resource) && !string.IsNullOrEmpty(resource))
-            {
-                Task.Run(() =>
-                {
-                    using (callback)
+                    if (string.Equals(fileName, "/PostDataTest.html", StringComparison.OrdinalIgnoreCase))
                     {
-                        var bytes = Encoding.UTF8.GetBytes(resource);
-                        stream = new MemoryStream(bytes);
+                        var postDataElement = request.PostData.Elements.FirstOrDefault();
+                        stream = ResourceHandler.GetMemoryStream("Post Data: " + (postDataElement == null ? "null" : postDataElement.GetBody()), Encoding.UTF8);
+                    }
 
-                        var fileExtension = Path.GetExtension(fileName);
-                        mimeType = ResourceHandler.GetMimeType(fileExtension);
+                    if (string.Equals(fileName, "/PostDataAjaxTest.html", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var postData = request.PostData;
+                        if (postData == null)
+                        {
+                            stream = ResourceHandler.GetMemoryStream("Post Data: null", Encoding.UTF8);
+                        }
+                        else
+                        {
+                            var postDataElement = postData.Elements.FirstOrDefault();
+                            stream = ResourceHandler.GetMemoryStream("Post Data: " + (postDataElement == null ? "null" : postDataElement.GetBody()), Encoding.UTF8);
+                        }
+                    }
+
+                    if(stream == null)
+                    {
+                        callback.Cancel();
+                    }
+                    else
+                    { 
+                        //Reset the stream position to 0 so the stream can be copied into the underlying unmanaged buffer
+                        stream.Position = 0;
+                        //Populate the response values - No longer need to implement GetResponseHeaders (unless you need to perform a redirect)
+                        ResponseLength = stream.Length;
+                        MimeType = "text/html";
+                        StatusCode = (int)HttpStatusCode.OK;
+                        Stream = stream;
 
                         callback.Continue();
                     }
-                });
+                }
+            });
 
-                return true;
-            }
-            else
-            {
-                callback.Dispose();
-            }
-
-            return false;
-        }
-        
-
-        void IResourceHandler.GetResponseHeaders(IResponse response, out long responseLength, out string redirectUrl)
-        {
-            responseLength = stream == null ? 0 : stream.Length;
-            redirectUrl = null;
-
-            response.StatusCode = (int)HttpStatusCode.OK;
-            response.StatusText = "OK";
-            response.MimeType = mimeType;
-        }
-
-        bool IResourceHandler.ReadResponse(Stream dataOut, out int bytesRead, ICallback callback)
-        {
-            //Dispose the callback as it's an unmanaged resource, we don't need it in this case
-            callback.Dispose();
-
-            if(stream == null)
-            {
-                bytesRead = 0;
-                return false;
-            }
-
-            //Data out represents an underlying buffer (typically 32kb in size).
-            var buffer = new byte[dataOut.Length];
-            bytesRead = stream.Read(buffer, 0, buffer.Length);
-            
-            dataOut.Write(buffer, 0, buffer.Length);
-
-            return bytesRead > 0;
-        }
-
-        bool IResourceHandler.CanGetCookie(Cookie cookie)
-        {
             return true;
-        }
-
-        bool IResourceHandler.CanSetCookie(Cookie cookie)
-        {
-            return true;
-        }
-
-        void IResourceHandler.Cancel()
-        {
-            
-        }
-
-        void IDisposable.Dispose()
-        {
-            
         }
     }
 }
--- CefSharp.Example/CefSharpSchemeHandlerFactory.cs
+++ CefSharp.Example/CefSharpSchemeHandlerFactory.cs
@@ -2,6 +2,12 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using CefSharp.Example.Properties;
+
 namespace CefSharp.Example
 {
     public class CefSharpSchemeHandlerFactory : ISchemeHandlerFactory
@@ -9,9 +15,55 @@ namespace CefSharp.Example
         public const string SchemeName = "custom";
         public const string SchemeNameTest = "test";
 
+        private static readonly IDictionary<string, string> ResourceDictionary;
+
+        static CefSharpSchemeHandlerFactory()
+        {
+            ResourceDictionary = new Dictionary<string, string>
+            {
+                { "/home.html", Resources.home_html },
+
+                { "/assets/css/shCore.css", Resources.assets_css_shCore_css },
+                { "/assets/css/shCoreDefault.css", Resources.assets_css_shCoreDefault_css },
+                { "/assets/css/docs.css", Resources.assets_css_docs_css },
+                { "/assets/js/application.js", Resources.assets_js_application_js },
+                { "/assets/js/jquery.js", Resources.assets_js_jquery_js },
+                { "/assets/js/shBrushCSharp.js", Resources.assets_js_shBrushCSharp_js },
+                { "/assets/js/shBrushJScript.js", Resources.assets_js_shBrushJScript_js },
+                { "/assets/js/shCore.js", Resources.assets_js_shCore_js },
+
+                { "/bootstrap/bootstrap-theme.min.css", Resources.bootstrap_theme_min_css },
+                { "/bootstrap/bootstrap.min.css", Resources.bootstrap_min_css },
+                { "/bootstrap/bootstrap.min.js", Resources.bootstrap_min_js },
+
+                { "/BindingTest.html", Resources.BindingTest },
+                { "/ExceptionTest.html", Resources.ExceptionTest },
+                { "/PopupTest.html", Resources.PopupTest },
+                { "/SchemeTest.html", Resources.SchemeTest },
+                { "/TooltipTest.html", Resources.TooltipTest },
+                { "/FramedWebGLTest.html", Resources.FramedWebGLTest },
+                { "/MultiBindingTest.html", Resources.MultiBindingTest },
+                { "/ScriptedMethodsTest.html", Resources.ScriptedMethodsTest },
+                { "/ResponseFilterTest.html", Resources.ResponseFilterTest },
+                { "/DraggableRegionTest.html", Resources.DraggableRegionTest },
+                { "/CssAnimationTest.html", Resources.CssAnimation },
+                { "/CdmSupportTest.html", Resources.CdmSupportTest }
+            };
+        }
+
         public IResourceHandler Create(IBrowser browser, IFrame frame, string schemeName, IRequest request)
         {
-            if (schemeName == SchemeName && request.Url.EndsWith("CefSharp.Core.xml", System.StringComparison.OrdinalIgnoreCase))
+            //Notes:
+            // - The 'host' portion is entirely ignored by this scheme handler.
+            // - If you register a ISchemeHandlerFactory for http/https schemes you should also specify a domain name
+            // - Avoid doing lots of processing in this method as it will affect performance.
+            // - Use the Default ResourceHandler implementation
+
+            var uri = new Uri(request.Url);
+            var fileName = uri.AbsolutePath;
+
+            //Load a file directly from Disk
+            if (fileName.EndsWith("CefSharp.Core.xml", StringComparison.OrdinalIgnoreCase))
             {
                 //Convenient helper method to lookup the mimeType
                 var mimeType = ResourceHandler.GetMimeType(".xml");
@@ -19,7 +71,26 @@ namespace CefSharp.Example
                 //mimeType is optional and will default to text/html
                 return ResourceHandler.FromFilePath("CefSharp.Core.xml", mimeType);
             }
-            return new CefSharpSchemeHandler();
+
+            if (string.Equals(fileName, "/PostDataTest.html", StringComparison.OrdinalIgnoreCase) ||
+                string.Equals(fileName, "/PostDataAjaxTest.html", StringComparison.OrdinalIgnoreCase))
+            {
+                return new CefSharpSchemeHandler();
+            }
+
+            if (string.Equals(fileName, "/EmptyResponseFilterTest.html", StringComparison.OrdinalIgnoreCase))
+            {
+                return ResourceHandler.FromString("", ".html");
+            }
+
+            string resource;
+            if (ResourceDictionary.TryGetValue(fileName, out resource) && !string.IsNullOrEmpty(resource))
+            {
+                var fileExtension = Path.GetExtension(fileName);
+                return ResourceHandler.FromString(resource, fileExtension);
+            }
+
+            return null;
         }
     }
 }
