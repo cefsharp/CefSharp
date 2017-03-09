42386cfac9da1c2bef3770ee32f526b1c0c264a4
João Neves <joao.neves@outsystems.com>
2017-03-09 15:56:20 +0000

    Support serilization of struct arrays to JS side

--- CefSharp.Example/AsyncBoundObject.cs
+++ CefSharp.Example/AsyncBoundObject.cs
@@ -8,6 +8,10 @@ using System.Threading;
 
 namespace CefSharp.Example
 {
+    public struct JsObject {
+        public string Value;
+    }
+
     public class AsyncBoundObject
     {
         //We expect an exception here, so tell VS to ignore
@@ -32,6 +36,13 @@ namespace CefSharp.Example
         public void DoSomething()
         {
             Thread.Sleep(1000);
+        }
+
+        public JsObject[] ObjectArray(string name) {
+            return new[] {
+                new JsObject() { Value = "Item1" },
+                new JsObject() { Value = "Item2" }
+            };
         }
     }
 }
