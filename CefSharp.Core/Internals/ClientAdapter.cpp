f93a387593f3dfc34c5165e9ea07cf3c11f7c221
amaitland <amaitland@users.noreply.github.com>
2017-05-16 23:13:06 +1000

    Use fully qualified cli::array as it appears VS2015 gets confused with the generic std::array (must be in the newer C++ library as code builds fine in VS2013)
    Resolves #2049

--- CefSharp.Core/Internals/ClientAdapter.cpp
+++ CefSharp.Core/Internals/ClientAdapter.cpp
@@ -724,7 +724,8 @@ namespace CefSharp
             {
                 auto resourceHandler = static_cast<ByteArrayResourceHandler^>(handler);
                 
-                array<Byte>^ buffer = resourceHandler->Data;
+                //NOTE: Prefix with cli:: namespace as VS2015 gets confused with std::array
+				cli::array<Byte>^ buffer = resourceHandler->Data;
                 pin_ptr<Byte> src = &buffer[0];
 
                 auto streamReader = CefStreamReader::CreateForData(static_cast<void*>(src), buffer->Length);
