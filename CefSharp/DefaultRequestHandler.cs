9e6a975690abb715904bbb61f8bb5ee67c06ac12
martinekvili <martinekvili@users.noreply.github.com>
2017-10-09 17:48:11 +0200

    Added default implementation of IRequestHandler

---
+++ CefSharp/DefaultRequestHandler.cs
@@ -0,0 +1,96 @@
+﻿// Copyright © 2010-2017 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace CefSharp
+{
+    /// <summary>
+    /// Default implementation of <see cref="IRequestHandler"/>.
+    /// This class provides default implementations of the methods from <see cref="IRequestHandler"/>,
+    /// therefore providing a convenience base class for any custom request handler.
+    /// </summary>
+    public class DefaultRequestHandler : IRequestHandler
+    {
+        public bool GetAuthCredentials(IWebBrowser browserControl, IBrowser browser, IFrame frame, bool isProxy, string host,
+            int port, string realm, string scheme, IAuthCallback callback)
+        {
+            callback.Dispose();
+            return false;
+        }
+
+        public IResponseFilter GetResourceResponseFilter(IWebBrowser browserControl, IBrowser browser, IFrame frame,
+            IRequest request, IResponse response)
+        {
+            return null;
+        }
+
+        public bool OnBeforeBrowse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, bool isRedirect)
+        {
+            return false;
+        }
+
+        public CefReturnValue OnBeforeResourceLoad(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
+            IRequestCallback callback)
+        {
+            callback.Dispose();
+            return CefReturnValue.Continue;
+        }
+
+        public bool OnCertificateError(IWebBrowser browserControl, IBrowser browser, CefErrorCode errorCode, string requestUrl,
+            ISslInfo sslInfo, IRequestCallback callback)
+        {
+            callback.Dispose();
+            return false;
+        }
+
+        public bool OnOpenUrlFromTab(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl,
+            WindowOpenDisposition targetDisposition, bool userGesture)
+        {
+            return false;
+        }
+
+        public void OnPluginCrashed(IWebBrowser browserControl, IBrowser browser, string pluginPath)
+        { }
+
+        public bool OnProtocolExecution(IWebBrowser browserControl, IBrowser browser, string url)
+        {
+            return false;
+        }
+
+        public bool OnQuotaRequest(IWebBrowser browserControl, IBrowser browser, string originUrl, long newSize,
+            IRequestCallback callback)
+        {
+            callback.Dispose();
+            return false;
+        }
+
+        public void OnRenderProcessTerminated(IWebBrowser browserControl, IBrowser browser, CefTerminationStatus status)
+        { }
+
+        public void OnRenderViewReady(IWebBrowser browserControl, IBrowser browser)
+        { }
+
+        public void OnResourceLoadComplete(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
+            IResponse response, UrlRequestStatus status, long receivedContentLength)
+        { }
+
+        public void OnResourceRedirect(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
+            IResponse response, ref string newUrl)
+        { }
+
+        public bool OnResourceResponse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
+            IResponse response)
+        {
+            return false;
+        }
+
+        public bool OnSelectClientCertificate(IWebBrowser browserControl, IBrowser browser, bool isProxy, string host, int port,
+            X509Certificate2Collection certificates, ISelectClientCertificateCallback callback)
+        {
+            callback.Dispose();
+            return false;
+        }
+    }
+}
