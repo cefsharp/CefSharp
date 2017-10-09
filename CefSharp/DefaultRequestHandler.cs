9dbf53ea99beae441d22200d8833d4351c4b9d9a
martinekvili <martinekvili@users.noreply.github.com>
2017-10-09 18:37:55 +0200

    Functions made virtual

--- CefSharp/DefaultRequestHandler.cs
+++ CefSharp/DefaultRequestHandler.cs
@@ -13,80 +13,80 @@ namespace CefSharp
     /// </summary>
     public class DefaultRequestHandler : IRequestHandler
     {
-        public bool GetAuthCredentials(IWebBrowser browserControl, IBrowser browser, IFrame frame, bool isProxy, string host,
+        public virtual bool GetAuthCredentials(IWebBrowser browserControl, IBrowser browser, IFrame frame, bool isProxy, string host,
             int port, string realm, string scheme, IAuthCallback callback)
         {
             callback.Dispose();
             return false;
         }
 
-        public IResponseFilter GetResourceResponseFilter(IWebBrowser browserControl, IBrowser browser, IFrame frame,
+        public virtual IResponseFilter GetResourceResponseFilter(IWebBrowser browserControl, IBrowser browser, IFrame frame,
             IRequest request, IResponse response)
         {
             return null;
         }
 
-        public bool OnBeforeBrowse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, bool isRedirect)
+        public virtual bool OnBeforeBrowse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, bool isRedirect)
         {
             return false;
         }
 
-        public CefReturnValue OnBeforeResourceLoad(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
+        public virtual CefReturnValue OnBeforeResourceLoad(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
             IRequestCallback callback)
         {
             callback.Dispose();
             return CefReturnValue.Continue;
         }
 
-        public bool OnCertificateError(IWebBrowser browserControl, IBrowser browser, CefErrorCode errorCode, string requestUrl,
+        public virtual bool OnCertificateError(IWebBrowser browserControl, IBrowser browser, CefErrorCode errorCode, string requestUrl,
             ISslInfo sslInfo, IRequestCallback callback)
         {
             callback.Dispose();
             return false;
         }
 
-        public bool OnOpenUrlFromTab(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl,
+        public virtual bool OnOpenUrlFromTab(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl,
             WindowOpenDisposition targetDisposition, bool userGesture)
         {
             return false;
         }
 
-        public void OnPluginCrashed(IWebBrowser browserControl, IBrowser browser, string pluginPath)
+        public virtual void OnPluginCrashed(IWebBrowser browserControl, IBrowser browser, string pluginPath)
         { }
 
-        public bool OnProtocolExecution(IWebBrowser browserControl, IBrowser browser, string url)
+        public virtual bool OnProtocolExecution(IWebBrowser browserControl, IBrowser browser, string url)
         {
             return false;
         }
 
-        public bool OnQuotaRequest(IWebBrowser browserControl, IBrowser browser, string originUrl, long newSize,
+        public virtual bool OnQuotaRequest(IWebBrowser browserControl, IBrowser browser, string originUrl, long newSize,
             IRequestCallback callback)
         {
             callback.Dispose();
             return false;
         }
 
-        public void OnRenderProcessTerminated(IWebBrowser browserControl, IBrowser browser, CefTerminationStatus status)
+        public virtual void OnRenderProcessTerminated(IWebBrowser browserControl, IBrowser browser, CefTerminationStatus status)
         { }
 
-        public void OnRenderViewReady(IWebBrowser browserControl, IBrowser browser)
+        public virtual void OnRenderViewReady(IWebBrowser browserControl, IBrowser browser)
         { }
 
-        public void OnResourceLoadComplete(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
+        public virtual void OnResourceLoadComplete(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
             IResponse response, UrlRequestStatus status, long receivedContentLength)
         { }
 
-        public void OnResourceRedirect(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
+        public virtual void OnResourceRedirect(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
             IResponse response, ref string newUrl)
         { }
 
-        public bool OnResourceResponse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
+        public virtual bool OnResourceResponse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request,
             IResponse response)
         {
             return false;
         }
 
-        public bool OnSelectClientCertificate(IWebBrowser browserControl, IBrowser browser, bool isProxy, string host, int port,
+        public virtual bool OnSelectClientCertificate(IWebBrowser browserControl, IBrowser browser, bool isProxy, string host, int port,
             X509Certificate2Collection certificates, ISelectClientCertificateCallback callback)
         {
             callback.Dispose();
