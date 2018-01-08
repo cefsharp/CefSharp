c303e1ca0f5996be3130d8d9f7ed0bbd36f877b1
amaitland <amaitland@users.noreply.github.com>
2018-01-08 15:12:52 +1000

    JSB - Add support for async methods to return classes
    Structs and classes can now be returned from an async method
    Properties and fields are supported now. The returned class/struct is readonly effectively, no updates will be sent to the bound object.
    
    Just to clarify this is only for objects bound using RegisterAsyncJsObject

---
+++ CefSharp.Example/JsSerializableClass.cs
@@ -0,0 +1,11 @@
+﻿// Copyright © 2010-2017 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+namespace CefSharp.Example
+{
+    public class JsSerializableClass
+    {
+        public string Value { get; set; }
+    }
+}
