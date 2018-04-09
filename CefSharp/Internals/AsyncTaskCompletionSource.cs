ec98f0d01b959d3782d97304e4f68428de23a74f
louis <shkdee@users.noreply.github.com>
2018-04-09 14:56:11 +0200

    comments and code style

--- CefSharp/Internals/AsyncTaskCompletionSource.cs
+++ CefSharp/Internals/AsyncTaskCompletionSource.cs
@@ -9,9 +9,9 @@ using System.Threading.Tasks;
 namespace CefSharp
 {
     /// <summary>
-    /// Wraps <see cref="TaskCompletionSource{T}"/> by providing a way to set its result in an async fashion.
-    /// This prevents the Task Continuation being executed sync on the same thread, which may be required when
-    /// continuations must not run on CEF UI threads.
+    /// Wraps <see cref="TaskCompletionSource{T}"/> by providing a way to set its result in an asynchronous
+    /// fashion. This prevents the resulting Task continuations from being executed synchronously on the same
+    /// thread as the one completing the Task, which may be better when that thread is a CEF UI thread.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class AsyncTaskCompletionSource<T>
@@ -20,21 +20,24 @@ namespace CefSharp
         private int resultSet = 0;
 
         /// <summary>
-        /// Gets the <see cref="Task{TResult}"/> created by this <see cref="AsyncTaskCompletionSource{T}"/>
+        /// Gets the <see cref="Task{TResult}"/> created by the wrapped <see cref="AsyncTaskCompletionSource{T}"/> instance.
         /// </summary>
         /// <seealso cref="TaskCompletionSource{TResult}.Task"/>
         public Task<T> Task => tcs.Task;
 
         /// <summary>
-        /// Set the TaskCompletionSource in an async fashion. This prevents the Task Continuation being executed
-        /// sync on the same thread. This is required otherwise contintinuations will happen on CEF UI threads.
+        /// Sets the wrapped <see cref="TaskCompletionSource{T}"/> instance result in an asynchronous fashion.
+        /// This prevents the Task continuations from being executed synchronously on the same thread as the
+        /// one calling this method, which is required when this thread is a CEF UI thread.
         /// </summary>
         /// <param name="result">result</param>
         public void TrySetResultAsync(T result)
         {
             if (Interlocked.Exchange(ref resultSet, 1) == 0)
+            {
                 System.Threading.Tasks.Task.Factory.StartNew(() => tcs.TrySetResult(result),
                     CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default);
+            }
         }
     }
 }
