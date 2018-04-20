b492905ceabc294560f5c59f3cd2cef0d78d8d14
amaitland <alex@maitlandcomputing.com.au>
2018-04-20 12:48:31 +1000

    Code cleanup - Move some structs and enums into separate namespace
    - Move the less likely to be used structs/enums into the relevant namespaces, would ideally like to move them all, would likely cause a few issues
    for some when upgrading though, so deferring for now.
    - Remove Geoposition as it's no longer used (Geolocation API was removed in CEF)

--- CefSharp/Enums/ColorType.cs
+++ CefSharp/Enums/ColorType.cs
@@ -2,7 +2,7 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-namespace CefSharp
+namespace CefSharp.Enums
 {
     /// <summary>
     /// Describes how to interpret the components of a pixel.
