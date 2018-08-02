391fe825d4898a06b482db62513f4d110c364921
alfein <alex.feinberg@outlook.com>
2018-08-01 23:21:02 -0400

    GetScreenInfo - Allow setting of Rect/AvaliableRect (#2474)
    * added some code to send monitor information back to the cef
    
    * reverted framework target change
    
    * small cleanup.  Handling location changed of the window to force refresh screeninfo
    
    * Fixed formatting of C++ code, added comments, removed code from samples
    
    * removed extra white space

--- CefSharp/Structs/ScreenInfo.cs
+++ CefSharp/Structs/ScreenInfo.cs
@@ -7,10 +7,40 @@ namespace CefSharp.Structs
     public struct ScreenInfo
     {
         public float ScaleFactor { get; private set; }
-        
+
         public ScreenInfo(float scaleFactor) : this()
         {
             ScaleFactor = scaleFactor;
         }
+
+        /// <summary>
+        /// Height of the monitor nearest to the window hosting the browser
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Width of the monitor nearest to the window hosting the browser
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Available Left location  of the monitor nearest to the window hosting the browser.  iF toolbar is on the left - this would be a non-zero value
+        /// </summary>
+        public int AvailableLeft { get; set; }
+
+        /// <summary>
+        /// Available Top location  of the monitor nearest to the window hosting the browser.  iF toolbar is on top - this would be a non-zero value
+        /// </summary>
+        public int AvailableTop { get; set; }
+
+        /// <summary>
+        /// Available Wdith of the monitor nearest to the window hosting the browser.  iF toolbar is on the left or on the right - this would be different from the <see cref="Width">Width</see> property
+        /// </summary>
+        public int AvailableWidth { get; set; }
+
+        /// <summary>
+        /// Available Heiht of the monitor nearest to the window hosting the browser.  iF toolbar is on top or on the bottom - this would be different from the <see cref="Height">Height</see> property
+        /// </summary>
+        public int AvailableHeight { get; set; }
     }
 }
