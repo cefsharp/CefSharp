a373852e287a262c605fe43252ba2836ea3af963
amaitland <amaitland@users.noreply.github.com>
2018-08-24 19:13:59 +1000

    OffScreen - Untabify

--- CefSharp.OffScreen/BitmapBuffer.cs
+++ CefSharp.OffScreen/BitmapBuffer.cs
@@ -11,86 +11,86 @@ using CefSharp.Structs;
 
 namespace CefSharp.OffScreen
 {
-	public class BitmapBuffer
-	{
-		private const int BytesPerPixel = 4;
-		private const PixelFormat Format = PixelFormat.Format32bppPArgb;
+    public class BitmapBuffer
+    {
+        private const int BytesPerPixel = 4;
+        private const PixelFormat Format = PixelFormat.Format32bppPArgb;
 
-		private byte[] buffer;
+        private byte[] buffer;
 
-		/// <summary>
-		/// Number of bytes
-		/// </summary>
-		public int NumberOfBytes { get; private set; }
-		/// <summary>
-		/// Width
-		/// </summary>
-		public int Width { get; private set; }
-		/// <summary>
-		/// Height
-		/// </summary>
-		public int Height { get; private set; }
-		/// <summary>
-		/// Dirty Rect - unified region containing th
-		/// </summary>
-		public Rect DirtyRect { get; private set; }
-		
-		public object BitmapLock { get; private set; }
+        /// <summary>
+        /// Number of bytes
+        /// </summary>
+        public int NumberOfBytes { get; private set; }
+        /// <summary>
+        /// Width
+        /// </summary>
+        public int Width { get; private set; }
+        /// <summary>
+        /// Height
+        /// </summary>
+        public int Height { get; private set; }
+        /// <summary>
+        /// Dirty Rect - unified region containing th
+        /// </summary>
+        public Rect DirtyRect { get; private set; }
+        
+        public object BitmapLock { get; private set; }
 
-		public BitmapBuffer(object bitmapLock)
-		{
-			BitmapLock = bitmapLock;
-		}
+        public BitmapBuffer(object bitmapLock)
+        {
+            BitmapLock = bitmapLock;
+        }
 
-		public byte[] Buffer
-		{
-			get { return buffer; }
-		}
+        public byte[] Buffer
+        {
+            get { return buffer; }
+        }
 
-		//TODO: May need to Pin the buffer in memory using GCHandle.Alloc(this.buffer, GCHandleType.Pinned);
-		private void ResizeBuffer(int width, int height)
-		{
-			if (buffer == null || width != Width || height != Height)
-			{
-				//No of Pixels (width * height) * BytesPerPixel
-				NumberOfBytes = width * height * BytesPerPixel;
+        //TODO: May need to Pin the buffer in memory using GCHandle.Alloc(this.buffer, GCHandleType.Pinned);
+        private void ResizeBuffer(int width, int height)
+        {
+            if (buffer == null || width != Width || height != Height)
+            {
+                //No of Pixels (width * height) * BytesPerPixel
+                NumberOfBytes = width * height * BytesPerPixel;
 
-				buffer = new byte[NumberOfBytes];
+                buffer = new byte[NumberOfBytes];
 
-				Width = width;
-				Height = height;
-			}
-		}
+                Width = width;
+                Height = height;
+            }
+        }
 
-		public void UpdateBuffer(int width, int height, IntPtr buffer, Rect dirtyRect)
-		{
-			lock (BitmapLock)
-			{
-				DirtyRect = dirtyRect;
-				ResizeBuffer(width, height);
-				Marshal.Copy(buffer, this.buffer, 0, NumberOfBytes);
-			}
-		}
+        public void UpdateBuffer(int width, int height, IntPtr buffer, Rect dirtyRect)
+        {
+            lock (BitmapLock)
+            {
+                DirtyRect = dirtyRect;
+                ResizeBuffer(width, height);
+                Marshal.Copy(buffer, this.buffer, 0, NumberOfBytes);
+            }
+        }
 
-		public Bitmap CreateBitmap()
-		{
-			lock (BitmapLock)
-			{
-				if (Width == 0 || Height == 0 || buffer.Length == 0)
-				{
-					return null;
-				}
+        public Bitmap CreateBitmap()
+        {
+            lock (BitmapLock)
+            {
+                if (Width == 0 || Height == 0 || buffer.Length == 0)
+                {
+                    return null;
+                }
 
-				var bitmap = new Bitmap(Width, Height, Format);
+                var bitmap = new Bitmap(Width, Height, Format);
 
-				var bitmapData = bitmap.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.WriteOnly, Format);
+                var bitmapData = bitmap.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.WriteOnly, Format);
 
-				Marshal.Copy(Buffer, 0, bitmapData.Scan0, NumberOfBytes);
+                Marshal.Copy(Buffer, 0, bitmapData.Scan0, NumberOfBytes);
 
-				bitmap.UnlockBits(bitmapData);
-					
-				return bitmap;
-			}
-		}
-	}
+                bitmap.UnlockBits(bitmapData);
+                    
+                return bitmap;
+            }
+        }
+    }
 }
--- CefSharp.OffScreen/DefaultRenderHandler.cs
+++ CefSharp.OffScreen/DefaultRenderHandler.cs
@@ -11,137 +11,137 @@ using Size = System.Drawing.Size;
 
 namespace CefSharp.OffScreen
 {
-	public class DefaultRenderHandler : IRenderHandler
-	{
-		private ChromiumWebBrowser browser;
+    public class DefaultRenderHandler : IRenderHandler
+    {
+        private ChromiumWebBrowser browser;
 
-		private Size popupSize;
-		private Point popupPosition;
+        private Size popupSize;
+        private Point popupPosition;
 
-		/// <summary>
-		/// Need a lock because the caller may be asking for the bitmap
-		/// while Chromium async rendering has returned on another thread.
-		/// </summary>
-		public readonly object BitmapLock = new object();
+        /// <summary>
+        /// Need a lock because the caller may be asking for the bitmap
+        /// while Chromium async rendering has returned on another thread.
+        /// </summary>
+        public readonly object BitmapLock = new object();
 
-		/// <summary>
-		/// Gets or sets a value indicating whether the popup is open.
-		/// </summary>
-		/// <value>
-		/// <c>true</c> if popup is opened; otherwise, <c>false</c>.
-		/// </value>
-		public bool PopupOpen { get; protected set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether the popup is open.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if popup is opened; otherwise, <c>false</c>.
+        /// </value>
+        public bool PopupOpen { get; protected set; }
 
-		/// <summary>
-		/// Contains the last bitmap buffer. Direct access
-		/// to the underlying buffer - there is no locking when trying
-		/// to access directly, use <see cref="BitmapBuffer.BitmapLock" /> where appropriate.
-		/// </summary>
-		/// <value>The bitmap.</value>
-		public BitmapBuffer BitmapBuffer { get; private set; }
+        /// <summary>
+        /// Contains the last bitmap buffer. Direct access
+        /// to the underlying buffer - there is no locking when trying
+        /// to access directly, use <see cref="BitmapBuffer.BitmapLock" /> where appropriate.
+        /// </summary>
+        /// <value>The bitmap.</value>
+        public BitmapBuffer BitmapBuffer { get; private set; }
 
-		/// <summary>
-		/// The popup Bitmap.
-		/// </summary>
-		public BitmapBuffer PopupBuffer { get; private set; }
+        /// <summary>
+        /// The popup Bitmap.
+        /// </summary>
+        public BitmapBuffer PopupBuffer { get; private set; }
 
-		/// <summary>
-		/// Gets the size of the popup.
-		/// </summary>
-		public Size PopupSize
-		{
-			get { return popupSize; }
-		}
+        /// <summary>
+        /// Gets the size of the popup.
+        /// </summary>
+        public Size PopupSize
+        {
+            get { return popupSize; }
+        }
 
-		/// <summary>
-		/// Gets the popup position.
-		/// </summary>
-		public Point PopupPosition
-		{
-			get { return popupPosition; }
-		}
+        /// <summary>
+        /// Gets the popup position.
+        /// </summary>
+        public Point PopupPosition
+        {
+            get { return popupPosition; }
+        }
 
-		public DefaultRenderHandler(ChromiumWebBrowser browser)
-		{
-			this.browser = browser;
+        public DefaultRenderHandler(ChromiumWebBrowser browser)
+        {
+            this.browser = browser;
 
-			popupPosition = new Point();
-			popupSize = new Size();
+            popupPosition = new Point();
+            popupSize = new Size();
 
-			BitmapBuffer = new BitmapBuffer(BitmapLock);
-			PopupBuffer = new BitmapBuffer(BitmapLock);
-		}
+            BitmapBuffer = new BitmapBuffer(BitmapLock);
+            PopupBuffer = new BitmapBuffer(BitmapLock);
+        }
 
-		public void Dispose()
-		{
-			browser = null;
-		}
+        public void Dispose()
+        {
+            browser = null;
+        }
 
         public virtual ScreenInfo? GetScreenInfo()
         {
             var screenInfo = new ScreenInfo { DeviceScaleFactor = 1.0F };
 
-			return screenInfo;
-		}
+            return screenInfo;
+        }
 
-		public virtual Rect? GetViewRect()
-		{
-			//TODO: See if this can be refactored and remove browser reference
-			var size = browser.Size;
+        public virtual Rect? GetViewRect()
+        {
+            //TODO: See if this can be refactored and remove browser reference
+            var size = browser.Size;
 
-			var viewRect = new Rect(0, 0, size.Width, size.Height);
+            var viewRect = new Rect(0, 0, size.Width, size.Height);
 
-			return viewRect;
-		}
+            return viewRect;
+        }
 
-		public virtual bool GetScreenPoint(int viewX, int viewY, out int screenX, out int screenY)
-		{
-			screenX = viewX;
-			screenY = viewY;
+        public virtual bool GetScreenPoint(int viewX, int viewY, out int screenX, out int screenY)
+        {
+            screenX = viewX;
+            screenY = viewY;
 
-			return false;
-		}
+            return false;
+        }
 
-		public virtual void OnPaint(PaintElementType type, Rect dirtyRect, IntPtr buffer, int width, int height)
-		{
-			var isPopup = type == PaintElementType.Popup;
+        public virtual void OnPaint(PaintElementType type, Rect dirtyRect, IntPtr buffer, int width, int height)
+        {
+            var isPopup = type == PaintElementType.Popup;
 
-			var bitmapBuffer = isPopup ? PopupBuffer : BitmapBuffer;
+            var bitmapBuffer = isPopup ? PopupBuffer : BitmapBuffer;
 
-			bitmapBuffer.UpdateBuffer(width, height, buffer, dirtyRect);
-		}
+            bitmapBuffer.UpdateBuffer(width, height, buffer, dirtyRect);
+        }
 
-		public virtual void OnCursorChange(IntPtr cursor, CursorType type, CursorInfo customCursorInfo)
-		{
-			
-		}
+        public virtual void OnCursorChange(IntPtr cursor, CursorType type, CursorInfo customCursorInfo)
+        {
+            
+        }
 
-		public virtual bool StartDragging(IDragData dragData, DragOperationsMask mask, int x, int y)
-		{
-			return false;
-		}
+        public virtual bool StartDragging(IDragData dragData, DragOperationsMask mask, int x, int y)
+        {
+            return false;
+        }
 
-		public virtual void UpdateDragCursor(DragOperationsMask operation)
-		{
-			
-		}
+        public virtual void UpdateDragCursor(DragOperationsMask operation)
+        {
+            
+        }
 
-		public virtual void OnPopupShow(bool show)
-		{
-			PopupOpen = show;
-		}
+        public virtual void OnPopupShow(bool show)
+        {
+            PopupOpen = show;
+        }
 
-		public virtual void OnPopupSize(Rect rect)
-		{
-			popupPosition.X = rect.X;
-			popupPosition.Y = rect.Y;
-			popupSize.Width = rect.Width;
-			popupSize.Height = rect.Height;
-		}
+        public virtual void OnPopupSize(Rect rect)
+        {
+            popupPosition.X = rect.X;
+            popupPosition.Y = rect.Y;
+            popupSize.Width = rect.Width;
+            popupSize.Height = rect.Height;
+        }
 
-		public virtual void OnImeCompositionRangeChanged(Range selectedRange, Rect[] characterBounds)
-		{
-			
-		}
-	}
+        public virtual void OnImeCompositionRangeChanged(Range selectedRange, Rect[] characterBounds)
+        {
+            
+        }
+    }
 }
