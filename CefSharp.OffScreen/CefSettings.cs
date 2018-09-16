4ab07c8a32c8f3b51b7c317884d5e39fd5a48c3c
amaitland <amaitland@users.noreply.github.com>
2018-09-16 13:12:42 +1000

    OffScreen - CefSettings remove command line args
    OSR mouse scroll issue has been resolved.

--- CefSharp.OffScreen/CefSettings.cs
+++ CefSharp.OffScreen/CefSettings.cs
@@ -20,14 +20,6 @@ namespace CefSharp.OffScreen
             //For OffScreen it doesn't make much sense to enable audio by default, so we disable it.
             //this can be removed in user code if required
             CefCommandLineArgs.Add("mute-audio", "1");
-
-            if (Cef.CefVersion.StartsWith("r3.3497"))
-            {
-                throw new System.Exception("Issue #2408 should have been resolved, remove the below code");
-            }
-
-            //https://github.com/cefsharp/CefSharp/issues/2408
-            CefCommandLineArgs.Add("disable-features", "AsyncWheelEvents,TouchpadAndWheelScrollLatching");
         }
     }
 }
