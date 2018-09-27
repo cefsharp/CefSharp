4cb5d5a3381d47984b4b14fc67ce7f8904e87c15
merceyz <merceyz@users.noreply.github.com>
2018-09-27 20:30:33 +0200

    Removed leftover using

--- CefSharp/WebBrowserExtensions.cs
+++ CefSharp/WebBrowserExtensions.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Specialized;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CefSharp.Internals;
