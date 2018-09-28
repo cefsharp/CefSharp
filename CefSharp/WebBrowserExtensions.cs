63a569c8076a706531606501f1f8c7c1801d0233
amaitland <amaitland@users.noreply.github.com>
2018-09-28 14:15:43 +1000

    WebBrowserExtensions - Add ExecuteScriptAsyncWhenPageLoaded
    Extensions method that attempts to execute javascript immediately if the page is loaded or wire up the LoadingStateChanged
    event handler.

--- CefSharp/WebBrowserExtensions.cs
+++ CefSharp/WebBrowserExtensions.cs
@@ -229,8 +229,62 @@ namespace CefSharp
         }
 
         /// <summary>
-        /// Loads 
+        /// Execute Javascript code in the context of this WebBrowser. This extension method uses the LoadingStateChanged event.
+        /// As the method name implies, the script will be executed asynchronously, and the method therefore returns before the
+        /// script has actually been executed.
+        /// </summary>
+        /// <param name="webBrowser">The ChromiumWebBrowser instance this method extends</param>
+        /// <param name="script">The Javascript code that should be executed.</param>
+        /// <param name="oneTime">The script will only be executed on first page load, subsiquent page loads will be ignored</param>
+        /// <remarks>Best effort is made to make sure the script is executed, there are likely a few edge cases where the script
+        /// won't be executed, if you suspect your script isn't being executed, then try executing in the LoadingStateChanged
+        /// event handler to confirm that it does indeed get executed.</remarks>
+        public static void ExecuteScriptAsyncWhenPageLoaded(this IWebBrowser webBrowser, string script, bool oneTime = true)
+        {
+            var useLoadingStateChangedEventHandler = webBrowser.IsBrowserInitialized == false || oneTime == false;
+
+            //Browser has been initialized, we check if there is a valid document and we're not loading
+            if (webBrowser.IsBrowserInitialized)
+            {
+                //CefBrowser wrapper
+                var browser = webBrowser.GetBrowser();
+                if (browser.HasDocument && browser.IsLoading == false)
+                {
+                    webBrowser.ExecuteScriptAsync(script);
+                }
+                else
+                {
+                    useLoadingStateChangedEventHandler = true;
+                }
+            }
+
+            //If the browser hasn't been initialized we can just wire up the LoadingStateChanged event
+            //If the script has already been executed and oneTime is false will be hooked up next page load.
+            if (useLoadingStateChangedEventHandler)
+            {
+                EventHandler<LoadingStateChangedEventArgs> handler = null;
+
+                handler = (sender, args) =>
+                {
+                    //Wait for while page to finish loading not just the first frame
+                    if (!args.IsLoading)
+                    {
+                        if (oneTime)
+                        {
+                            webBrowser.LoadingStateChanged -= handler;
+                        }
+
+                        webBrowser.ExecuteScriptAsync(script);
+                    }
+                };
+
+                webBrowser.LoadingStateChanged += handler;
+            }
+        }
+
+        /// <summary>
         /// Creates a new instance of IRequest with the specified Url and Method = POST
+        /// and then calls <see cref="IFrame.LoadRequest(IRequest)"/>
         /// </summary>
         /// <param name="browser"></param>
         /// <param name="url"></param>
