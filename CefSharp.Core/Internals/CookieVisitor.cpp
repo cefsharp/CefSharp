98d726298797f0e816d8e45f0f7981d13e75d666
Chris <merceyz@users.noreply.github.com>
2018-12-13 10:59:05 +0100

    Refactoring - Move CefCookie conversion to TypeConversion (#2597)

--- CefSharp.Core/Internals/CookieVisitor.cpp
+++ CefSharp.Core/Internals/CookieVisitor.cpp
@@ -4,54 +4,13 @@
 
 #include "Stdafx.h"
 #include "CookieVisitor.h"
+#include "TypeConversion.h"
 
 namespace CefSharp
 {
     bool CookieVisitor::Visit(const CefCookie& cefCookie, int count, int total, bool& deleteCookie)
     {
-        auto cookie = gcnew Cookie();
-        String^ cookieName = StringUtils::ToClr(cefCookie.name);
-
-        if (!String::IsNullOrEmpty(cookieName))
-        {
-            cookie->Name = StringUtils::ToClr(cefCookie.name);
-            cookie->Value = StringUtils::ToClr(cefCookie.value);
-            cookie->Domain = StringUtils::ToClr(cefCookie.domain);
-            cookie->Path = StringUtils::ToClr(cefCookie.path);
-            cookie->Secure = cefCookie.secure == 1;
-            cookie->HttpOnly = cefCookie.httponly == 1;
-
-            if (cefCookie.has_expires)
-            {
-                auto expires = cefCookie.expires;
-                cookie->Expires = DateTimeUtils::FromCefTime(expires.year,
-                    expires.month,
-                    expires.day_of_month,
-                    expires.hour,
-                    expires.minute,
-                    expires.second,
-                    expires.millisecond);
-            }
-
-
-            auto creation = cefCookie.creation;
-            cookie->Creation = DateTimeUtils::FromCefTime(creation.year,
-                creation.month,
-                creation.day_of_month,
-                creation.hour,
-                creation.minute,
-                creation.second,
-                creation.millisecond);
-
-            auto lastAccess = cefCookie.last_access;
-            cookie->LastAccess = DateTimeUtils::FromCefTime(lastAccess.year,
-                lastAccess.month,
-                lastAccess.day_of_month,
-                lastAccess.hour,
-                lastAccess.minute,
-                lastAccess.second,
-                lastAccess.millisecond);
-        }
+        auto cookie = TypeConversion::FromNative(cefCookie);
 
         return _visitor->Visit(cookie, count, total, deleteCookie);
     }
--- CefSharp.Core/ResourceHandlerWrapper.cpp
+++ CefSharp.Core/ResourceHandlerWrapper.cpp
@@ -7,6 +7,7 @@
 #include "Internals/CefRequestWrapper.h"
 #include "Internals/CefResponseWrapper.h"
 #include "Internals/CefCallbackWrapper.h"
+#include "Internals/TypeConversion.h"
 #include "ResourceHandlerWrapper.h"
 
 using namespace System::Runtime::InteropServices;
@@ -64,49 +65,6 @@ namespace CefSharp
 
     Cookie^ ResourceHandlerWrapper::GetCookie(const CefCookie& cefCookie)
     {
-        auto cookie = gcnew Cookie();
-        String^ cookieName = StringUtils::ToClr(cefCookie.name);
-
-        if (!String::IsNullOrEmpty(cookieName))
-        {
-            cookie->Name = StringUtils::ToClr(cefCookie.name);
-            cookie->Value = StringUtils::ToClr(cefCookie.value);
-            cookie->Domain = StringUtils::ToClr(cefCookie.domain);
-            cookie->Path = StringUtils::ToClr(cefCookie.path);
-            cookie->Secure = cefCookie.secure == 1;
-            cookie->HttpOnly = cefCookie.httponly == 1;
-
-            if (cefCookie.has_expires)
-            {
-                auto expires = cefCookie.expires;
-                cookie->Expires = DateTimeUtils::FromCefTime(expires.year,
-                    expires.month,
-                    expires.day_of_month,
-                    expires.hour,
-                    expires.minute,
-                    expires.second,
-                    expires.millisecond);
-            }
-
-            auto creation = cefCookie.creation;
-            cookie->Creation = DateTimeUtils::FromCefTime(creation.year,
-                creation.month,
-                creation.day_of_month,
-                creation.hour,
-                creation.minute,
-                creation.second,
-                creation.millisecond);
-
-            auto lastAccess = cefCookie.last_access;
-            cookie->LastAccess = DateTimeUtils::FromCefTime(lastAccess.year,
-                lastAccess.month,
-                lastAccess.day_of_month,
-                lastAccess.hour,
-                lastAccess.minute,
-                lastAccess.second,
-                lastAccess.millisecond);
-        }
-
-        return cookie;
+        return TypeConversion::FromNative(cefCookie);
     }
 }
