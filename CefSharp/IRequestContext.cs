5dc6e1fc27454c34ab4cd98ecdc0b9642978e785
amaitland <amaitland@users.noreply.github.com>
2018-12-30 21:03:33 +1000

    IRequestContext/RequestContextExtensions - Further refinements to xml doc

--- CefSharp/IRequestContext.cs
+++ CefSharp/IRequestContext.cs
@@ -242,25 +242,37 @@ namespace CefSharp
         /// and manifestJson should contain the contents that would otherwise be read from the "manifest.json" file on disk.
         /// The loaded extension will be accessible in all contexts sharing the same storage (HasExtension returns true).
         /// However, only the context on which this method was called is considered the loader (DidLoadExtension returns true) and only the
-        /// loader will receive IRequestContextHandler callbacks for the extension. <see cref="IExtensionHandler.OnExtensionLoaded"/> will be
-        /// called on load success or <see cref="IExtensionHandler.OnExtensionLoadFailed"/> will be called on load failure.
+        /// loader will receive IRequestContextHandler callbacks for the extension.
+        ///
+        /// <see cref="IExtensionHandler.OnExtensionLoaded"/> will be called on load success or
+        /// <see cref="IExtensionHandler.OnExtensionLoadFailed"/> will be called on load failure.
+        /// 
         /// If the extension specifies a background script via the "background" manifest key then <see cref="IExtensionHandler.OnBeforeBackgroundBrowser"/>
         /// will be called to create the background browser. See that method for additional information about background scripts.
-        /// For visible extension views the client application should evaluate the manifest to determine the correct extension URL to load and then pass
-        /// that URL to the IBrowserHost.CreateBrowser* function after the extension has loaded. For example, the client can look for the "browser_action"
-        /// manifest key as documented at https://developer.chrome.com/extensions/browserAction. Extension URLs take the form "chrome-extension:///".
-        /// Browsers that host extensions differ from normal browsers as follows: - Can access chrome.* JavaScript APIs if allowed by the manifest.
-        /// Visit chrome://extensions-support for the list of extension APIs currently supported by CEF. - Main frame navigation to non-extension
-        /// content is blocked.
+        /// 
+        /// For visible extension views the client application should evaluate the manifest to determine the correct extension URL to load and then
+        /// load the extension URL in a ChromiumWebBrowser instance after the extension has loaded.
+        ///
+        /// For example, the client can look for the "browser_action" manifest key as documented at https://developer.chrome.com/extensions/browserAction.
+        /// Extension URLs take the form "chrome-extension://&lt;extension_id&gt;/&lt;path&gt;"
+        /// Browsers that host extensions differ from normal browsers as follows:
+        /// 
+        /// - Can access chrome.* JavaScript APIs if allowed by the manifest. Visit chrome://extensions-support for the list of extension APIs currently supported by CEF.
+        /// - Main frame navigation to non-extension content is blocked.
         /// - Pinch-zooming is disabled.
         /// - <see cref="IBrowserHost.GetExtension"/> returns the hosted extension.
-        /// - CefBrowserHost::IsBackgroundHost returns true for background hosts. See https://developer.chrome.com/extensions for extension implementation and usage documentation.
+        /// - CefBrowserHost::IsBackgroundHost returns true for background hosts.
+        ///
+        /// See https://developer.chrome.com/extensions for extension implementation and usage documentation.
         /// </summary>
         /// <param name="rootDirectory">If extension resources will be read from disk using the default load implementation then rootDirectoy
         /// should be the absolute path to the extension resources directory and manifestJson should be null</param>
         /// <param name="manifestJson">If extension resources will be provided by the client then rootDirectory should be a path component unique to the extension
         /// and manifestJson should contain the contents that would otherwise be read from the manifest.json file on disk</param>
         /// <param name="handler">handle events related to browser extensions</param>
+        /// <remarks>For extensions that load a popup you are required to query the Manifest, build a Url in the format
+        /// chrome-extension://{extension.Identifier}/{default_popup} with default_popup url coming from the mainfest. With the extension
+        /// url you then need to open a new Form/Window/Tab and create a new ChromiumWebBrowser instance to host the extension popup</remarks>
         void LoadExtension(string rootDirectory, string manifestJson, IExtensionHandler handler);
     }
 }
--- CefSharp/RequestContextExtensions.cs
+++ CefSharp/RequestContextExtensions.cs
@@ -2,8 +2,6 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-
-using System;
 using System.IO;
 
 namespace CefSharp
@@ -12,21 +10,7 @@ namespace CefSharp
     {
         /// <summary>
         /// Load an extension from the given directory.
-        /// The loaded extension will be accessible in all contexts sharing the same storage (HasExtension returns true).
-        /// However, only the context on which this method was called is considered the loader (DidLoadExtension returns true) and only the
-        /// loader will receive IRequestContextHandler callbacks for the extension. <see cref="IExtensionHandler.OnExtensionLoaded"/> will be
-        /// called on load success or <see cref="IExtensionHandler.OnExtensionLoadFailed"/> will be called on load failure.
-        /// If the extension specifies a background script via the "background" manifest key then <see cref="IExtensionHandler.OnBeforeBackgroundBrowser"/>
-        /// will be called to create the background browser. See that method for additional information about background scripts.
-        /// For visible extension views the client application should evaluate the manifest to determine the correct extension URL to load and then pass
-        /// that URL to the IBrowserHost.CreateBrowser* function after the extension has loaded. For example, the client can look for the "browser_action"
-        /// manifest key as documented at https://developer.chrome.com/extensions/browserAction. Extension URLs take the form "chrome-extension:///".
-        /// Browsers that host extensions differ from normal browsers as follows: - Can access chrome.* JavaScript APIs if allowed by the manifest.
-        /// Visit chrome://extensions-support for the list of extension APIs currently supported by CEF. - Main frame navigation to non-extension
-        /// content is blocked.
-        /// - Pinch-zooming is disabled.
-        /// - <see cref="IBrowserHost.GetExtension"/> returns the hosted extension.
-        /// - <see cref="IBrowserHost.IsBackgroundHost"/> returns true for background hosts. See https://developer.chrome.com/extensions for extension implementation and usage documentation.
+        /// For further details see <seealso cref="IRequestContext.LoadExtension(string, string, IExtensionHandler)"/>
         /// </summary>
         /// <param name="requestContext">request context</param>
         /// <param name="rootDirectory">absolute path to the directory that contains the extension to be loaded.</param>
@@ -40,21 +24,7 @@ namespace CefSharp
         /// Load extension(s) from the given directory. This methods obtains all the sub directories of <paramref name="rootDirectory"/>
         /// and calls <see cref="IRequestContext.LoadExtension(string, string, IExtensionHandler)"/> if manifest.json
         /// is found in the sub folder.
-        /// The loaded extension will be accessible in all contexts sharing the same storage (HasExtension returns true).
-        /// However, only the context on which this method was called is considered the loader (DidLoadExtension returns true) and only the
-        /// loader will receive IRequestContextHandler callbacks for the extension. <see cref="IExtensionHandler.OnExtensionLoaded"/> will be
-        /// called on load success or <see cref="IExtensionHandler.OnExtensionLoadFailed"/> will be called on load failure.
-        /// If the extension specifies a background script via the "background" manifest key then <see cref="IExtensionHandler.OnBeforeBackgroundBrowser"/>
-        /// will be called to create the background browser. See that method for additional information about background scripts.
-        /// For visible extension views the client application should evaluate the manifest to determine the correct extension URL to load and then pass
-        /// that URL to the IBrowserHost.CreateBrowser* function after the extension has loaded. For example, the client can look for the "browser_action"
-        /// manifest key as documented at https://developer.chrome.com/extensions/browserAction. Extension URLs take the form "chrome-extension:///".
-        /// Browsers that host extensions differ from normal browsers as follows: - Can access chrome.* JavaScript APIs if allowed by the manifest.
-        /// Visit chrome://extensions-support for the list of extension APIs currently supported by CEF. - Main frame navigation to non-extension
-        /// content is blocked.
-        /// - Pinch-zooming is disabled.
-        /// - <see cref="IBrowserHost.GetExtension"/> returns the hosted extension.
-        /// - <see cref="IBrowserHost.IsBackgroundHost"/> returns true for background hosts. See https://developer.chrome.com/extensions for extension implementation and usage documentation.
+        /// For further details see <seealso cref="IRequestContext.LoadExtension(string, string, IExtensionHandler)"/>
         /// </summary>
         /// <param name="requestContext">request context</param>
         /// <param name="rootDirectory">absolute path to the directory that contains the extension(s) to be loaded.</param>
