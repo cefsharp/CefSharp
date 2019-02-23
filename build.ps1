5e83050ae041f0cf5dd79e43bb6d5f18834a9514
amaitland <amaitland@users.noreply.github.com>
2019-02-23 14:46:58 +1000

    Upgrade version numbers to 71.0.1

--- build.ps1
+++ build.ps1
@@ -3,9 +3,9 @@
     [Parameter(Position = 0)] 
     [string] $Target = "vs2015",
     [Parameter(Position = 1)]
-    [string] $Version = "71.0.0",
+    [string] $Version = "71.0.1",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "71.0.0"   
+    [string] $AssemblyVersion = "71.0.1"   
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
