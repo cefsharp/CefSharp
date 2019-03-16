32ffab5a75e157cd58b1d46606078e598d0a7027
João Neves <joao.neves@outsystems.com>
2019-03-16 09:35:09 +0000

    Added license header

--- CefSharp.Wpf/DragCursorProvider.cs
+++ CefSharp.Wpf/DragCursorProvider.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright © 2019 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Windows.Input;
