68326c938fcf35a919a8041e9cfd2c36e765a482
amaitland <amaitland@users.noreply.github.com>
2019-06-24 20:41:32 +1000

    Add DefaultRequestHandler (marked as Obsolete) for backwards compatibility
    Ditching Default from naming convention.

---
+++ CefSharp/Handler/DefaultRequestHandler.cs
@@ -0,0 +1,14 @@
+// Copyright © 2019 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
+
+namespace CefSharp.Handler
+{
+    /// <inheritdoc/>>
+    [Obsolete("Use RequestHandler instead.")]
+    public class DefaultRequestHandler : RequestHandler
+    {
+    }
+}
