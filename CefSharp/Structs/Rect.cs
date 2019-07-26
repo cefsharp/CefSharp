9ca6fa92ab3c5caf0b29cca7bb85aad48b23cb47
amaitland <amaitland@users.noreply.github.com>
2019-07-26 20:02:47 +1000

    Rect - Add DebuggerDisplay

--- CefSharp/Structs/Rect.cs
+++ CefSharp/Structs/Rect.cs
@@ -2,11 +2,14 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+using System.Diagnostics;
+
 namespace CefSharp.Structs
 {
     /// <summary>
     /// Represents a rectangle
     /// </summary>
+    [DebuggerDisplay("X = {X}, Y = {Y}, Width = {Width}, Height = {Height}")]
     public struct Rect
     {
         /// <summary>
