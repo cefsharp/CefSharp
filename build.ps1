88ab0a1a69516aca77923b3656997ca82aaa69d8
amaitland <amaitland@users.noreply.github.com>
2019-08-14 10:14:18 +1000

    Update version numbers to 76.1.90

--- build.ps1
+++ build.ps1
@@ -3,9 +3,9 @@
     [Parameter(Position = 0)] 
     [string] $Target = "vs2015",
     [Parameter(Position = 1)]
-    [string] $Version = "75.1.140",
+    [string] $Version = "76.1.90",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "76.1.50"
+    [string] $AssemblyVersion = "76.1.90"
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
