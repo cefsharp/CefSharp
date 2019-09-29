0d236b3b450c43feef40a9e139c6baa706dd3f5b
amaitland <amaitland@users.noreply.github.com>
2019-09-29 14:19:08 +1000

    Example - Update ResponseFilter example to reflect NetworkService changes
    - MemoryStreamResponseFilter renamed to StreamResponseFilter
    - Stream is now passed into StreamResponseFilter constructor
    - ExampleResourceRequestHandler updated to reflect that it now represents a single resource request
    - ExampleResourceRequestHandler is now used for all requests from the CefSharpSchemeHandlerFactory.SchemeName custom scheme
    
    Issue #2743

--- CefSharp.Example/Filters/StreamResponseFilter.cs
+++ CefSharp.Example/Filters/StreamResponseFilter.cs
@@ -8,22 +8,26 @@ using System.IO;
 namespace CefSharp.Example.Filters
 {
     /// <summary>
-    /// MemoryStreamResponseFilter - copies all data from IResponseFilter.Filter
-    /// to a MemoryStream. This is provided as an example to get you started and has not been
+    /// StreamResponseFilter - copies all data from IResponseFilter.Filter
+    /// to the provided Stream. This is provided as an example to get you started and has not been
     /// production tested. If you experience problems you should refer to the CEF documentation
     /// and ask any questions you have on http://magpcss.org/ceforum/index.php
     /// Make sure to ask your question in the context of the CEF API (remember that CefSharp is just a wrapper).
     /// https://magpcss.org/ceforum/apidocs3/projects/(default)/CefResponseFilter.html#Filter(void*,size_t,size_t&,void*,size_t,size_t&)
     /// </summary>
-    public class MemoryStreamResponseFilter : IResponseFilter
+    public class StreamResponseFilter : IResponseFilter
     {
-        private MemoryStream memoryStream;
+        private Stream responseStream;
+
+        public StreamResponseFilter(Stream stream)
+        {
+            responseStream = stream;
+        }
 
         bool IResponseFilter.InitFilter()
         {
-            //NOTE: We could initialize this earlier, just one possible use of InitFilter
-            memoryStream = new MemoryStream();
-
+            //Will only be called a single time.
+            //The filter will not be installed if this method returns false.
             return true;
         }
 
@@ -47,7 +51,7 @@ namespace CefSharp.Example.Filters
             dataOut.Write(readBytes, 0, readBytes.Length);
 
             //Write buffer to the memory stream
-            memoryStream.Write(readBytes, 0, readBytes.Length);
+            responseStream.Write(readBytes, 0, readBytes.Length);
 
             //If we read less than the total amount avaliable then we need
             //return FilterStatus.NeedMoreData so we can then write the rest
@@ -61,13 +65,7 @@ namespace CefSharp.Example.Filters
 
         void IDisposable.Dispose()
         {
-            memoryStream.Dispose();
-            memoryStream = null;
-        }
-
-        public byte[] Data
-        {
-            get { return memoryStream.ToArray(); }
+            responseStream = null;
         }
     }
 }
