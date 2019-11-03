f0697b241a05de96388f41a4e98f711bc84c739b
amaitland <amaitland@users.noreply.github.com>
2019-11-03 11:47:15 +1000

    Upgrade version to 77.1.180

--- build.ps1
+++ build.ps1
@@ -3,9 +3,9 @@
     [Parameter(Position = 0)] 
     [string] $Target = "vs2015",
     [Parameter(Position = 1)]
-    [string] $Version = "75.1.140",
+    [string] $Version = "77.1.180",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "75.1.140"
+    [string] $AssemblyVersion = "77.1.180"
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
