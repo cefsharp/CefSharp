0c060263717bf7b6c90da872fed4741f870c2a2f
amaitland <amaitland@users.noreply.github.com>
2019-12-02 14:49:03 +1000

    WinForms - ControlExtensions.InvokeOnUiThreadIfRequired perform no action if control disposed/ishandlecreated false

--- CefSharp.WinForms/Internals/ControlExtensions.cs
+++ CefSharp.WinForms/Internals/ControlExtensions.cs
@@ -14,11 +14,18 @@ namespace CefSharp.WinForms.Internals
     {
         /// <summary>
         /// Executes the Action asynchronously on the UI thread, does not block execution on the calling thread.
+        /// No action will be performed if the control doesn't have a valid handle or the control has been disposed.
         /// </summary>
         /// <param name="control">the control for which the update is required</param>
         /// <param name="action">action to be performed on the control</param>
         public static void InvokeOnUiThreadIfRequired(this Control control, Action action)
         {
+            //No action
+            if (control.IsDisposed || !control.IsHandleCreated)
+            {
+                return;
+            }
+
             if (control.InvokeRequired)
             {
                 control.BeginInvoke(action);
