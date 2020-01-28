27cae10f157e277d22df5f33fef310bc7f62f61f
amaitland <amaitland@users.noreply.github.com>
2020-01-29 05:52:55 +1000

    Upgrade to CEF 79.1.35+gfebbb4a+chromium-79.0.3945.130 / Chromium 79.0.3945.130

--- build.ps1
+++ build.ps1
@@ -3,9 +3,9 @@
     [Parameter(Position = 0)] 
     [string] $Target = "vs2015",
     [Parameter(Position = 1)]
-    [string] $Version = "79.1.310",
+    [string] $Version = "79.1.350",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "79.1.310"
+    [string] $AssemblyVersion = "79.1.350"
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
