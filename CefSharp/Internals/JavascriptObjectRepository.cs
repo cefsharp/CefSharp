cefsharp/CefSharp

commit 3517f5ce1fdf16cce18fb02f3586f400400b754d

Core - Refactor JavascriptObjectRepository to IJavascriptObjectRepositoryInternal
The new interface has the methods we use internally but don't want to expose to
average users.

CefSharp/Internals/JavascriptObjectRepository.cs
@@ -30,7 +30,7 @@ namespace CefSharp.Internals
     /// All of the registered objects are tracked via meta-data for the objects 
     /// expressed starting with the JavaScriptObject type.
     /// </summary>
-    public class JavascriptObjectRepository : IJavascriptObjectRepository
+    public class JavascriptObjectRepository : IJavascriptObjectRepositoryInternal
     {
         public const string AllObjects = "All";
 
@@ -50,7 +50,7 @@ namespace CefSharp.Internals
         /// <summary>
         /// Has the browser this repository is associated with been initilized (set in OnAfterCreated)
         /// </summary>
-        public bool IsBrowserInitialized { get; set; }
+        bool IJavascriptObjectRepositoryInternal.IsBrowserInitialized { get; set; }
 
         public void Dispose()
         {
@@ -59,19 +59,17 @@ namespace CefSharp.Internals
             ObjectsBoundInJavascript = null;
         }
 
-        public bool HasBoundObjects
+        bool IJavascriptObjectRepository.HasBoundObjects
         {
             get { return objects.Count > 0; }
         }
 
-        public bool IsBound(string name)
+        bool IJavascriptObjectRepository.IsBound(string name)
         {
             return objects.Values.Any(x => x.Name == name);
         }
 
-        //Ideally this would internal, unfurtunately it's used in C++
-        //and it's hard to expose internals
-        public List<JavascriptObject> GetObjects(List<string> names = null)
+        List<JavascriptObject> IJavascriptObjectRepositoryInternal.GetObjects(List<string> names)
         {
             //If there are no objects names or the count is 0 then we will raise
             //the resolve event then return all objects that are registered,
@@ -84,9 +82,11 @@ namespace CefSharp.Internals
                 return objects.Values.Where(x => x.RootObject).ToList();
             }
 
+            IJavascriptObjectRepository repository = this;
+
             foreach (var name in names)
             {
-                if (!IsBound(name))
+                if (repository.IsBound(name))
                 {
                     RaiseResolveObjectEvent(name);
                 }
@@ -99,7 +99,7 @@ namespace CefSharp.Internals
             return objectsByName;
         }
 
-        public void ObjectsBound(List<Tuple<string, bool, bool>> objs)
+        void IJavascriptObjectRepositoryInternal.ObjectsBound(List<Tuple<string, bool, bool>> objs)
         {
             var boundObjectHandler = ObjectBoundInJavascript;
             var boundObjectsHandler = ObjectsBoundInJavascript;
@@ -134,7 +134,7 @@ namespace CefSharp.Internals
             return result;
         }
 
-        public void Register(string name, object value, bool isAsync, BindingOptions options)
+        void IJavascriptObjectRepository.Register(string name, object value, bool isAsync, BindingOptions options)
         {
             if (name == null)
             {
@@ -146,9 +146,11 @@ namespace CefSharp.Internals
                 throw new ArgumentNullException("value");
             }
 
+            IJavascriptObjectRepositoryInternal repository = this;
+
             //Enable WCF if not already enabled - can only be done before the browser has been initliazed
             //if done after the subprocess won't be WCF enabled it we'll have to throw an exception
-            if (!IsBrowserInitialized && !isAsync)
+            if (!repository.IsBrowserInitialized && !isAsync)
             {
                 CefSharpSettings.WcfEnabled = true;
             }
@@ -185,12 +187,12 @@ namespace CefSharp.Internals
             AnalyseObjectForBinding(jsObject, analyseMethods: true, analyseProperties: !isAsync, readPropertyValue: false, camelCaseJavascriptNames: camelCaseJavascriptNames);
         }
 
-        public void UnRegisterAll()
+        void IJavascriptObjectRepository.UnRegisterAll()
         {
             objects.Clear();
         }
 
-        public bool UnRegister(string name)
+        bool IJavascriptObjectRepository.UnRegister(string name)
         {
             foreach (var kvp in objects)
             {
@@ -206,7 +208,7 @@ namespace CefSharp.Internals
             return false;
         }
 
-        internal bool TryCallMethod(long objectId, string name, object[] parameters, out object result, out string exception)
+        bool IJavascriptObjectRepositoryInternal.TryCallMethod(long objectId, string name, object[] parameters, out object result, out string exception)
         {
             exception = "";
             result = null;
@@ -334,7 +336,7 @@ namespace CefSharp.Internals
             return false;
         }
 
-        internal bool TryGetProperty(long objectId, string name, out object result, out string exception)
+        bool IJavascriptObjectRepositoryInternal.TryGetProperty(long objectId, string name, out object result, out string exception)
         {
             exception = "";
             result = null;
@@ -364,7 +366,7 @@ namespace CefSharp.Internals
             return false;
         }
 
-        internal bool TrySetProperty(long objectId, string name, object value, out string exception)
+        bool IJavascriptObjectRepositoryInternal.TrySetProperty(long objectId, string name, object value, out string exception)
         {
             exception = "";
             JavascriptObject obj;

CefSharp.Core/ManagedCefBrowserAdapter.cpp
@@ -162,7 +162,7 @@ IJavascriptCallbackFactory^ ManagedCefBrowserAdapter::JavascriptCallbackFactory:
     return _javascriptCallbackFactory;
 }
 
-JavascriptObjectRepository^ ManagedCefBrowserAdapter::JavascriptObjectRepository::get()
+IJavascriptObjectRepositoryInternal^ ManagedCefBrowserAdapter::JavascriptObjectRepository::get()
 {
     return _javaScriptObjectRepository;
 }

CefSharp.Core/ManagedCefBrowserAdapter.h
@@ -32,7 +32,7 @@ namespace CefSharp
         MCefRefPtr<ClientAdapter> _clientAdapter;
         BrowserProcessServiceHost^ _browserProcessServiceHost;
         IWebBrowserInternal^ _webBrowserInternal;
-        JavascriptObjectRepository^ _javaScriptObjectRepository;
+        IJavascriptObjectRepositoryInternal^ _javaScriptObjectRepository;
         JavascriptCallbackFactory^ _javascriptCallbackFactory;
         IMethodRunnerQueue^ _methodRunnerQueue;
         IBrowser^ _browserWrapper;
@@ -130,9 +130,9 @@ namespace CefSharp
             CefSharp::Internals::IJavascriptCallbackFactory^ get();
         }
 
-        virtual property JavascriptObjectRepository^ JavascriptObjectRepository
+        virtual property IJavascriptObjectRepositoryInternal^ JavascriptObjectRepository
         {
-            CefSharp::Internals::JavascriptObjectRepository^ get();
+            CefSharp::Internals::IJavascriptObjectRepositoryInternal^ get();
         }
 
         virtual property IMethodRunnerQueue^ MethodRunnerQueue

CefSharp.Test/Framework/ConcurrentMethodRunnerQueueFacts.cs
@@ -25,7 +25,7 @@ namespace CefSharp.Test.Framework
         {
             var boundObject = new AsyncBoundObject();
 
-            var objectRepository = new JavascriptObjectRepository();
+            IJavascriptObjectRepositoryInternal objectRepository = new JavascriptObjectRepository();
             objectRepository.Register("testObject", boundObject, true, new BindingOptions { CamelCaseJavascriptNames = false });
             var methodInvocation = new MethodInvocation(1, 1, 1, nameof(boundObject.AsyncWaitTwoSeconds), 1);
             methodInvocation.Parameters.Add("Echo Me!");
@@ -47,7 +47,7 @@ namespace CefSharp.Test.Framework
             const string expectedResult = "Echo Me!";
             var boundObject = new AsyncBoundObject();
 
-            var objectRepository = new JavascriptObjectRepository();
+            IJavascriptObjectRepositoryInternal objectRepository = new JavascriptObjectRepository();
             objectRepository.Register("testObject", boundObject, true, new BindingOptions { CamelCaseJavascriptNames = false });
             var methodInvocation = new MethodInvocation(1, 1, 1, nameof(boundObject.AsyncWaitTwoSeconds), 1);
             methodInvocation.Parameters.Add(expectedResult);

CefSharp/CefSharp.csproj
@@ -106,6 +106,7 @@
     <Compile Include="Internals\BrowserRefCounter.cs" />
     <Compile Include="Internals\CommandLineArgDictionary.cs" />
     <Compile Include="Internals\CookieManagerDecorator.cs" />
+    <Compile Include="Internals\IJavascriptObjectRepositoryInternal.cs" />
     <Compile Include="Internals\ParentProcessMonitor.cs" />
     <Compile Include="Internals\HeaderNameValueCollection.cs" />
     <Compile Include="Internals\TaskScheduler\LimitedConcurrencyLevelTaskScheduler.cs" />

CefSharp/Internals/BrowserProcessService.cs
@@ -9,7 +9,7 @@ namespace CefSharp.Internals
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession, ConcurrencyMode = ConcurrencyMode.Multiple)]
     internal class BrowserProcessService : IBrowserProcess
     {
-        private readonly JavascriptObjectRepository javascriptObjectRepository;
+        private readonly IJavascriptObjectRepositoryInternal javascriptObjectRepository;
         private readonly BrowserProcessServiceHost host;
 
         public BrowserProcessService()

CefSharp/Internals/BrowserProcessServiceHost.cs
@@ -14,9 +14,9 @@ namespace CefSharp.Internals
     {
         private const long OneHundredAndTwentyEightMegaBytesInBytes = 128 * 1024 * 1024;
 
-        public JavascriptObjectRepository JavascriptObjectRepository { get; private set; }
+        public IJavascriptObjectRepositoryInternal JavascriptObjectRepository { get; private set; }
 
-        public BrowserProcessServiceHost(JavascriptObjectRepository javascriptObjectRepository, int parentProcessId, int browserId, IJavascriptCallbackFactory callbackFactory)
+        public BrowserProcessServiceHost(IJavascriptObjectRepositoryInternal javascriptObjectRepository, int parentProcessId, int browserId, IJavascriptCallbackFactory callbackFactory)
             : base(typeof(BrowserProcessService), new Uri[0])
         {
             JavascriptObjectRepository = javascriptObjectRepository;

CefSharp/Internals/ConcurrentMethodRunnerQueue.cs
@@ -16,12 +16,12 @@ namespace CefSharp.Internals
     /// </summary>
     public class ConcurrentMethodRunnerQueue : IMethodRunnerQueue
     {
-        private readonly JavascriptObjectRepository repository;
+        private readonly IJavascriptObjectRepositoryInternal repository;
         private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
         public event EventHandler<MethodInvocationCompleteArgs> MethodInvocationComplete;
 
-        public ConcurrentMethodRunnerQueue(JavascriptObjectRepository repository)
+        public ConcurrentMethodRunnerQueue(IJavascriptObjectRepositoryInternal repository)
         {
             this.repository = repository;
         }

commit d89355dcdf4fdd816880c058ecd3e3a95fad5a9b

Core - JavascriptObjectRepository fix IsBound check
Bug introduced in 3517f5ce1fdf16cce18fb02f3586f400400b754d

CefSharp/Internals/JavascriptObjectRepository.cs
@@ -86,7 +86,7 @@ namespace CefSharp.Internals
 
             foreach (var name in names)
             {
-                if (repository.IsBound(name))
+                if (!repository.IsBound(name))
                 {
                     RaiseResolveObjectEvent(name);
                 }