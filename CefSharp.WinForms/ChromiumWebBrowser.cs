29e6a6d03a31e0c5335066c7cbb9aa1b2366ee16
Gilberto Stankiewicz <gistanki@microsoft.com>
2020-03-25 15:37:02 -0700

    Fix a typo in an error message (#3087)

--- CefSharp.WinForms/ChromiumWebBrowser.cs
+++ CefSharp.WinForms/ChromiumWebBrowser.cs
@@ -139,7 +139,7 @@ namespace CefSharp.WinForms
                 }
                 if (value != null && value.GetType() != typeof(RequestContext))
                 {
-                    throw new Exception(string.Format("RequestContxt can only be of type {0} or null", typeof(RequestContext)));
+                    throw new Exception(string.Format("RequestContext can only be of type {0} or null", typeof(RequestContext)));
                 }
                 requestContext = value;
             }
