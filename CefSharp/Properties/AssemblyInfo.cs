bce627097b08cdc28ac016f38a7d16579d403467
amaitland <amaitland@users.noreply.github.com>
2020-04-27 17:45:53 +1000

    Upgrade version number to 81.2.210

--- CefSharp/Properties/AssemblyInfo.cs
+++ CefSharp/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ namespace CefSharp
         public const bool ComVisible = false;
         public const string AssemblyCompany = "The CefSharp Authors";
         public const string AssemblyProduct = "CefSharp";
-        public const string AssemblyVersion = "80.0.40";
-        public const string AssemblyFileVersion = "80.0.40.0";
+        public const string AssemblyVersion = "81.2.210";
+        public const string AssemblyFileVersion = "81.2.210.0";
         public const string AssemblyCopyright = "Copyright © 2020 The CefSharp Authors";
         public const string CefSharpCoreProject = "CefSharp.Core, PublicKey=" + PublicKey;
         public const string CefSharpBrowserSubprocessProject = "CefSharp.BrowserSubprocess, PublicKey=" + PublicKey;
