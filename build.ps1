d0c14cd03675b3e1922a1b872c011e0695f587c9
amaitland <amaitland@users.noreply.github.com>
2020-08-14 15:17:03 +1000

    Update version number to 84.4.0

--- build.ps1
+++ build.ps1
@@ -3,9 +3,9 @@
     [Parameter(Position = 0)] 
     [string] $Target = "vs2015",
     [Parameter(Position = 1)]
-    [string] $Version = "84.3.30",
+    [string] $Version = "84.4.0",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "84.3.30"
+    [string] $AssemblyVersion = "84.4.0"
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
