89e95ef3bec8eaa23e5374da182bb57cd882d2c9
amaitland <amaitland@users.noreply.github.com>
2020-08-26 14:54:31 +1000

    Core - Add CompositionUnderline.Style (IME)
    https://source.chromium.org/chromium/chromium/src/+/master:ui/base/ime/win/imm32_manager.cc;l=82;drc=c70af83bc44f6829277cdc3621e7015d6e0d7530;bpv=1;bpt=1?q=GCS_COMPCLAUSE&ss=chromium%2Fchromium%2Fsrc
    
    Resolves #3107

--- CefSharp/Structs/CompositionUnderline.cs
+++ CefSharp/Structs/CompositionUnderline.cs
@@ -2,6 +2,8 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+using CefSharp.Enums;
+
 namespace CefSharp.Structs
 {
     /// <summary>
@@ -31,6 +33,11 @@ namespace CefSharp.Structs
         public bool Thick { get; private set; }
 
         /// <summary>
+        /// Style
+        /// </summary>
+        public CompositionUnderlineStyle Style { get; private set; }
+
+        /// <summary>
         /// Constructor.
         /// </summary>
         /// <param name="range">Underline character range.</param>
@@ -46,6 +53,26 @@ namespace CefSharp.Structs
             Color = color;
             BackgroundColor = backGroundColor;
             Thick = thick;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="range">Underline character range.</param>
+        /// <param name="color">Text color. 32-bit ARGB color value, not premultiplied. The color components are always in a known order.
+        /// Equivalent to the SkColor type.</param>
+        /// <param name="backGroundColor">Background color. 32-bit ARGB color value, not premultiplied. The color components are always in
+        /// a known order. Equivalent to the SkColor type.</param>
+        /// <param name="thick">True for thickunderline.</param>
+        /// <param name="style">Style</param>
+        public CompositionUnderline(Range range, uint color, uint backGroundColor, bool thick, CompositionUnderlineStyle style)
+            : this()
+        {
+            Range = range;
+            Color = color;
+            BackgroundColor = backGroundColor;
+            Thick = thick;
+            Style = style;
         }
     }
 }
