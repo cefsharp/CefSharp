178482d9af048785476f5aebdc5ec054a4ed787f
amaitland <amaitland@users.noreply.github.com>
2020-08-26 14:54:31 +1000

    Core - Add CompositionUnderline.Style (IME)
    https://source.chromium.org/chromium/chromium/src/+/master:ui/base/ime/win/imm32_manager.cc;l=82;drc=c70af83bc44f6829277cdc3621e7015d6e0d7530;bpv=1;bpt=1?q=GCS_COMPCLAUSE&ss=chromium%2Fchromium%2Fsrc
    
    Resolves #3107

---
+++ CefSharp/Enums/CompositionUnderlineStyle.cs
@@ -0,0 +1,29 @@
+// Copyright © 2020 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+namespace CefSharp.Enums
+{
+    /// <summary>
+    /// Composition underline style.
+    /// </summary>
+    public enum CompositionUnderlineStyle
+    {
+        /// <summary>
+        /// Solid
+        /// </summary>
+        Solid,
+        /// <summary>
+        /// Dot
+        /// </summary>
+        Dot,
+        /// <summary>
+        /// Dash
+        /// </summary>
+        Dash,
+        /// <summary>
+        /// None
+        /// </summary>
+        None
+    }
+}
