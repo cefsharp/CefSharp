32304440451bd462c1ec5c6f9d41296eefdecd86
amaitland <amaitland@users.noreply.github.com>
2020-09-16 14:32:30 +1000

    WPF Example - Add Chrome internal Urls menu
    - Add new menu for testing/debugging purposes

--- CefSharp.Example/CefExample.cs
+++ CefSharp.Example/CefExample.cs
@@ -38,6 +38,9 @@ namespace CefSharp.Example
         public const string RenderProcessCrashedUrl = "http://processcrashed";
         public const string TestUnicodeResourceUrl = "http://test/resource/loadUnicode";
         public const string PopupParentUrl = "http://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_close";
+        public const string ChromeInternalUrls = "chrome://chrome-urls";
+        public const string ChromeNetInternalUrls = "chrome://net-internals";
+        public const string ChromeProcessInternalUrls = "chrome://process-internals";
 
         // Use when debugging the actual SubProcess, to make breakpoints etc. inside that project work.
         private static readonly bool DebuggingSubProcess = Debugger.IsAttached;
