93e75f5b9e823433371f9eda32588fd17ebff2d5
Alex Maitland <amaitland@users.noreply.github.com>
2020-10-09 10:53:09 +1000

    Example - Update as per MinimalExample

--- CefSharp.Wpf.HwndHost.Example/App.xaml.cs
+++ CefSharp.Wpf.HwndHost.Example/App.xaml.cs
@@ -28,7 +28,23 @@ namespace CefSharp.Wpf.HwndHost.Example
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static void InitializeCefSharp()
         {
-            var settings = new CefSettings();
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            //Example of setting a command line argument
+            //Enables WebRTC
+            // - CEF Doesn't currently support permissions on a per browser basis see https://bitbucket.org/chromiumembedded/cef/issues/2582/allow-run-time-handling-of-media-access
+            // - CEF Doesn't currently support displaying a UI for media access permissions
+            //
+            //NOTE: WebRTC Device Id's aren't persisted as they are in Chrome see https://bitbucket.org/chromiumembedded/cef/issues/2064/persist-webrtc-deviceids-across-restart
+            settings.CefCommandLineArgs.Add("enable-media-stream");
+            //https://peter.sh/experiments/chromium-command-line-switches/#use-fake-ui-for-media-stream
+            settings.CefCommandLineArgs.Add("use-fake-ui-for-media-stream");
+            //For screen sharing add (see https://bitbucket.org/chromiumembedded/cef/issues/2582/allow-run-time-handling-of-media-access#comment-58677180)
+            settings.CefCommandLineArgs.Add("enable-usermedia-screen-capturing");
 
             // Set BrowserSubProcessPath based on app bitness at runtime
             settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
---
+++ CefSharp.Wpf.HwndHost.Example/Converter/TitleConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CefSharp.Wpf.HwndHost.Example.Converter
+{
+    public class TitleConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "CefSharp.MinimalExample.Wpf.HwndHost - " + (value ?? "No Title Specified");
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
