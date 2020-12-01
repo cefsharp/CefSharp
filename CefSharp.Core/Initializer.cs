b3104223dd80eded863e142021cad0ae90126b0d
Alex Maitland <amaitland@users.noreply.github.com>
2020-12-01 10:23:58 +1000

    Net Core - ModuleInitializer load CefShar.Core.Runtime.dl

--- CefSharp.Core/Initializer.cs
+++ CefSharp.Core/Initializer.cs
@@ -3,20 +3,32 @@
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 using System.IO;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
 
 namespace CefSharp
 {
     public static class Initializer
     {
-        public static string InitValue = "";
+        public static string CefSharpCoreRuntimeLocation = "";
 
         [ModuleInitializer]
         internal static void ModuleInitializer()
         {
-            InitValue = "CefSharp Loaded!!!";
+            var assembly = LoadCefSharpCoreRuntime();
+
+            CefSharpCoreRuntimeLocation = assembly.Location;
+        }
+
+        public static Assembly LoadCefSharpCoreRuntime()
+        {
+            //Load into the same context as CefSharp.Core, if user was to create their own context then
+            //this should keep thing together.
+            var currentCtx = AssemblyLoadContext.GetLoadContext(typeof(Initializer).Assembly);
+
             var browserSubprocessDllPath = Path.Combine(Path.GetDirectoryName(typeof(Initializer).Assembly.Location), "CefSharp.Core.Runtime.dll");
-            System.Runtime.Loader.AssemblyLoadContext.Default.LoadFromAssemblyPath(browserSubprocessDllPath);
+            return currentCtx.LoadFromAssemblyPath(browserSubprocessDllPath);
         }
     }
 }
--- CefSharp.WinForms/Initializer.cs
+++ CefSharp.WinForms/Initializer.cs
@@ -2,21 +2,20 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace CefSharp.WinForms
 {
     public static class Initializer
     {
-        public static string InitValue = "";
+        public static string CefSharpCoreRuntimeLocation = "";
 
         [ModuleInitializer]
         internal static void ModuleInitializer()
         {
-            InitValue = "CefSharp WinForms Loaded!!!";
-            var browserSubprocessDllPath = Path.Combine(Path.GetDirectoryName(typeof(Initializer).Assembly.Location), "CefSharp.Core.Runtime.dll");
-            System.Runtime.Loader.AssemblyLoadContext.Default.LoadFromAssemblyPath(browserSubprocessDllPath);
+            var assembly = CefSharp.Initializer.LoadCefSharpCoreRuntime();
+
+            CefSharpCoreRuntimeLocation = assembly.Location;
         }
     }
 }
