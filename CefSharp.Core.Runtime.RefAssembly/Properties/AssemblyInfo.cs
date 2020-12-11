910a5bd2a349d90bc0eb9522a8caf3b7c4b93e66
Alex Maitland <amaitland@users.noreply.github.com>
2020-12-11 19:51:31 +1000

    Net Full - Generate CefSharp.Core.Runtime Ref Assembly
    For now the powershell build script generates the .cs file based on a x86 release build.ps1
    It's not possible to directly install GenApi as it requires a Sdk style project

--- CefSharp.Core.Runtime.RefAssembly/Properties/AssemblyInfo.cs
+++ CefSharp.Core.Runtime.RefAssembly/Properties/AssemblyInfo.cs
@@ -1,10 +1,14 @@
+// Copyright © 2020 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using CefSharp;
 
-[assembly:AssemblyTitle("CefSharp.Core.Runtime")]
-[assembly:AssemblyDescription("CefSharp Core Library (VC++)")]
+[assembly: AssemblyTitle("CefSharp.Core.Runtime")]
+[assembly:AssemblyDescription("CefSharp Core Runtime Library (VC++)")]
 [assembly:AssemblyCompany(AssemblyInfo.AssemblyCompany)]
 [assembly:AssemblyProduct(AssemblyInfo.AssemblyProduct)]
 [assembly:AssemblyCopyright(AssemblyInfo.AssemblyCopyright)]
