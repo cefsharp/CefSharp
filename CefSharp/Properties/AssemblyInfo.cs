995512d9f0c7957dae4f14565cba4973ae3d7ec1
Alex Maitland <amaitland@users.noreply.github.com>
2021-01-27 20:14:43 +1000

    Upgrade build version to 87.1.132

--- CefSharp/Properties/AssemblyInfo.cs
+++ CefSharp/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ namespace CefSharp
         public const bool ComVisible = false;
         public const string AssemblyCompany = "The CefSharp Authors";
         public const string AssemblyProduct = "CefSharp";
-        public const string AssemblyVersion = "87.1.13";
-        public const string AssemblyFileVersion = "87.1.13.0";
+        public const string AssemblyVersion = "87.1.132";
+        public const string AssemblyFileVersion = "87.1.132.0";
         public const string AssemblyCopyright = "Copyright © 2021 The CefSharp Authors";
         public const string CefSharpCoreProject = "CefSharp.Core, PublicKey=" + PublicKey;
         public const string CefSharpBrowserSubprocessProject = "CefSharp.BrowserSubprocess, PublicKey=" + PublicKey;
