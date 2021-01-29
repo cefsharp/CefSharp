a4fdda694506d98fcaf661c64f9980d95c2d8f1d
Alex Maitland <amaitland@users.noreply.github.com>
2021-01-29 17:01:17 +1000

    Update to CEF 88.1.6+g4fe33a1+chromium-88.0.4324.96 / Chromium 88.0.4324.96

--- build.netcore.ps1
+++ build.netcore.ps1
@@ -3,9 +3,9 @@
     [Parameter(Position = 0)] 
     [string] $Target = "netcore31",
     [Parameter(Position = 1)]
-    [string] $Version = "87.1.132",
+    [string] $Version = "88.1.60",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "87.1.132"
+    [string] $AssemblyVersion = "88.1.60"
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
