4608924d5a0c676857720917550674b88bd7b701
Alex Maitland <amaitland@users.noreply.github.com>
2021-04-13 15:21:16 +1000

    Example - Add CefSettings.ChromeRuntime (commented out)

--- CefSharp.Example/CefExample.cs
+++ CefSharp.Example/CefExample.cs
@@ -173,6 +173,10 @@ namespace CefSharp.Example
 
             //settings.LogSeverity = LogSeverity.Verbose;
 
+            //Experimental setting see https://bitbucket.org/chromiumembedded/cef/issues/2969/support-chrome-windows-with-cef-callbacks
+            //for details
+            //settings.ChromeRuntime = true;
+
             if (DebuggingSubProcess)
             {
                 var architecture = RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant();
