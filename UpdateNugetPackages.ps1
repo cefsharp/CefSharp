4a0fb32edddbff516b4b3ec5b74e0e398c2a9ee4
Alex Maitland <amaitland@users.noreply.github.com>
2021-05-08 14:01:51 +1000

    Upgrade to CEF 90.6.5+g7a604aa+chromium-90.0.4430.93 / Chromium 90.0.4430.93

--- UpdateNugetPackages.ps1
+++ UpdateNugetPackages.ps1
@@ -6,7 +6,7 @@
 # Update the .Net 4.5.2 csproj files using nuget.exe
 # Update the .Net Core csproj files modifying the xml file directly
 
-$CefVersion = '90.5.7'
+$CefVersion = '90.6.5'
 
 function RemoveEnsureNuGetPackageBuildImports
 {
