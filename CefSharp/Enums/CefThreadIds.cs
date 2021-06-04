e1ac768a42365a0d65b59a78cdac6b8402abcc3d
Alex Maitland <amaitland@users.noreply.github.com>
2021-06-04 15:29:48 +1000

    Core - Update CefThreadIds enum
    This is a breaking change
    
    Resolves #3597

--- CefSharp/Enums/CefThreadIds.cs
+++ CefSharp/Enums/CefThreadIds.cs
@@ -12,25 +12,40 @@ namespace CefSharp
         // BROWSER PROCESS THREADS -- Only available in the browser process.
 
         /// <summary>
-        /// The CEF UI thread in the browser. In CefSharp this is ALWAYS
-        /// separate from the application's main thread (and thus the main 
-        /// WinForm UI thread).
+        /// The CEF UI thread in the browser. In CefSharp this is by default
+        /// different from your main applications UI Thread
+        /// (e.g. WPF/WinForms UI Threads). Only when MultiThreadedMessageLoop
+        /// is false will this be the same as your app UI Thread.
         /// </summary>
         TID_UI,
 
         /// <summary>
-        /// Used to interact with the database.
+        /// Used for blocking tasks (e.g. file system access) where the user won't
+        /// notice if the task takes an arbitrarily long time to complete. All tasks
+        /// posted after <see cref="IBrowserProcessHandler.OnContextInitialized"/>
+        /// and before Cef.Shutdown() are guaranteed to run.
         /// </summary>
-        TID_DB,
+        TID_FILE_BACKGROUND,
 
         /// <summary>
-        /// Used to interact with the file system.
+        /// Used for blocking tasks (e.g. file system access) that affect UI or
+        /// responsiveness of future user interactions. Do not use if an immediate
+        /// response to a user interaction is expected. All tasks posted after
+        /// <see cref="IBrowserProcessHandler.OnContextInitialized"/> and before Cef.Shutdown()
+        /// are guaranteed to run.
+        /// Examples:
+        /// - Updating the UI to reflect progress on a long task.
+        /// - Loading data that might be shown in the UI after a future user
+        ///   interaction.
         /// </summary>
-        TID_FILE,
+        TID_FILE_USER_VISIBLE,
 
         /// <summary>
-        /// Used for file system operations that block user interactions.
-        /// Responsiveness of this thread affects users.
+        /// Used for blocking tasks (e.g. file system access) that affect UI
+        /// immediately after a user interaction. All tasks posted after
+        /// <see cref="IBrowserProcessHandler.OnContextInitialized"/> and before Cef.Shutdown()
+        /// are guaranteed to run.
+        /// Example: Generating data shown in the UI immediately after a click.
         /// </summary>
         TID_FILE_USER_BLOCKING,
 
@@ -40,12 +55,9 @@ namespace CefSharp
         TID_PROCESS_LAUNCHER,
 
         /// <summary>
-        /// Used to handle slow HTTP cache operations.
-        /// </summary>
-        TID_CACHE,
-
-        /// <summary>
-        /// Used to process IPC and network messages.
+        /// Used to process IPC and network messages. Do not perform blocking tasks on
+        /// this thread. All tasks posted after <see cref="IBrowserProcessHandler.OnContextInitialized"/>
+        /// and before Cef.Shutdown() are guaranteed to run.
         /// </summary>
         TID_IO,
 
@@ -53,6 +65,10 @@ namespace CefSharp
 
         /// <summary>
         /// The main thread in the renderer. Used for all WebKit and V8 interaction.
+        /// Tasks may be posted to this thread after
+        /// CefRenderProcessHandler::OnWebKitInitialized but are not guaranteed to
+        /// run before sub-process termination (sub-processes may be killed at any time
+        /// without warning).
         /// </summary>
         TID_RENDERER,
     }
