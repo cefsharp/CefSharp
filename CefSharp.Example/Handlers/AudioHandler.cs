d0bdc735eb4f190d279a9b402edffd67a335acbc
ValentinNikin <30625611+ValentinNikin@users.noreply.github.com>
2021-07-30 23:30:59 +0300

    Updated example for Audio Handler (#3724)
    * Implemented example for Audio Handler
    
    * Rename AudioHandlerExample to AudioHandler
    
    * Allow unsafe code
    
    * Remove UnmanagedMemoryStream using
    
    * Fix problem with build
    
    * Allow unsafe code for .NET Core project
    
    * Remove excess PropertyGroup
    
    Co-authored-by: Valentin Nikin <nikin.v@tecomgroup.ru>

--- CefSharp.Example/Handlers/AudioHandler.cs
+++ CefSharp.Example/Handlers/AudioHandler.cs
@@ -2,9 +2,10 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-using System;
 using CefSharp.Enums;
 using CefSharp.Structs;
+using System;
+using System.IO;
 
 namespace CefSharp.Example.Handlers
 {
@@ -13,15 +14,18 @@ namespace CefSharp.Example.Handlers
         private ChannelLayout channelLayout;
         private int channelCount;
         private int sampleRate;
+        private readonly FileStream rawAudioFile;
+
+        public AudioHandler(string path) : base()
+        {
+            // The output file with raw audio data (PCM, 32-bit, float) will be saved in this path
+            this.rawAudioFile = new FileStream(path, FileMode.Create, FileAccess.Write);
+        }
 
         protected override bool GetAudioParameters(IWebBrowser chromiumWebBrowser, IBrowser browser, ref AudioParameters parameters)
         {
-            //Cancel Capture
-            return false;
-        }
-        protected override void OnAudioStreamError(IWebBrowser chromiumWebBrowser, IBrowser browser, string errorMessage)
-        {
-            base.OnAudioStreamError(chromiumWebBrowser, browser, errorMessage);
+            // return true to activate audio stream capture
+            return true;
         }
 
         protected override void OnAudioStreamStarted(IWebBrowser chromiumWebBrowser, IBrowser browser, AudioParameters parameters, int channels)
@@ -33,10 +37,33 @@ namespace CefSharp.Example.Handlers
 
         protected override void OnAudioStreamPacket(IWebBrowser chromiumWebBrowser, IBrowser browser, IntPtr data, int noOfFrames, long pts)
         {
-            //NOTE: data is an array representing the raw PCM data as a floating point type, i.e. 4-byte value(s)
-            //Based on and the channelLayout value passed to IAudioHandler.OnAudioStreamStarted
-            //you can calculate the size of the data array in bytes.
-            //See https://github.com/cefsharp/CefSharp/issues/2806 for discussion on implementing an example.
+            /*
+             * NOTE: data is an array representing the raw PCM data as a floating point type, i.e. 4-byte value(s)
+             * Based on noOfFrames and the channels value passed to IAudioHandler.OnAudioStreamStarted
+             * you can calculate the size of the data array in bytes.
+             * 
+             * Audio data (PCM, 32-bit, float) will be save to rawAudioFile stream.
+             */
+
+            unsafe
+            {
+                float** channelData = (float**)data.ToPointer();
+                int size = channelCount * noOfFrames * 4;
+                byte[] samples = new byte[size];
+                fixed (byte* pDestByte = samples)
+                {
+                    float* pDest = (float*)pDestByte;
+
+                    for (int i = 0; i < noOfFrames; i++)
+                    {
+                        for (int c = 0; c < channelCount; c++)
+                        {
+                            *pDest++ = channelData[c][i];
+                        }
+                    }
+                }
+                rawAudioFile.Write(samples, 0, size);
+            }
         }
 
         protected override void OnAudioStreamStopped(IWebBrowser chromiumWebBrowser, IBrowser browser)
@@ -44,8 +71,14 @@ namespace CefSharp.Example.Handlers
             base.OnAudioStreamStopped(chromiumWebBrowser, browser);
         }
 
+        protected override void OnAudioStreamError(IWebBrowser chromiumWebBrowser, IBrowser browser, string errorMessage)
+        {
+            base.OnAudioStreamError(chromiumWebBrowser, browser, errorMessage);
+        }
+
         protected override void Dispose(bool disposing)
         {
+            rawAudioFile.Close();
             base.Dispose(disposing);
         }
     }
