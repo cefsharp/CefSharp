9df9b43b3b0982075fc0fc2371414cd555f29213
amaitland <amaitland@users.noreply.github.com>
2021-08-10 14:57:32 +1000

    Upgrade build version to 92.0.250

--- build.ps1
+++ build.ps1
@@ -3,9 +3,9 @@
     [Parameter(Position = 0)] 
     [string] $Target = "vs2015",
     [Parameter(Position = 1)]
-    [string] $Version = "92.0.200",
+    [string] $Version = "92.0.250",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "92.0.200"
+    [string] $AssemblyVersion = "92.0.250"
 )
 
 $WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
