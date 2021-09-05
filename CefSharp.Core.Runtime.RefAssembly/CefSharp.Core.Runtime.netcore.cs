f5bbb8852235e8972581e4509f53bf670fad34e2
Alex Maitland <amaitland@users.noreply.github.com>
2021-09-05 10:33:37 +1000

    Net Core - Update Ref Assembly Generated Code

--- CefSharp.Core.Runtime.RefAssembly/CefSharp.Core.Runtime.netcore.cs
+++ CefSharp.Core.Runtime.RefAssembly/CefSharp.Core.Runtime.netcore.cs
@@ -63,6 +63,7 @@ namespace CefSharp.Core
         public static System.Threading.Tasks.TaskFactory FileThreadTaskFactory { get { throw null; } set { } }
         public static System.Threading.Tasks.TaskFactory IOThreadTaskFactory { get { throw null; } set { } }
         public static bool IsInitialized { get { throw null; } }
+        public static bool IsShutdown { get { throw null; } }
         public static System.Threading.Tasks.TaskFactory UIThreadTaskFactory { get { throw null; } set { } }
         public static bool AddCrossOriginWhitelistEntry(string sourceOrigin, string targetProtocol, string targetDomain, bool allowTargetSubdomains) { throw null; }
         public static void AddDisposable(System.IDisposable item) { }
