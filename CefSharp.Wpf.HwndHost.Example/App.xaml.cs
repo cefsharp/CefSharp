a92ffdbd3c09834b5a2e5cef8e3c7345bd34193a
Alex Maitland <amaitland@users.noreply.github.com>
2022-02-10 09:51:12 +1000

    Example - Switch to using CefRuntime.SubscribeAnyCpuAssemblyResolver

--- CefSharp.Wpf.HwndHost.Example/App.xaml.cs
+++ CefSharp.Wpf.HwndHost.Example/App.xaml.cs
@@ -4,8 +4,6 @@
 
 using System;
 using System.IO;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace CefSharp.Wpf.HwndHost.Example
@@ -17,9 +15,13 @@ namespace CefSharp.Wpf.HwndHost.Example
     {
         public App()
         {
-            //Add Custom assembly resolver
-            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+#if !NETCOREAPP3_1
+            CefRuntime.SubscribeAnyCpuAssemblyResolver();
+#endif
+        }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
             var settings = new CefSettings()
             {
                 //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
@@ -40,25 +42,8 @@ namespace CefSharp.Wpf.HwndHost.Example
 
             // Make sure you set performDependencyCheck false
             Cef.Initialize(settings, performDependencyCheck: false);
-        }
 
-        // Will attempt to load missing assembly from either x86 or x64 subdir
-        // Required by CefSharp to load the unmanaged dependencies when running using AnyCPU
-        private static Assembly Resolver(object sender, ResolveEventArgs args)
-        {
-            if (args.Name.StartsWith("CefSharp.Core.Runtime"))
-            {
-                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
-                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                       Environment.Is64BitProcess ? "x64" : "x86",
-                                                       assemblyName);
-
-                return File.Exists(archSpecificPath)
-                           ? Assembly.LoadFile(archSpecificPath)
-                           : null;
-            }
-
-            return null;
+            base.OnStartup(e);
         }
     }
 }
