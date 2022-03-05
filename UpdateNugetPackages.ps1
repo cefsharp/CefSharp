87f161dd0fc68e96ab93ce3f659b1f78671a4bdb
Alex Maitland <amaitland@users.noreply.github.com>
2022-03-06 09:21:16 +1000

    Upgrade to CEF 99.2.9+gf426765+chromium-99.0.4844.51 / Chromium 99.0.4844.51

--- UpdateNugetPackages.ps1
+++ UpdateNugetPackages.ps1
@@ -5,7 +5,7 @@
 # Update the vcxproj files first
 # Update the .Net csproj files modifying the xml file directly
 
-$CefVersion = '99.2.7'
+$CefVersion = '99.2.9'
 $CefSharpVersion = $CefVersion + "0"
 
 function RemoveEnsureNuGetPackageBuildImports
