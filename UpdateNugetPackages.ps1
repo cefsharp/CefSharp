78b682a8729bc5bd452697be047b318413aa57ec
Alex Maitland <amaitland@users.noreply.github.com>
2022-04-19 19:44:21 +1000

    UpdateNugetPackages.ps1 - Fix EnsureNuGetPackageBuildImports and remove UpdateNupsecCommonArches
    - Updating of CefSharp.Common.nuspec is handled by build.ps1
    - Order code so functions come before inline code
    - Fix removing EnsureNuGetPackageBuildImports

--- UpdateNugetPackages.ps1
+++ UpdateNugetPackages.ps1
@@ -3,58 +3,17 @@
 
 param(
 	[Parameter(Position = 1)]
-	[string] $CefVersion = "100.0.12",
+	[string] $CefVersion = "101.0.11",
 	[Parameter(Position = 2)]
-	[string] $CefSharpVersion = "",
-	[Parameter(Position = 3)]
-	[string] $BuildArches = "x86 x64 arm64"
+	[string] $CefSharpVersion = ""
 	)
+
 # Update projects files
 # I haven't found a clean solution that allows for using just nuget.exe and dotnet.exe to do this
 # Update the vcxproj files first
 # Update the .Net csproj files modifying the xml file directly
-$ARCHES = [System.Collections.ArrayList]$BuildArches.ToLower().Split(" ");
 Set-StrictMode -version latest;
 $ErrorActionPreference = "Stop";
-
-if ($CefSharpVersion -eq "")
-{
-	$CefSharpVersion = $CefVersion + "0"
-}
-
-function UpdateNupsecCommonArches()
-{
-	$nuspecFile = (Resolve-Path 'NuGet\CefSharp.Common.nuspec');
-	$CefSharpNugetXml = [xml](Get-Content $nuspecFile );
-	$ns = new-object Xml.XmlNamespaceManager $CefSharpNugetXml.NameTable
-	$ns.AddNamespace("ns", $CefSharpNugetXml.DocumentElement.NamespaceURI)
-
-	$srcNode = $CefSharpNugetXml.SelectSingleNode("//ns:package/ns:metadata/ns:dependencies/ns:group/ns:dependency",$ns);
-
-	$parentNode = $srcNode.parentNode;
-	$parentNode.RemoveAll();
-
-
-	$ARCHES.Remove("arm64")
-
-	foreach ($arch in $ARCHES)
-	{
-		$clone = $srcNode.CloneNode($true);
-		$clone.SetAttribute("id","cef.redist." + $arch);
-		$parentNode.AppendChild( $clone ) | Out-Null;
-
-
-	}
-
-	if ( $parentNode.get_HasChildNodes() -eq $false )
-	{
-		$parentNode.AppendChild( $srcNode ); # we need to keep one node on there as it is used as a template, for building other arches this group is ignored anyway
-	}
-
-	$CefSharpNugetXml.Save( $nuspecFile )
-}
-
-UpdateNupsecCommonArches
 
 function DownloadNuget()
 {
@@ -69,25 +28,33 @@ function DownloadNuget()
 		Die "Please install nuget. More information available at: http://docs.nuget.org/docs/start-here/installing-nuget"
 	}
 }
-$WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
-$nuget = Join-Path $WorkingDir .\nuget\NuGet.exe
-DownloadNuget
-
 
 function RemoveEnsureNuGetPackageBuildImports
 {
 	param([Parameter(Position = 0, ValueFromPipeline = $true)][string] $FileName)
 
 	$xml = [xml](Get-Content $FileName)
-	$target = $xml.SelectSingleNode("//Project/Target[@Name='EnsureNuGetPackageBuildImports']");
+	$ns = new-object Xml.XmlNamespaceManager $xml.NameTable
+	$ns.AddNamespace("ns", $xml.DocumentElement.NamespaceURI)
+
+	$target = $xml.SelectSingleNode("//ns:Project/ns:Target[@Name='EnsureNuGetPackageBuildImports']", $ns);
 	
-	if($target -ne $null)
+	if($null -ne $target)
 	{
 		$target.ParentNode.RemoveChild($target)
 
 		$xml.Save( $FileName )
 	}
 }
+
+if ($CefSharpVersion -eq "")
+{
+	$CefSharpVersion = $CefVersion + "0"
+}
+
+$WorkingDir = split-path -parent $MyInvocation.MyCommand.Definition
+$nuget = Join-Path $WorkingDir .\nuget\NuGet.exe
+DownloadNuget
 
 $vcxprojFiles = @('CefSharp.Core.Runtime\CefSharp.Core.Runtime.vcxproj','CefSharp.BrowserSubprocess.Core\CefSharp.BrowserSubprocess.Core.vcxproj')
 
@@ -107,7 +74,6 @@ foreach($file in $vcxprojFiles)
 	RemoveEnsureNuGetPackageBuildImports (Resolve-Path $file)
 }
 
-
 #Read the newly updated version number from the packages.CefSharp.Core.Runtime.config
 
 $CefSharpCorePackagesXml = [xml](Get-Content (Resolve-Path 'CefSharp.Core.Runtime\packages.CefSharp.Core.Runtime.config'))
@@ -125,8 +91,7 @@ foreach($file in $csprojFiles)
 	$xml.Load($file)
 
 	$packRef = $xml.SelectSingleNode("//Project/ItemGroup/PackageReference[@Include='chromiumembeddedframework.runtime']");
-	$packRef.Version = $RedistVersion
-	
+	$packRef.Version = $RedistVersion	
 	
 	$xml.Save( $file )
 }
