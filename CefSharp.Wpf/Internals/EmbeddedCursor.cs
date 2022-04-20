452edca0ec8c9fcdf385c16d50d43d1f3263255f
Alex Maitland <amaitland@users.noreply.github.com>
2022-04-21 09:39:50 +1000

    WPF - Update EmbeddedCursor Res Ids
    This is no longer actually being used and will be removed in the next version
    as manually keeping the Id's up to date is unnecessary work.

--- CefSharp.Wpf/Internals/EmbeddedCursor.cs
+++ CefSharp.Wpf/Internals/EmbeddedCursor.cs
@@ -14,51 +14,29 @@ namespace CefSharp.Wpf.Internals
     // See #4021
     public static class EmbeddedCursor
     {
-        //const int IDC_ALIAS = 35610;
-        //const int IDC_CELL = 35611;
-        //const int IDC_COLRESIZE = 35612;
-        //const int IDC_COPYCUR = 35613;
-        //const int IDC_HAND_GRAB = 35614;
-        //const int IDC_HAND_GRABBING = 35615;
-        //const int IDC_PAN_EAST = 35616;
-        //const int IDC_PAN_MIDDLE = 35617;
-        //const int IDC_PAN_MIDDLE_HORIZONTAL = 35618;
-        //const int IDC_PAN_MIDDLE_VERTICAL = 35619;
-        //const int IDC_PAN_NORTH = 35620;
-        //const int IDC_PAN_NORTH_EAST = 35621;
-        //const int IDC_PAN_NORTH_WEST = 35622;
-        //const int IDC_PAN_SOUTH = 35623;
-        //const int IDC_PAN_SOUTH_EAST = 35624;
-        //const int IDC_PAN_SOUTH_WEST = 35625;
-        //const int IDC_PAN_WEST = 35626;
-        //const int IDC_ROWRESIZE = 35627;
-        //const int IDC_VERTICALTEXT = 35628;
-        //const int IDC_ZOOMIN = 35629;
-        //const int IDC_ZOOMOUT = 35630;
-
         private static Dictionary<CursorType, int> EmbeddedCursors = new Dictionary<CursorType, int>
         {
-            { CursorType.Alias, 35610 },
-            { CursorType.Cell, 35611 },
-            { CursorType.ColumnResize, 35612 },
-            { CursorType.Copy, 35613 },
-            { CursorType.Grab, 35614 },
-            { CursorType.Grabbing, 35615 },
-            { CursorType.EastPanning, 35616 },
-            { CursorType.MiddlePanning, 35617 },
-            { CursorType.MiddlePanningHorizontal, 35618 },
-            { CursorType.MiddlePanningVertical, 35619 },
-            { CursorType.NorthPanning, 35620 },
-            { CursorType.NortheastPanning, 35621 },
-            { CursorType.NorthwestPanning, 35622 },
-            { CursorType.SouthPanning, 35623 },
-            { CursorType.SoutheastPanning, 35624 },
-            { CursorType.SouthwestPanning, 35625 },
-            { CursorType.WestPanning, 35626 },
-            { CursorType.RowResize, 35627 },
-            { CursorType.VerticalText, 35628 },
-            { CursorType.ZoomIn, 35629 },
-            { CursorType.ZoomOut, 35630 }
+            { CursorType.Alias, 35650 },
+            { CursorType.Cell, 35651 },
+            { CursorType.ColumnResize, 35652 },
+            { CursorType.Copy, 35653 },
+            { CursorType.Grab, 35654 },
+            { CursorType.Grabbing, 35655 },
+            { CursorType.EastPanning, 35656 },
+            { CursorType.MiddlePanning, 35657 },
+            { CursorType.MiddlePanningHorizontal, 35658 },
+            { CursorType.MiddlePanningVertical, 35659 },
+            { CursorType.NorthPanning, 35660 },
+            { CursorType.NortheastPanning, 35661 },
+            { CursorType.NorthwestPanning, 35662 },
+            { CursorType.SouthPanning, 35663 },
+            { CursorType.SoutheastPanning, 35664 },
+            { CursorType.SouthwestPanning, 35665 },
+            { CursorType.WestPanning, 35666 },
+            { CursorType.RowResize, 35667 },
+            { CursorType.VerticalText, 35668 },
+            { CursorType.ZoomIn, 35669 },
+            { CursorType.ZoomOut, 35670 }
         };
 
         public static bool TryLoadCursor(CursorType cursorType, out IntPtr cursor)
