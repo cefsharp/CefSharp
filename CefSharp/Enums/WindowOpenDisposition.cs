4e0129009120b78cd35591a6e4e4f1861026d1c7
Alex Maitland <amaitland@users.noreply.github.com>
2022-05-05 09:38:42 +1000

    Core - Update WindowOpenDisposition (101.0.4951.54)

--- CefSharp/Enums/WindowOpenDisposition.cs
+++ CefSharp/Enums/WindowOpenDisposition.cs
@@ -12,42 +12,57 @@ namespace CefSharp
         /// <summary>
         /// An enum constant representing the unknown option.
         /// </summary>
-        Unknown,
+        Unknown = 0,
         /// <summary>
         /// An enum constant representing the current tab option.
         /// </summary>
-        CurrentTab,
+        CurrentTab = 1,
         /// <summary>
         /// Indicates that only one tab with the url should exist in the same window
         /// </summary>
-        SingletonTab,
+        SingletonTab = 2,
         /// <summary>
         /// An enum constant representing the new foreground tab option.
         /// </summary>
-        NewForegroundTab,
+        NewForegroundTab = 3,
         /// <summary>
         /// An enum constant representing the new background tab option.
         /// </summary>
-        NewBackgroundTab,
+        NewBackgroundTab = 4,
         /// <summary>
         /// An enum constant representing the new popup option.
         /// </summary>
-        NewPopup,
+        NewPopup = 5,
         /// <summary>
         /// An enum constant representing the new window option.
         /// </summary>
-        NewWindow,
+        NewWindow = 6,
         /// <summary>
         /// An enum constant representing the save to disk option.
         /// </summary>
-        SaveToDisk,
+        SaveToDisk = 7,
         /// <summary>
         /// An enum constant representing the off the record option.
         /// </summary>
-        OffTheRecord,
+        OffTheRecord = 8,
         /// <summary>
         /// An enum constant representing the ignore action option.
         /// </summary>
-        IgnoreAction
+        IgnoreAction = 9,
+
+        /// <summary>
+		/// Activates an existing tab containing the url, rather than navigating.
+		/// This is similar to SINGLETON_TAB, but searches across all windows from
+		/// the current profile and anonymity (instead of just the current one);
+		/// closes the current tab on switching if the current tab was the NTP with
+		/// no session history; and behaves like CURRENT_TAB instead of
+		/// NEW_FOREGROUND_TAB when no existing tab is found.
+		/// </summary>
+		SwitchToTab = 10,
+
+        /// <summary>
+        /// Creates a new document picture-in-picture window showing a child WebView.
+        /// </summary>
+        NewPictureInPicture = 11,
     }
 }
