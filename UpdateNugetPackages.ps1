923e687fda79c1883e0ed25409316d4e4c4c8653
Alex Maitland <amaitland@users.noreply.github.com>
2022-05-30 13:37:46 +1000

    Upgrade to CEF 102.0.9+g1c5e658+chromium-102.0.5005.63 / Chromium 102.0.5005.63

--- UpdateNugetPackages.ps1
+++ UpdateNugetPackages.ps1
@@ -3,7 +3,7 @@
 
 param(
 	[Parameter(Position = 1)]
-	[string] $CefVersion = "102.0.6",
+	[string] $CefVersion = "102.0.9",
 	[Parameter(Position = 2)]
 	[string] $CefSharpVersion = ""
 	)
--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2019",
     [Parameter(Position = 1)]
-    [string] $Version = "102.0.50",
+    [string] $Version = "102.0.90",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "102.0.50",
+    [string] $AssemblyVersion = "102.0.90",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore", "NetFramework452", "NetCore31")]
     [string] $TargetFramework = "NetFramework",
