423cb21d0d5dd5f452e4f5dd548894eb325d6ef8
Alex Maitland <amaitland@users.noreply.github.com>
2022-07-01 09:34:44 +1000

    Upgrade to CEF 103.0.8+g444ebe7+chromium-103.0.5060.66 / Chromium 103.0.5060.66

--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2019",
     [Parameter(Position = 1)]
-    [string] $Version = "103.0.60",
+    [string] $Version = "103.0.80",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "103.0.60",
+    [string] $AssemblyVersion = "103.0.80",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore", "NetFramework452", "NetCore31")]
     [string] $TargetFramework = "NetFramework",
