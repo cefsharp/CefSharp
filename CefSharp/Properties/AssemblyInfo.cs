6467ece9b6478c6e7fd0fdcfabbf33b2ed5abfa3
Alex Maitland <amaitland@users.noreply.github.com>
2022-08-02 13:14:32 +1000

    Upgrade to 104.4.15+g12fbff8+chromium-104.0.5112.65 / Chromium 104.0.5112.65

--- CefSharp/Properties/AssemblyInfo.cs
+++ CefSharp/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ namespace CefSharp
         public const bool ComVisible = false;
         public const string AssemblyCompany = "The CefSharp Authors";
         public const string AssemblyProduct = "CefSharp";
-        public const string AssemblyVersion = "103.0.80";
-        public const string AssemblyFileVersion = "103.0.80.0";
+        public const string AssemblyVersion = "104.4.150";
+        public const string AssemblyFileVersion = "104.4.150.0";
         public const string AssemblyCopyright = "Copyright © 2022 The CefSharp Authors";
         public const string CefSharpCoreProject = "CefSharp.Core, PublicKey=" + PublicKey;
         public const string CefSharpBrowserSubprocessProject = "CefSharp.BrowserSubprocess, PublicKey=" + PublicKey;
--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2019",
     [Parameter(Position = 1)]
-    [string] $Version = "103.0.80",
+    [string] $Version = "104.4.150",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "103.0.80",
+    [string] $AssemblyVersion = "104.4.150",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore", "NetFramework452", "NetCore31")]
     [string] $TargetFramework = "NetFramework",
