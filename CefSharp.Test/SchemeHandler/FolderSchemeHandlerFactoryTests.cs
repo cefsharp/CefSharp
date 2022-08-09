4a972f6b24c210670afc7b1a6374760df0c1767f
Alex Maitland <amaitland@users.noreply.github.com>
2022-08-09 11:16:22 +1000

    Test - FolderSchemeHandlerFactoryTests.CanDeleteFileAfterLoading
    Integration test for #4119

--- CefSharp.Test/SchemeHandler/FolderSchemeHandlerFactoryTests.cs
+++ CefSharp.Test/SchemeHandler/FolderSchemeHandlerFactoryTests.cs
@@ -13,6 +13,11 @@ namespace CefSharp.Test.SchemeHandler
     public class FolderSchemeHandlerFactoryTests
     {
 
+#if NETCOREAPP
+        private static string ExampleResourcesFolderPath = Path.GetFullPath(@"..\..\..\..\..\..\CefSharp.Example\Resources");
+#else
+        private static string ExampleResourcesFolderPath = Path.GetFullPath(@"..\..\..\..\..\CefSharp.Example\Resources");
+#endif
         private readonly ITestOutputHelper output;
         private readonly CefSharpFixture fixture;
 
@@ -26,11 +31,6 @@ namespace CefSharp.Test.SchemeHandler
         public async Task CanWork()
         {
             const string expected = "https://folderschemehandlerfactory.test/";
-#if NETCOREAPP
-            var folder = Path.GetFullPath(@"..\..\..\..\..\..\CefSharp.Example\Resources");
-#else
-            var folder = Path.GetFullPath(@"..\..\..\..\..\CefSharp.Example\Resources");
-#endif
 
             using (var requestContext = new RequestContext(Cef.GetGlobalRequestContext()))
             using (var browser = new ChromiumWebBrowser(CefExample.DefaultUrl, requestContext: requestContext))
@@ -39,7 +39,7 @@ namespace CefSharp.Test.SchemeHandler
 
                 requestContext.RegisterSchemeHandlerFactory("https",
                     "folderSchemeHandlerFactory.test",
-                    new FolderSchemeHandlerFactory(folder, defaultPage: "HelloWorld.html"));
+                    new FolderSchemeHandlerFactory(ExampleResourcesFolderPath, defaultPage: "HelloWorld.html"));
 
                 var response = await browser.LoadUrlAsync(expected);
 
@@ -51,6 +51,54 @@ namespace CefSharp.Test.SchemeHandler
                 var jsResponse = await browser.EvaluateScriptAsync("document.documentElement.outerHTML");
 
                 Assert.Contains("Hello World", jsResponse.Result.ToString());
+
+                output.WriteLine("Url {0}", mainFrame.Url);
+            }
+        }
+
+        [Fact]
+        public async Task CanDeleteFileAfterLoading()
+        {
+            const string expected = "https://folderschemehandlerfactory.test/";
+            const string html = "I'm going to be deleted after use!";
+
+            using (var requestContext = new RequestContext(Cef.GetGlobalRequestContext()))
+            using (var browser = new ChromiumWebBrowser(CefExample.DefaultUrl, requestContext: requestContext))
+            {
+                _ = await browser.WaitForInitialLoadAsync();
+
+                var tempFile = Path.Combine(ExampleResourcesFolderPath, "DeleteAfterUse.html");
+
+                if(File.Exists(tempFile))
+                {
+                    File.Delete(tempFile);
+                }
+
+                Assert.False(File.Exists(tempFile));
+
+                File.WriteAllText(tempFile, html);
+
+                Assert.True(File.Exists(tempFile));
+
+                requestContext.RegisterSchemeHandlerFactory("https",
+                    "folderSchemeHandlerFactory.test",
+                    new FolderSchemeHandlerFactory(ExampleResourcesFolderPath, defaultPage: "DeleteAfterUse.html"));
+
+                var response = await browser.LoadUrlAsync(expected);
+
+                var mainFrame = browser.GetMainFrame();
+                Assert.True(mainFrame.IsValid);
+                Assert.Equal(expected, mainFrame.Url);
+                Assert.Equal(200, response.HttpStatusCode);
+
+                var jsResponse = await browser.EvaluateScriptAsync("document.documentElement.outerHTML");
+
+                Assert.Contains(html, jsResponse.Result.ToString());
+
+                //Delete the file
+                File.Delete(tempFile);
+
+                Assert.False(File.Exists(tempFile));
 
                 output.WriteLine("Url {0}", mainFrame.Url);
             }
