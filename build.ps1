96926d61b41c92a94e3473c0a2dee593cdea0c1c
Alex Maitland <amaitland@users.noreply.github.com>
2022-09-19 12:58:34 +1000

    Upgrade to 105.3.39+g2ec21f9+chromium-105.0.5195.127 / Chromium 105.0.5195.127

--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2019",
     [Parameter(Position = 1)]
-    [string] $Version = "105.3.330",
+    [string] $Version = "105.3.390",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "105.3.330",
+    [string] $AssemblyVersion = "105.3.390",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore", "NetFramework452", "NetCore31")]
     [string] $TargetFramework = "NetFramework",
