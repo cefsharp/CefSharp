7cdab11dc36a8e42a18f6cf07f8876ce28a708db
Alex Maitland <amaitland@users.noreply.github.com>
2023-03-12 16:53:52 +1000

    Upgrade to 111.2.2+g1b83ff6+chromium-111.0.5563.65 / Chromium 111.0.5563.65

--- UpdateNugetPackages.ps1
+++ UpdateNugetPackages.ps1
@@ -3,7 +3,7 @@
 
 param(
 	[Parameter(Position = 1)]
-	[string] $CefVersion = "111.1.2",
+	[string] $CefVersion = "111.2.2",
 	[Parameter(Position = 2)]
 	[string] $CefSharpVersion = ""
 	)
--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2019",
     [Parameter(Position = 1)]
-    [string] $Version = "111.1.20",
+    [string] $Version = "111.2.20",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "111.1.20",
+    [string] $AssemblyVersion = "111.2.20",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore", "NetFramework452", "NetCore31")]
     [string] $TargetFramework = "NetFramework",
