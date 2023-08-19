1328732a4a7579136c1ce497d6c8217e6165a126
Alex Maitland <amaitland@users.noreply.github.com>
2023-08-20 05:41:20 +1000

    Upgrade to 116.0.13+g557a56f+chromium-116.0.5845.97 / Chromium 116.0.5845.97

--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2019",
     [Parameter(Position = 1)]
-    [string] $Version = "116.0.110",
+    [string] $Version = "116.0.130",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "116.0.110",
+    [string] $AssemblyVersion = "116.0.130",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore", "NetFramework452", "NetCore31")]
     [string] $TargetFramework = "NetFramework",
