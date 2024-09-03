fc0bed592dec5c5f793eddd3649cf2e97774f37f
SLT World <76160587+SLT-World@users.noreply.github.com>
2024-09-03 16:01:46 +0800

    Set --hide-crash-restore-bubble and --disable-chrome-login-prompt (#28)
    Resolves https://github.com/cefsharp/CefSharp/issues/4830 and https://github.com/cefsharp/CefSharp/issues/4903

--- CefSharp.Wpf.HwndHost/CefSettings.cs
+++ CefSharp.Wpf.HwndHost/CefSettings.cs
@@ -9,5 +9,18 @@ namespace CefSharp.Wpf.HwndHost
     /// </summary>
     public class CefSettings : CefSettingsBase
     {
+        /// <summary>
+        /// Intialize with default values
+        /// </summary>
+        public CefSettings() : base()
+        {
+            // CEF doesn't call GetAuthCredentials unless the Chrome login prompt is disabled
+            // https://github.com/chromiumembedded/cef/issues/3603 
+            CefCommandLineArgs.Add("disable-chrome-login-prompt");
+
+            // Disable "Restore pages" popup after incorrect shutdown
+            // https://github.com/chromiumembedded/cef/issues/3767
+            CefCommandLineArgs.Add("hide-crash-restore-bubble");
+        }
     }
 }
