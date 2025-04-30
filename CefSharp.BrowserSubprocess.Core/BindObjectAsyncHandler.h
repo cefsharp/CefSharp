333b40f88616a375d484e579921c83ea202067fc
amaitland <307872+amaitland@users.noreply.github.com>
2025-04-30 19:05:54 +1000

    Readd missing code

--- CefSharp.BrowserSubprocess.Core/BindObjectAsyncHandler.h
+++ CefSharp.BrowserSubprocess.Core/BindObjectAsyncHandler.h
@@ -183,6 +183,11 @@ namespace CefSharp
                                 auto promiseResolve = CefV8Value::CreatePromise();
                                 promiseResolve->ResolvePromise(CreateResultObject(0, "Object(s) already bound", false));
                                 retval = promiseResolve;
+
+                                if (notifyIfAlreadyBound)
+                                {
+                                    NotifyObjectBound(frame, objectNamesWithBoundStatus);
+                                }
                             }
                         }
                         else
