8fe8810937abe3d9f24248ddfd22cdb1d32ef121
amaitland <307872+amaitland@users.noreply.github.com>
2025-05-11 07:39:44 +1000

    Upgrade to 136.1.4+g89c0a8c+chromium-136.0.7103.93 / Chromium 136.0.7103.93

--- UpdateNugetPackages.ps1
+++ UpdateNugetPackages.ps1
@@ -3,7 +3,7 @@
 
 param(
 	[Parameter(Position = 1)]
-	[string] $CefVersion = "135.0.17",
+	[string] $CefVersion = "136.1.4",
 	[Parameter(Position = 2)]
 	[string] $CefSharpVersion = ""
 	)
--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2019",
     [Parameter(Position = 1)]
-    [string] $Version = "135.0.170",
+    [string] $Version = "136.1.40",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "135.0.170",
+    [string] $AssemblyVersion = "136.1.40",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore", "NetFramework462", "NetCore31")]
     [string] $TargetFramework = "NetFramework",
