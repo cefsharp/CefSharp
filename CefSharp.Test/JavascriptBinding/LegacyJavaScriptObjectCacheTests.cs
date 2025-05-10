e064ceec200a23a67b89aa788d0154f8c5333383
amaitland <307872+amaitland@users.noreply.github.com>
2025-05-11 09:50:51 +1000

    Remove comments for consistency

--- CefSharp.Test/JavascriptBinding/LegacyJavaScriptObjectCacheTests.cs
+++ CefSharp.Test/JavascriptBinding/LegacyJavaScriptObjectCacheTests.cs
@@ -11,7 +11,6 @@ namespace CefSharp.Test.JavascriptBinding
         [Fact]
         public void InsertOrUpdateShouldAddObjectsToCache()
         {
-            // Arrange  
             var cache = new LegacyJavaScriptObjectCache();
             var javascriptObjects = new List<JavascriptObject>
             {
@@ -19,10 +18,8 @@ namespace CefSharp.Test.JavascriptBinding
                new JavascriptObject { Name = "Object2" }
             };
 
-            // Act  
             cache.InsertOrUpdate(BrowserId, javascriptObjects);
 
-            // Assert  
             var cachedValues = cache.GetCacheValues(BrowserId);
             Assert.Contains(javascriptObjects[0], cachedValues);
             Assert.Contains(javascriptObjects[1], cachedValues);
@@ -31,7 +28,6 @@ namespace CefSharp.Test.JavascriptBinding
         [Fact]
         public void GetCacheValuesShouldReturnAllCachedObjects()
         {
-            // Arrange  
             var cache = new LegacyJavaScriptObjectCache();
             var javascriptObjects = new List<JavascriptObject>
             {
@@ -40,17 +36,14 @@ namespace CefSharp.Test.JavascriptBinding
             };
             cache.InsertOrUpdate(BrowserId, javascriptObjects);
 
-            // Act  
             var cachedValues = cache.GetCacheValues(BrowserId);
 
-            // Assert  
             Assert.Equal(2, cachedValues.Count);
         }
 
         [Fact]
         public void GetCacheShouldReturnUnderlyingDictionary()
         {
-            // Arrange  
             var cache = new LegacyJavaScriptObjectCache();
             var javascriptObjects = new List<JavascriptObject>
             {
@@ -58,10 +51,8 @@ namespace CefSharp.Test.JavascriptBinding
             };
             cache.InsertOrUpdate(BrowserId, javascriptObjects);
 
-            // Act  
             var cachedDictionary = cache.GetCache(BrowserId);
 
-            // Assert  
             Assert.Single(cachedDictionary);
             Assert.True(cachedDictionary.ContainsKey("Object1"));
         }
@@ -69,7 +60,6 @@ namespace CefSharp.Test.JavascriptBinding
         [Fact]
         public void InsertOrUpdateShouldReplaceExistingObjects()
         {
-            // Arrange  
             var cache = new LegacyJavaScriptObjectCache();
             var initialObjects = new List<JavascriptObject>
             {
@@ -81,10 +71,8 @@ namespace CefSharp.Test.JavascriptBinding
             };
             cache.InsertOrUpdate(BrowserId, initialObjects);
 
-            // Act  
             cache.InsertOrUpdate(BrowserId, updatedObjects);
 
-            // Assert  
             var cachedValues = cache.GetCacheValues(BrowserId);
             Assert.DoesNotContain(initialObjects[0], cachedValues);
             Assert.Contains(updatedObjects[0], cachedValues);
@@ -93,7 +81,6 @@ namespace CefSharp.Test.JavascriptBinding
         [Fact]
         public void InsertOrUpdateShouldAppendObjectsWithDifferentNames()
         {
-            // Arrange  
             var cache = new LegacyJavaScriptObjectCache();
             var initialObjects = new List<JavascriptObject>
             {
@@ -105,10 +92,8 @@ namespace CefSharp.Test.JavascriptBinding
             };
             cache.InsertOrUpdate(BrowserId, initialObjects);
 
-            // Act  
             cache.InsertOrUpdate(BrowserId, updatedObjects);
 
-            // Assert  
             var cachedValues = cache.GetCacheValues(BrowserId);
             Assert.Contains(initialObjects[0], cachedValues);
             Assert.Contains(updatedObjects[0], cachedValues);
@@ -117,7 +102,6 @@ namespace CefSharp.Test.JavascriptBinding
         [Fact]
         public void ClearCacheShouldDoNothing()
         {
-            // Arrange  
             var cache = new LegacyJavaScriptObjectCache();
             var javascriptObjects = new List<JavascriptObject>
             {
@@ -125,10 +109,8 @@ namespace CefSharp.Test.JavascriptBinding
             };
             cache.InsertOrUpdate(BrowserId, javascriptObjects);
 
-            // Act  
             cache.ClearCache(BrowserId);
 
-            // Assert  
             var cachedValues = cache.GetCacheValues(BrowserId);
             Assert.Contains(javascriptObjects[0], cachedValues);
         }
