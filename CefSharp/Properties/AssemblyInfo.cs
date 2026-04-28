0d97181f04680e5b569e5d9856f14cd463dca554
amaitland <307872+amaitland@users.noreply.github.com>
2026-04-28 21:16:21 +1000

    Upgrade to 147.0.10+gd58e84d+chromium-147.0.7727.118 / Chromium 147.0.7727.118

--- CefSharp/Properties/AssemblyInfo.cs
+++ CefSharp/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ namespace CefSharp
         public const bool ComVisible = false;
         public const string AssemblyCompany = "The CefSharp Authors";
         public const string AssemblyProduct = "CefSharp";
-        public const string AssemblyVersion = "146.0.100";
-        public const string AssemblyFileVersion = "146.0.100.0";
+        public const string AssemblyVersion = "147.0.100";
+        public const string AssemblyFileVersion = "147.0.100.0";
         public const string AssemblyCopyright = "Copyright © 2023 The CefSharp Authors";
         public const string CefSharpCoreProject = "CefSharp.Core, PublicKey=" + PublicKey;
         public const string CefSharpBrowserSubprocessProject = "CefSharp.BrowserSubprocess, PublicKey=" + PublicKey;
