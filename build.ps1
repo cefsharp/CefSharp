afb971469bd255ac3329abafa182e7a82185d61c
amaitland <307872+amaitland@users.noreply.github.com>
2026-06-07 08:35:53 +1000

    Upgrade to 149.0.2+ged5b3fd+chromium-149.0.7827.53 / Chromium 149.0.7827.53

--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2022",
     [Parameter(Position = 1)]
-    [string] $Version = "148.0.90",
+    [string] $Version = "149.0.20",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "148.0.90",
+    [string] $AssemblyVersion = "149.0.20",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore")]
     [string] $TargetFramework = "NetFramework",
