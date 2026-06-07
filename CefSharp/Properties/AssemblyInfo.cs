065a28d8af4e166bc4ceb0d3c0b8601501fd8b1d
Jozef Izso <izso@cisco.com>
2026-06-07 12:39:56 +0200

    Upgrade to v144.0.27+g3fae261+chromium-144.0.7559.254

--- CefSharp/Properties/AssemblyInfo.cs
+++ CefSharp/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ namespace CefSharp
         public const bool ComVisible = false;
         public const string AssemblyCompany = "The CefSharp Authors";
         public const string AssemblyProduct = "CefSharp";
-        public const string AssemblyVersion = "144.0.260";
-        public const string AssemblyFileVersion = "144.0.260.0";
+        public const string AssemblyVersion = "144.0.270";
+        public const string AssemblyFileVersion = "144.0.270.0";
         public const string AssemblyCopyright = "Copyright © 2026 The CefSharp Authors";
         public const string CefSharpCoreProject = "CefSharp.Core, PublicKey=" + PublicKey;
         public const string CefSharpBrowserSubprocessProject = "CefSharp.BrowserSubprocess, PublicKey=" + PublicKey;
