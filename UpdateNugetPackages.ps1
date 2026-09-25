b8e9a49f5c109f76b966937acc6ec0da56fb9f53
amaitland <307872+amaitland@users.noreply.github.com>
2026-09-26 07:31:19 +1000

    Upgrade to 152.0.10+g82a832e+chromium-152.0.7977.140 / Chromium 152.0.7977.140

--- UpdateNugetPackages.ps1
+++ UpdateNugetPackages.ps1
@@ -3,7 +3,7 @@
 
 param(
 	[Parameter(Position = 1)]
-	[string] $CefVersion = "152.0.6",
+	[string] $CefVersion = "152.0.10",
 	[Parameter(Position = 2)]
 	[string] $CefSharpVersion = ""
 	)
--- build.ps1
+++ build.ps1
@@ -5,9 +5,9 @@ param(
     [Parameter(Position = 0)] 
     [string] $Target = "vs2022",
     [Parameter(Position = 1)]
-    [string] $Version = "152.0.60",
+    [string] $Version = "152.0.100",
     [Parameter(Position = 2)]
-    [string] $AssemblyVersion = "152.0.60",
+    [string] $AssemblyVersion = "152.0.100",
     [Parameter(Position = 3)]
     [ValidateSet("NetFramework", "NetCore")]
     [string] $TargetFramework = "NetFramework",
